8d2255a14bbc3e63506502d2db29e4fdf9c67497
James Vodanovich <james@jvodan.com>
2015-07-15 19:10:03 +1000

    set pass

--- images/03.serviceImages/email/home/configurators/set_email_admin_secret.sh
+++ images/03.serviceImages/email/home/configurators/set_email_admin_secret.sh
@@ -40,7 +40,8 @@ fcnt=`expr $fcnt + 1`
 	then  
 	salt=`dd if=/dev/urandom count=20 bs=1  | od -h -w | cut -f2- -d" " |head -1 |sed "/[\t ]./s///g" `
  	sec=`echo -n ${salt}:${email_admin_secret} |shasum |cut -f 1 -d" "`
- 	cat /home/app/config.inc.php | sed "/SETUP_PASSWORD/s//$sec/" > /tmp/config.inc.php 
+ 	cat /home/app/config.inc.php | sed "/\'setup_password\'.*/s//\'setup_password\'\] =\'${salt}:${sec}\';/" > /tmp/config.inc.php 
+ 	#cat /home/app/config.inc.php | sed "/SETUP_PASSWORD/s//$sec/" > /tmp/config.inc.php 
  	cp 	/tmp/config.inc.php  /home/app/config.inc.php
  			
  	fi
