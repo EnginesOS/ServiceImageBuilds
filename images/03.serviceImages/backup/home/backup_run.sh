90e59462740606f23fd5a3f52ddb78daa81527f9
James Vodanovich <james@jvodan.com>
2015-07-18 16:21:16 +1000

    report return status of each backup

--- images/03.serviceImages/backup/home/backup_run.sh
+++ images/03.serviceImages/backup/home/backup_run.sh
@@ -5,4 +5,10 @@ for backup in `ls $Backup_ConfigDir`
         		ts=`date +%d_%m_%y`
         		bfn=${backup}_${ts}.log        		
                 duply $backup backup > /var/log/backup/$bfn
+                	if test $? -eq 0
+                		then
+                			echo "Sucessfully backed up $backup"
+                		else
+                			echo "Problem with backup $backup"
+                	fi
         done
