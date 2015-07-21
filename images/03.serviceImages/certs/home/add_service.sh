daaf1de49c7acae145f2b4fd0a3b679bc59dc865
James Vodanovich <james@jvodan.com>
2015-07-21 15:29:10 +1000

    fix

--- images/03.serviceImages/certs/home/add_service.sh
+++ images/03.serviceImages/certs/home/add_service.sh
@@ -9,47 +9,7 @@ load_service_hash_to_environment
 
 #FIXME make engines.internal settable
 
-	if test -z "${cron_job}"
-	then
-		echo Error:Missing cron_job
-        exit -1
-    fi
-  	if test -z ${title}
-	then
-		echo Error:missing title
-        exit -1
-    fi  
-    	if test -z ${parent_engine}
-	then
-		echo Error:missing parent_engine
-        exit -1
-    fi  
-    
-mkdir -p /home/cron/entries/${parent_engine}/
-
-mins=`echo $cron_job | cut -d' ' -f1`
- if [[ $mins == @* ]] 
-  then
-  cmd=`echo $cron_job | cut -d' ' -f 2- `
-	
-  else
-    hrs=`echo $cron_job | cut -d' ' -f2`
-	day=`echo $cron_job | cut -d' ' -f3`
-	dow=`echo $cron_job | cut -d' ' -f4`
-	dom=`echo $cron_job | cut -d' ' -f5`
-	cmd=`echo $cron_job | cut -d' ' -f 6- `
-fi 
-
-if test $action_type == "web"
-	then
-		cmd="wget http://${parent_engine}.engines.internal:8000/$cmd"
-	else
-		cmd="docker exec ${parent_engine} $cmd"
-	fi
-
-echo $mins $hrs $day $dow $dom $cmd  | sed "/STAR/s//\*/g" > /home/cron/entries/${parent_engine}/$title
-
-/home/rebuild_crontab.sh
+openssl genrsa -out device.key 2048
 
 echo "Success"
 exit 0
--- images/03.serviceImages/certs/home/rm_service.sh
+++ images/03.serviceImages/certs/home/rm_service.sh
@@ -8,22 +8,6 @@ load_service_hash_to_environment
 
 #FIXME make engines.internal settable
 
-	if test -z ${cron_job}
-	then
-		echo Error:Missing cron_job
-        exit -1
-    fi
-  	if test -z ${title}
-	then
-		echo Error:missing name
-        exit -1
-    fi  
-
-if test -f /home/entries/${parent_engine}/$title
- then
-	rm /home/cron/entries/${parent_engine}/$title
-	/home/rebuild_crontab.sh
-fi
 
 echo "Success"
 exit 0
