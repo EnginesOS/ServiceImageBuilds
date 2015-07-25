953c188f31e639b3fecfe880a7a1de02f9f91bcb
James Vodanovich <james@jvodan.com>
2015-07-25 18:46:11 +1000

    split dirs into public (shaerd with mgmt) and private

--- images/03.serviceImages/certs/home/add_service.sh
+++ images/03.serviceImages/certs/home/add_service.sh
@@ -13,7 +13,7 @@ if test -z "${cert_name}"
 		echo Error:Missing cert_name
         exit -1
     fi
-openssl genrsa -out  /home/certs/store/keys/${cert_name}.key 2048
+openssl genrsa -out  /home/certs/store/public/keys/${cert_name}.key 2048
 
 echo $country >/home/certs/saved/${cert_name}_setup
 echo $state >>/home/certs/saved/${cert_name}_setup
@@ -24,8 +24,8 @@ echo \*.$domainname  >>/home/certs/saved/${cert_name}_setup
 echo "" >>/home/certs/saved/${cert_name}_setup
 echo "" >>/home/certs/saved/${cert_name}_setup
 echo "" >>/home/certs/saved/${cert_name}_setup
-openssl req -new -key /home/certs/store/keys/${cert_name}.key -out /home/certs/saved/${cert_name}.csr < /home/certs/saved/${cert_name}_setup
-openssl x509 -req -in /home/certs/saved/${cert_name}.csr -CA /home/certs/store/ca/certs/system_CA.pem -CAkey /home/certs/store/ca/keys/system_CA.key -CAcreateserial -out /home/certs/store/certs/${cert_name}.crt -days 500
+openssl req -new -key /home/certs/store/public/keys/${cert_name}.key -out /home/certs/saved/${cert_name}.csr < /home/certs/saved/${cert_name}_setup
+openssl x509 -req -in /home/certs/saved/${cert_name}.csr -CA /home/certs/store/public/ca/certs/system_CA.pem -CAkey /home/certs/store/private/ca/keys/system_CA.key -CAcreateserial -out /home/certs/store/public/certs/${cert_name}.crt -days 500
 
 echo "Success"
 exit 0
--- images/03.serviceImages/certs/home/rm_service.sh
+++ images/03.serviceImages/certs/home/rm_service.sh
@@ -15,7 +15,7 @@ if test -z "${cert_name}"
  
  #FIXME make revocate   in crl
  
- rm /home/certs/store/keys/${cert_name}.key /home/certs/saved/${cert_name}.csr  /home/certs/store/certs/${cert_name}.crt
+ rm /home/certs/store/public/keys/${cert_name}.key /home/certs/saved/${cert_name}.csr  /home/certs/public/store/certs/${cert_name}.crt
  
 echo "Success"
 exit 0
