c654b2b70eb3cc9d90abe79ca81cd80e92e5a776
James Vodanovich <james@jvodan.com>
2015-08-11 09:19:18 +1000

    if no default destination then save but do create system backup if system back set (re)create on backup default destination change

--- images/03.serviceImages/backup/home/configurators/set_default_destination.sh
+++ images/03.serviceImages/backup/home/configurators/set_default_destination.sh
@@ -8,6 +8,10 @@ echo $1 >/home/configurators/saved/default_destination
 
 load_service_hash_to_environment
 
+if test -f /home/configurators/saved/system_backup
+ then
+ 	/home/configurators/set_system_backup.sh `cat /home/configurators/saved/system_backup`
+ fi
 
 #FIX ME Need to reload  system backup or does gui do this ?
 exit 0
