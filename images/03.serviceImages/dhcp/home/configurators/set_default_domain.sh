3fb5ab8cb56920edd525236a0f9b74c36e9b4637
James Vodanovich <james@engines.onl>
2015-10-05 00:06:35 +1100

    m

---
+++ images/03.serviceImages/dhcp/home/configurators/set_default_domain.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+service_hash=$1
+
+echo $1 >/home/configurators/saved/default_domain
+
+. /home/engines/scripts/functions.sh
+
+load_service_hash_to_environment
+
+
+echo $1 |grep = >/dev/null
+        if test $? -ne 0
+        then
+                exit
+        fi
+
+res="${1//[^:]}"
+echo $res
+fcnt=${#res}
+fcnt=`expr $fcnt + 1`
+
+        while test $fcnt -ge $n
+        do
+                nvp="`echo $1 |cut -f$n -d:`"
+                n=`expr $n + 1`
+                name=`echo $nvp |cut -f1 -d=`
+                if ! test -z $name 
+                	then
+                	value="`echo $nvp |cut -f2 -d=`"
+                		if ! test -z $value
+                			then
+                			export $name="$value"
+                		fi
+                fi
+        done
+        
+        
+   	if test  ${#defaultdomain} -gt 5
+	then  
+ 		echo smtp.${defaultdomain} > /etc/postfix/mailname
+ 			
+ 	fi
+ 
+exit 0
