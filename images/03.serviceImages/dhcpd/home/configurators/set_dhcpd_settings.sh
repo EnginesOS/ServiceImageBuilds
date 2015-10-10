8485ff08ed7d50aead9e4741983ad6c265f1237a
James Vodanovich <james@engines.onl>
2015-10-10 22:10:35 +1100

    missed netmask

--- images/03.serviceImages/dhcpd/home/configurators/set_dhcpd_settings.sh
+++ images/03.serviceImages/dhcpd/home/configurators/set_dhcpd_settings.sh
@@ -64,6 +64,6 @@ if test -z $max_lease
   	max_lease=36000
 fi
 
-cat /home/dhcpd.conf.tmpl |sed -e /DOMAIN_NAME/s//$domain_name/ -e /ENGINES_IP/s//$dns_server1/ -e /OPTIONAL_DNS/s//$dns_server2/ -e /DEFAULT_LEASE/s//$default_lease/ -e /MAX_LEASE/s//$max_lease/ -e /SUBNET/s//$subnet/ -e /RANGE_MIN/s//$start/ -e /RANGE_MAX/s//$end/ -e /GATEWAY/s//$default_gateway/ >/tmp/dhcpd.conf
+cat /home/dhcpd.conf.tmpl |sed -e /NETMASK/s//$netmask/ -e /DOMAIN_NAME/s//$domain_name/ -e /ENGINES_IP/s//$dns_server1/ -e /OPTIONAL_DNS/s//$dns_server2/ -e /DEFAULT_LEASE/s//$default_lease/ -e /MAX_LEASE/s//$max_lease/ -e /SUBNET/s//$subnet/ -e /RANGE_MIN/s//$start/ -e /RANGE_MAX/s//$end/ -e /GATEWAY/s//$default_gateway/ >/tmp/dhcpd.conf
 
 sudo /home/install_dhcpd_conf.sh
