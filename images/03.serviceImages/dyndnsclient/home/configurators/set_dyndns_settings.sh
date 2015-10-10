27a6ede072c120a0223c52a24f8bcb68064590af
James Vodanovich <james@engines.onl>
2015-10-10 23:44:35 +1100

    match names

--- images/03.serviceImages/dyndnsclient/home/configurators/set_dyndns_settings.sh
+++ images/03.serviceImages/dyndnsclient/home/configurators/set_dyndns_settings.sh
@@ -8,13 +8,27 @@ echo $1 >/home/configurators/saved/dhcpd_settings
 
 load_service_hash_to_environment
 
-if test -z $domain_name
+if test -z $provider
+ then
+ 	echo "provider not set"
+ 	exit 127
+ fi
+if test -z $login
+ then
+ 	echo "login not set"
+ 	exit 127
+ fi
+ if test -z $login
+ then
+ 	echo "login not set"
+ 	exit 127
+ fi
+ if test -z $domain_name
  then
  	echo "domain_name not set"
  	exit 127
  fi
 
+cat /home/dyndns.conf.tmpl |sed -e /PROVIDER/s//$provider/ -e /LOGIN/s//$login/ -e /PASSWORD/s//$password/ -e /DOMAIN/s//$domain_name/ >/home/dyndns/dyndns.conf 
 
-cat /home/dyndns.conf.tmpl |sed -e /PROVIDER/s//$provider/ -e /LOGIN/s//$login/ -e /PASSWORD/s//$password/ -e /DOMAIN/s//$domain/ >/home/dyndns/dyndns.conf 
 
-sudo /home/install_dhcpd_conf.sh
