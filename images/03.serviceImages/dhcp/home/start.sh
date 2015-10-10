dac5c40da54e8f1286e62663736eb211b6143a6c
James Vodanovich <james@engines.onl>
2015-10-10 11:34:56 +1100

    handle no dhcpd.conf by waiting for it's creation

--- images/03.serviceImages/dhcp/home/start.sh
+++ images/03.serviceImages/dhcp/home/start.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-
-
-
-PID_FILE=/var/run/dhcpd.pid
-export PID_FILE
-. /home/trap.sh
-
-
 mkdir -p /engines/var/run/flags/
 
 
+ PID_FILE=/var/run/dhcpd.pid
+export PID_FILE
+
+. /home/trap.sh
+
+if ! test -f /etc/dhcpd/dhcpd.conf
+   then		
+	touch /engines/var/run/flags/wait_for_dhcpd.conf
+	echo $$ > /var/run/dhcpd.pid
+	while ! test -f /etc/dhcpd/dhcpd.conf
+		do		 
+		sleep 60 &
+		wait
+	done 	
+   fi
 
 
 sudo -n syslogd  -R syslog.engines.internal:5140
