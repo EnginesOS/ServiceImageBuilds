4098d640b201605a720d8eafba13721287fa1e17
James Vodanovich <james@engines.onl>
2015-10-18 11:33:59 +1100

    return blank when not set

--- images/03.serviceImages/dhcpd/home/configurators/read_dhcpd_settings.sh
+++ images/03.serviceImages/dhcpd/home/configurators/read_dhcpd_settings.sh
@@ -4,6 +4,6 @@ if test -f /home/configurators/saved/dhcpd_settings
 	then
 		cat /home/configurators/saved/dhcpd_settings
 	else
-		echo ":domain_name=Not Set:netmask=Not Set:subnet=Not Set:start=Not Set:end=Not Set:default_lease=Not Set:max_lease=Not Set:default_gateway=Not Set:dns_server1=Not Set:dns_server2=Not Set:"
+		echo ":d:"
 fi
 exit 0
