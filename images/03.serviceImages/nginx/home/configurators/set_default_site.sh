7950e8eb26a7d7b2e732573b8d6283a06006d7d0
James Vodanovich <james@engines.onl>
2015-10-20 11:33:02 +1100

    if default_site is empty replace with std default

--- images/03.serviceImages/nginx/home/configurators/set_default_site.sh
+++ images/03.serviceImages/nginx/home/configurators/set_default_site.sh
@@ -36,13 +36,14 @@ fcnt=`expr $fcnt + 1`
         done
         
         
-   	if test -n $default_site_url
+   	if test  ${#default_site_url} -gt 4
 	then  
 	
 	cat /etc/nginx/templates/default_site.tmpl | sed "/FQDN/s//$default_site_url/" > /tmp/site.fqdn
 	 cp /tmp/site.fqdn /etc/nginx/sites-enabled/default
 
- 			
+ else
+ 	cp 	cat /etc/nginx/templates/empty_default /etc/nginx/sites-enabled/default 	
  	fi
  
 exit 0
