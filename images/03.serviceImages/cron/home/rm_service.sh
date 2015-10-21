fd4b923b89156ffefb0491269410b7b851c1c8dd
James Vodanovich <james@engines.onl>
2015-10-21 15:25:53 +1100

    check correct file prior to del

--- images/03.serviceImages/cron/home/rm_service.sh
+++ images/03.serviceImages/cron/home/rm_service.sh
@@ -8,18 +8,18 @@ load_service_hash_to_environment
 
 #FIXME make engines.internal settable
 
-	if test -z ${cron_job}
+	if test -z "${cron_job}"
 	then
 		echo Error:Missing cron_job
         exit -1
     fi
   	if test -z ${title}
 	then
-		echo Error:missing name
+		echo Error:missing title
         exit -1
     fi  
 
-if test -f /home/entries/${parent_engine}/$title
+if test -f /home/cron/entries/${parent_engine}/$title
  then
 	rm /home/cron/entries/${parent_engine}/$title
 	/home/rebuild_crontab.sh
