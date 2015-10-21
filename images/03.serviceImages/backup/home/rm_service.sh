4aba9b55ff1d54feafbe218353621d2562485f60
James Vodanovich <james@engines.onl>
2015-10-21 12:55:33 +1100

    dbg

--- images/03.serviceImages/backup/home/rm_service.sh
+++ images/03.serviceImages/backup/home/rm_service.sh
@@ -6,9 +6,13 @@ service_hash=$1
 . /home/engines/scripts/functions.sh
 
 load_service_hash_to_environment
-
+echo name $name
+echo parent_engine $parent_engine
+echo src_type $src_type
 dirname=${parent_engine}_${name}_${src_type}
 dirname=${Backup_ConfigDir}/$dirname
+
+echo dirname $dirname
 
  echo "${dirname}: $*" >>/var/log/backup//rmbackup.log
 if test -n $1
