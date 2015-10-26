e132e750a3f2a1ea797e2da245e341cec64dc9a4
James L Vodanovich <james@jvodan.com>
2015-10-26 04:17:29 +0000

    tidy

--- images/03.serviceImages/registry/home/start.sh
+++ images/03.serviceImages/registry/home/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-
+pid=$$
+echo $pid >/tmp/pid
 PID_FILE=/tmp/pid
 export PID_FILE
 . /home/trap.sh
@@ -11,7 +12,7 @@ mkdir -p /engines/var/run/flags/
 cd /home/registry
 cd /home/registry/EnginesSystemRegistry/src/
 git pull
- rm /var/lib/engines/registry/.reglock 
+ rm -f /tmp/registry.lock
 /usr/local/rbenv/shims/ruby /home/registry/EnginesSystemRegistry/src/server.rb  > /var/log/regsitry.log&
 pid=$!
 echo $pid >/tmp/pid
@@ -20,3 +21,4 @@ touch /engines/var/run/flags/startup_complete
 wait 
 kill -TERM  $pid
 rm -f /engines/var/run/flags/startup_complete
+rm -f /tmp/pid
