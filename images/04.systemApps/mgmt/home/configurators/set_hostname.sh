ac8b7cb99cef611b2996b1fcbf6ce89eec51cbb0
James L Vodanovich <james@jvodan.com>
2015-10-27 22:55:39 +0000

    domain_name is now required

--- images/04.systemApps/mgmt/home/configurators/set_hostname.sh
+++ images/04.systemApps/mgmt/home/configurators/set_hostname.sh
@@ -10,13 +10,15 @@ load_service_hash_to_environment
 if test -z ${hostname} 
 	then  
 	 echo "Error: Missing hostname"
+	  exit 128
 fi
 
-/opt/engines/scripts/set_hostname.sh $hostname
 
-if ! test -z ${domain_name} 
+
+if  test -z ${domain_name} 
 	then  
-		/opt/engines/scripts/set_domain_name.sh $domain_name
+		 echo "Error: Missing domain_name"
+	  exit 128
 fi
-
+/opt/engines/scripts/set_hostname.sh $hostname $domain_name
 exit 0
