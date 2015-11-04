b02ab03e907f99167539817fe1fc938cafff3fc4
James L Vodanovich <james@jvodan.com>
2015-11-04 05:51:22 +0000

    backup last add service run config build files

--- images/03.serviceImages/nginx/home/add_service.sh
+++ images/03.serviceImages/nginx/home/add_service.sh
@@ -104,8 +104,12 @@ if test "$proto" = default
 	     	fi
 	 	cp /tmp/site.path /etc/nginx/sites-enabled/${proto}_${fqdn}.site
 fi
-	 
-	 rm /tmp/site.*
+
+mkdir -p /tmp/last_run
+cp 	 /site.* /tmp/last_run
+
+rm /tmp/site.*
+
 	 if ! test -d /var/log/nginx/$fqdn/https/
 	 	then
 	 		mkdir -p /var/log/nginx/$fqdn/https/
@@ -114,6 +118,7 @@ fi
  	then
  		mkdir -p /var/log/nginx/$fqdn/http/
  	fi
-	 kill -HUP `cat /run/nginx/nginx.pid`
+ 	
+ kill -HUP `cat /run/nginx/nginx.pid`
 	 
 	 echo Success
