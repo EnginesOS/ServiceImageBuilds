40b53e5221380dd620c30cd1eefb7177bd3989cc
James L Vodanovich <james@jvodan.com>
2015-11-12 22:44:05 +0000

    add error checking

--- images/03.serviceImages/certs/home/add_service.sh
+++ images/03.serviceImages/certs/home/add_service.sh
@@ -18,7 +18,7 @@ if test -z "${cert_name}"
     mkdir -p /home/certs/store/public/keys/
     mkdir -p /home/certs/store/public/certs
     
-openssl genrsa -out  /home/certs/store/public/keys/${cert_name}.key 2048
+openssl genrsa -out  /home/certs/store/public/keys/${cert_name}.key.tmp 2048
 
 echo $country >/home/certs/saved/${cert_name}_setup
 echo $state >>/home/certs/saved/${cert_name}_setup
@@ -29,8 +29,25 @@ echo \*.$domainname  >>/home/certs/saved/${cert_name}_setup
 echo "" >>/home/certs/saved/${cert_name}_setup
 echo "" >>/home/certs/saved/${cert_name}_setup
 echo "" >>/home/certs/saved/${cert_name}_setup
-openssl req -new -key /home/certs/store/public/keys/${cert_name}.key -out /home/certs/saved/${cert_name}.csr < /home/certs/saved/${cert_name}_setup
-openssl x509 -req -in /home/certs/saved/${cert_name}.csr -CA  /home/certs/store/public/ca/certs/system_CA.pem -CAkey /home/certs/store/private/ca/keys/system_CA.key -CAcreateserial -out /home/certs/store/public/certs/${cert_name}.crt -days 500
-
+openssl req -new -key /home/certs/store/public/keys/${cert_name}.key.tmp -out /home/certs/saved/${cert_name}.csr < /home/certs/saved/${cert_name}_setup
+if test $? -ne 0
+ then
+ 	echo "Failed to Create CSR"
+ 	exit 127
+ fi
+openssl x509 -req -in /home/certs/saved/${cert_name}.csr -CA  /home/certs/store/public/ca/certs/system_CA.pem -CAkey /home/certs/store/private/ca/keys/system_CA.key -CAcreateserial -out /home/certs/store/public/certs/${cert_name}.crt.tmp -days 500
+if test $? -ne 0
+ then
+ 	echo "Failed to sign CSR"
+ 	exit 127
+ fi
+  if test -f  /home/certs/store/public/keys/${cert_name}.key.tmp -a -f /home/certs/store/public/certs/${cert_name}.crt.tmp
+   then
+    mv /home/certs/store/public/keys/${cert_name}.key.tmp /home/certs/store/public/keys/${cert_name}.key
+    mv /home/certs/store/public/certs/${cert_name}.crt.tmp /home/certs/store/public/certs/${cert_name}.crt
+   else
+    echo "Cert and Key files not present"
+    exit 127
+   fi
 echo "Success"
 exit 0
