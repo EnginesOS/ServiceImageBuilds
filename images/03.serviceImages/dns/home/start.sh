a57c1b59eb360b8a9dbabe7a450880ea7afe3caa
James L Vodanovich <james@jvodan.com>
2015-11-23 20:42:25 +0000

    handle dynamic docker net

--- images/03.serviceImages/dns/home/start.sh
+++ images/03.serviceImages/dns/home/start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 
+grep BLANK /var/lib/bind/engines/engines.dnsrecords >/dev/null
+if test $? -eq 0
+ then
+	ip=`echo -n  `grep mgmt /etc/hosts|awk '{print $1}' |cut -d. -f-3`.1`
+	cat  /etc/bind/templates/engines.internal.domain.tmpl |sed "/IP/s//$ip/" > /var/lib/bind/engines/engines.dnsrecords
+ fi
 
 PID_FILE=/var/run/named/named.pid
 export PID_FILE
