82b8715dfe1601dc0e83065659af495f78546f91
James L Vodanovich <james@jvodan.com>
2015-12-27 05:51:08 +0000

    random pass phrase and readers for backup

--- images/03.serviceImages/backup/home/prep_conf.sh
+++ images/03.serviceImages/backup/home/prep_conf.sh
@@ -2,13 +2,16 @@
 
 cp /home/tmpl/duply_conf  $1
 
+
+
 key=`cat /home/backup/.gnupg/key_id`
 	if test -z "$key"
 	then
 		echo "GPG_KEY='disabled'" >> $1
 	else
+	    pass=`cat /home/backup/.gnupg/pass`
 		echo GPG_KEY=$key >> $1
-		echo GPG_PW=backup >> $1
+		echo GPG_PW=$pass >> $1
 	fi
 	
 
---
+++ images/03.serviceImages/backup/home/readers/public_key.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ gpg -a --export `cat /home/backup/.gnupg/key_id`
