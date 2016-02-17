e3ad08eefd33dcde3b3e915de24fd2ad6c241283
James L Vodanovich <james@jvodan.com>
2016-02-17 05:22:40 +0000

    tidy start

--- images/03.serviceImages/sumologic/home/start.sh
+++ images/03.serviceImages/sumologic/home/start.sh
@@ -14,9 +14,8 @@ if test -f /home/configurators/saved/credentials
  	sleep 500
  	exit
  fi
-#access_id=$collector_id
-#access_key=$collector_key
-#echo $access_id 
+
+echo Access ID $access_id 
 
 collector_name=${collector_name:=collector_container}
 sources_json=${SUMO_SOURCES_JSON:=/home/sumo-sources.json}
