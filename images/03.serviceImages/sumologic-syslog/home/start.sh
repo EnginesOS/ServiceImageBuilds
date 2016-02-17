e962de37e76b2fc1cd8f995869d1646f36a296f7
James L Vodanovich <james@jvodan.com>
2016-02-17 01:56:38 +0000

    log

--- images/03.serviceImages/sumologic-syslog/home/start.sh
+++ images/03.serviceImages/sumologic-syslog/home/start.sh
@@ -5,13 +5,16 @@ if test -f /home/configurators/saved/credentials
  	 cat /home/configurators/saved/credentials | /home/engines/bin/json_to_env >/tmp/.env
  	. /tmp/.env
  	rm -f /engines/var/run/missing_configuration
+ 	echo Configured
  else
  	touch /engines/var/run/missing_configuration
  	echo "Not Configured"
+ 	
  	exit
  fi
 access_id=$collector_id
-access_key=$collector_id
+access_key=$collector_key
+echo $collector_id 
 #access_id=${SUMO_ACCESS_ID:=$1}
 #access_key=${SUMO_ACCESS_KEY:=$2}
 collector_name=${SUMO_COLLECTOR_NAME:=collector_container}
