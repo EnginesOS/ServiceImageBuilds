b53671180b30a923e6f44cbd925cdca9b4227271
James L Vodanovich <james@jvodan.com>
2016-02-17 01:46:21 +0000

    set args

--- images/03.serviceImages/sumologic-syslog/home/start.sh
+++ images/03.serviceImages/sumologic-syslog/home/start.sh
@@ -10,6 +10,8 @@ if test -f /home/configurators/saved/credentials
  	echo "Not Configured"
  	exit
  fi
+access_id=$collector_id
+access_key=$collector_id
 #access_id=${SUMO_ACCESS_ID:=$1}
 #access_key=${SUMO_ACCESS_KEY:=$2}
 collector_name=${SUMO_COLLECTOR_NAME:=collector_container}
@@ -18,12 +20,12 @@ sources_json=${SUMO_SOURCES_JSON:=/etc/sumo-sources.json}
 if [ -z "$access_id" ] || [ -z "$access_key" ]; then
         echo "FATAL: Please provide credentials, either via the SUMO_ACCESS_ID and SUMO_ACCESS_KEY environment variables,"
         echo "       or as the first two command line arguments!"
-        exit 1
+        sleep 500
 fi
 
 if [ ! -e $sources_json ]; then
         echo "FATAL: Unable to find $sources_json - please make sure you include it in your image!"
-        exit 1
+           sleep 500
 fi
 
 sudo /opt/SumoCollector/collector console -- -t -i $access_id -k $access_key -n $collector_name -s $sources_json
