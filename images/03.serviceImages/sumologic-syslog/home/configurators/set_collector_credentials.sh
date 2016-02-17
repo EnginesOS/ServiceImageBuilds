eb3e0a52c3659bcb09f4218e75101161f561b341
James L Vodanovich <james@jvodan.com>
2016-02-17 03:31:41 +0000

    polish and no ' ' around json string

--- images/03.serviceImages/sumologic-syslog/home/configurators/set_collector_credentials.sh
+++ images/03.serviceImages/sumologic-syslog/home/configurators/set_collector_credentials.sh
@@ -2,7 +2,7 @@
 
 service_hash=$1
 
-echo \'$1\' >/home/configurators/saved/credentials
+echo $1 >/home/configurators/saved/credentials
 
  echo $service_hash | /home/engines/bin/json_to_env >/tmp/.env
 
--- images/03.serviceImages/sumologic-syslog/home/start.sh
+++ images/03.serviceImages/sumologic-syslog/home/start.sh
@@ -16,21 +16,23 @@ if test -f /home/configurators/saved/credentials
  fi
 access_id=$collector_id
 access_key=$collector_key
-echo $collector_id 
-#access_id=${SUMO_ACCESS_ID:=$1}
-#access_key=${SUMO_ACCESS_KEY:=$2}
-collector_name=${SUMO_COLLECTOR_NAME:=collector_container}
+echo $access_id 
+
+collector_name=${collector_name:=collector_container}
 sources_json=${SUMO_SOURCES_JSON:=/home/sumo-sources.json}
 
 if [ -z "$access_id" ] || [ -z "$access_key" ]; then
         echo "FATAL: Please provide credentials, either via the SUMO_ACCESS_ID and SUMO_ACCESS_KEY environment variables,"
         echo "       or as the first two command line arguments!"
         sleep 500
+        exit -1
 fi
 
 if [ ! -e $sources_json ]; then
         echo "FATAL: Unable to find $sources_json - please make sure you include it in your image!"
            sleep 500
+           exit -1
 fi
 
-sudo /opt/SumoCollector/collector console -- -t -i $access_id -k $access_key -n $collector_name -s $sources_json
+sudo /opt/SumoCollector/collector console -- -t -i $access_id -k $access_key -n $collector_name -s $sources_json &
+wait $!
