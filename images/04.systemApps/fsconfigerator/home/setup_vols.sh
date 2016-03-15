40b2fc6b61d7252e8f973625f013e7a560f3efd3
James L Vodanovich <james@jvodan.com>
2016-03-15 18:46:50 +1100

    tidy

--- images/04.systemApps/fsconfigerator/home/setup_vols.sh
+++ images/04.systemApps/fsconfigerator/home/setup_vols.sh
@@ -20,30 +20,27 @@ chown $fw_user -R /client/state
 chgrp 22020  -R /client/state
 chmod g+w  -R /client/state
 
-if test -f /dest/fs/.persistent_lock
- then
-  chown -R $fw_user /dest/fs/*
-  chmod g+w -R  /dest/fs/*
+#if test -f /dest/fs/.persistent_lock
+# then
+#  chown -R $fw_user /dest/fs/*
+#  chmod g+w -R  /dest/fs/*
   
-else
+#else
 
 	cd /home/fs_src/
 	
 	for dest_dir in `ls /dest/fs/`
 	 do	 
-	 if test -f /dest/fs/$dir/.persistent_lock
+	 if test -f /dest/fs/$dest_dir/.persistent_lock
  		then
- 		 chown -R $fw_user /dest/fs/$dir
-  		  chmod g+w -R  /dest/fs/$dir
+ 		 chown -R $fw_user /dest/fs/$dest_dir
+  		  chmod g+w -R  /dest/fs/$dest_dir
  		continue
  		fi
 	   src_dir=`echo $dest_dir | sed "/_/s//\//g" | sed " /\/home\/fs/s//\/home\/fs_src/" `
 	   cp -rpn $src_dir/. /dest/fs/$dest_dir
-	   	   touch /dest/fs/$dir/.persistent_lock
-	   chown -R ${fw_user}.${data_gid}  /dest/fs/$dir
-	   echo "touch /dest/fs/$dir/.persistent_lock"
-
-	touch /dest/fs/$dir/.persistent
+	   	   touch /dest/fs/$dest_dir/.persistent_lock
+	   chown -R ${fw_user}.${data_gid}  /dest/fs/$dest_dir
 	 done
 
 	#if no presistance dirs/files need to set permission here
@@ -59,13 +56,13 @@ else
 			touch /dest/fs/_home_app_/.persistent
     fi
 
-	touch /dest/fs/.persistent_lock
-	touch /dest/fs/.persistent
+	#touch /dest/fs/.persistent_lock
+	#touch /dest/fs/.persistent
 	
-fi
+#fi
 
-   chown engines /home/fs/
-   chown engines /dest/fs/
+   #chown 21000 /home/fs/
+ #  chown 21000 /dest/fs/
 touch /client/state/flags/volume_setup_complete
 
  exit 0
