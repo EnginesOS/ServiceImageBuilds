c5f419682284939cf1a464f2e2b52395090cedd6
James L Vodanovich <james@jvodan.com>
2016-03-17 10:32:48 +1100

    connect to mgmt and not host

--- images/03.serviceImages/backup/home/run_docker_backup.sh
+++ images/03.serviceImages/backup/home/run_docker_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $1 | ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/.ssh/run_backup_on_engine engines@engines.internal  /opt/engines/scripts/run_backup_on_engine.sh
+echo $1 | ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/.ssh/run_backup_on_engine engines@mgmt.engines.internal  /opt/engines/scripts/run_backup_on_engine.sh
