63156daa70d0bbe6f027fc7760382485c62d9dcc
James L Vodanovich <james@jvodan.com>
2016-04-06 13:43:18 +1000

    correct typos

--- images/03.serviceImages/email/home/configurators/read_anti_spam_rbls.sh
+++ images/03.serviceImages/email/home/configurators/read_anti_spam_rbls.sh
@@ -13,29 +13,29 @@
 #,check_policy_service inet:127.0.0.1:60000,permit
 config_dir=/home/configurators/saved/antispam/
 
-if test -f ${config_dir)/zen.spamhaus.org
+if test -f ${config_dir}/zen.spamhaus.org
  then
-   ${zen_spamhaus_org} = true
+   zen_spamhaus_org=true
  fi
- if test -f ${config_dir)/bl.spamcop.net
+ if test -f ${config_dir}/bl.spamcop.net
  then
-   ${bl_spamcop_net} = true
+   bl_spamcop_net=true
  fi
- if test -f ${config_dir)/cbl.abuseat.org
+ if test -f ${config_dir}/cbl.abuseat.org
  then
-   ${cbl_abuseat_org} = true
+   cbl_abuseat_org=true
  fi
- if test -f ${config_dir)/dnsbl.njabl.org
+ if test -f ${config_dir}/dnsbl.njabl.org
  then
-   ${dnsbl_njabl_org} = true
+   dnsbl_njabl_org=true
  fi
- if test -f ${config_dir)/dnsbl.sorbs.net
+ if test -f ${config_dir}/dnsbl.sorbs.net
  then
-   ${dnsbl_sorbs_net} = true
+   dnsbl_sorbs_net=true
  fi
- if test -f ${config_dir)/dsn.rfc-ignorant.org
+ if test -f ${config_dir}/dsn.rfc-ignorant.org
  then
-   ${dsn_rfc-ignorant_org} = true
+   dsn_rfc-ignorant_org=true
  fi
  
  echo "{\"zen_spamhaus_org\":\"${zen_spamhaus_org}\",\"bl_spamcop_net\":\"${bl_spamcop_net}\",\"cbl_abuseat_org\":\"${cbl_abuseat_org}\",\"dnsbl_njabl_org\":\"${dnsbl_njabl_org}\",\"dnsbl_sorbs_net\":\"${dnsbl_sorbs_net}\",\"dsn_rfc-ignorant_org\":\"${dsn_rfc-ignorant_org}\"}"
