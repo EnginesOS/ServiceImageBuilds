8ad0d69875b8e3f20b9777c00121626db6b38389
James L Vodanovich <james@jvodan.com>
2016-04-07 12:02:32 +1000

    grant_ugo_rw_access.sh

--- images/01.baseImages/03.Framework_base/home/engines/scripts/_grant_ugo_rw_access.sh
+++ images/01.baseImages/03.Framework_base/home/engines/scripts/_grant_ugo_rw_access.sh
@@ -10,6 +10,6 @@
 fi
 path=`echo $entered_path |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
 #FIXME needs to handle target of symbolic link
-echo chmod go-w $recursive /home/$path
- chmod go-w $recursive /home/$path
+echo chmod go+w $recursive /home/$path
+ chmod go+w $recursive /home/$path
  
