d58c9a88e9b51f48d992b8ae4aeafb0836c0cafd
James L Vodanovich <james@jvodan.com>
2016-04-11 15:16:44 +1000

    log viewer

---
+++ images/03.serviceImages/log_viewer/home/start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PID_FILE=/var/run/apache.pid
+
+export PID_FILE
+. /home/trap.sh
+
+mkdir -p /engines/var/run/flags/
+
+/usr/sbin/apache2ctl  -DFOREGROUND &
+
+touch /engines/var/run/flags/startup_complete  
+wait 
+rm -f /engines/var/run/flags/startup_complete
+
+
+ 
+ 
+
