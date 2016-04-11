bba687d867105f9c9f75c506858ad7da809fd22e
James L Vodanovich <james@jvodan.com>
2016-04-12 08:40:42 +1000

    create apache pid dir

--- images/03.serviceImages/log_viewer/home/start.sh
+++ images/03.serviceImages/log_viewer/home/start.sh
@@ -5,6 +5,7 @@ PID_FILE=/var/run/apache.pid
 export PID_FILE
 . /home/trap.sh
 mkdir -p /var/log/apache2
+mkdir -p /var/run/apache2/
 mkdir -p /engines/var/run/flags/
 
 /usr/sbin/apache2ctl  -DFOREGROUND &
