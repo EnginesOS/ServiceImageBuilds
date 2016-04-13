90a65a05e3d782dd0da532f8927e4a798932f748
James L Vodanovich <james@jvodan.com>
2016-04-13 14:36:48 +1000

    fix

--- images/03.serviceImages/log_viewer/home/add_service.sh
+++ images/03.serviceImages/log_viewer/home/add_service.sh
@@ -6,7 +6,7 @@ service_hash=$1
  echo $service_hash | /home/engines/bin/json_to_env >/tmp/.env
  . /tmp/.env
 
-if ! test -d 
+if ! test -d /home/saved/$parent_engine/
  then
  	mkdir -p /home/saved/$parent_engine/
  fi
