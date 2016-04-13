d8578e7a11fac1d4c02f019401ac5b2def7ade0b
James L Vodanovich <james@jvodan.com>
2016-04-13 19:07:15 +1000

    create path prefix from type and correct regex matches for apache

--- images/03.serviceImages/log_viewer/home/add_service.sh
+++ images/03.serviceImages/log_viewer/home/add_service.sh
@@ -11,6 +11,29 @@ if ! test -d /home/saved/$parent_engine/
  	mkdir -p /home/saved/$parent_engine/
  fi
  
+ case $log_type in
+ nginx)
+ /var/log/engines/services/nginx/$parent_engine/$log_file_path
+ ;;
+ 
+ apache)
+ if test -z $ctype
+  then
+   ctype=container
+  fi
+ log_file_path=/var/log/engines/${ctype}s/$parent_engine/$log_file_path
+ ;;
+ 
+ raw)
+ if test -z $ctype
+  then
+   ctype=container
+  fi
+ log_file_path=/var/log/engines/${ctype}s/$parent_engine/$log_file_path
+ ;;
+ 
+ esac
+ 
  conf=/home/saved/$parent_engine/$log_name
 
 echo  \"$parent_engine_$log_name\": { \"display\" : \"$parent_engine $log_name\", \"path\"    : \"/var/log/engines/$log_file_path\",  > /tmp/.conf
