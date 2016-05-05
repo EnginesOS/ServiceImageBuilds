03211c0285c711eb5217db8afa6626c5b216385c
James L Vodanovich <james@jvodan.com>
2016-05-05 15:29:44 +1000

    dbg missing hostname error when on providing domain_name

--- images/03.serviceImages/dns/home/rm_service.sh
+++ images/03.serviceImages/dns/home/rm_service.sh
@@ -10,7 +10,7 @@ service_hash=$1
 	if ! test -z ${domain_name}
 	 then	
 	 	rm /home/bind/engines/domains/${domain_name}
-	  rm  /home/bind/domain_list/${ip_type}/${domain_name}
+	    rm  /home/bind/domain_list/${ip_type}/${domain_name}
 	 	rm /home/bind/engines/zones/named.conf.${domain_name}
 	 	cat /home/bind/engines/domains/* > /home/bind/engines/domains.hosted
 	 	kill -HUP `cat /var/run/named/named.pid`
@@ -19,7 +19,7 @@ service_hash=$1
 
 	if test -z ${hostname}
 	then
-		echo Error:Missing hostname
+		echo Error:Missing hostname ${domain_name}
         exit -1
     fi
 
