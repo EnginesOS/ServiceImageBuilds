53260cc07e240a7b98b0ab79e372809c3ada1cbe
James L Vodanovich <james@jvodan.com>
2016-05-17 15:33:58 +1000

    yard

--- images/03.serviceImages/system/home/start.sh
+++ images/03.serviceImages/system/home/start.sh
@@ -10,12 +10,14 @@ mkdir -p /engines/var/run/flags/
 
 cd /home/
 
-
+cd /opt/engines/
+ yard server &
+ yard_pid=$!
 /usr/local/rbenv/versions/2.3.0/bin/thin    -C config.yaml -R ./config.ru start > /var/log/system.log
 
 
 touch /engines/var/run/flags/startup_complete  
 wait 
-kill -TERM  $pid
+kill -TERM  $pid $yard_pid
 rm -f /engines/var/run/flags/startup_complete
 
