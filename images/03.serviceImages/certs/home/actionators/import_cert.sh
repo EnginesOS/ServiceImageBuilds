7c4332d26d36cc1c044efa025b7d88f8035c5545
James L Vodanovich <james@jvodan.com>
2016-06-09 18:37:53 +1000

    certs remove

--- images/03.serviceImages/certs/home/actionators/import_cert.sh
+++ images/03.serviceImages/certs/home/actionators/import_cert.sh
@@ -9,7 +9,31 @@ if test $1 = "default"
     domain_name=$1
  default=0
  fi
+ rm /home/certs/store/public/certs/$domain_name.crt
+ rm /home/certs/store/public/keys/$domain_name.key
  
-cat - > /home/certs/$domain_name.crt
+ while read line; do
+  echo "reading: ${line}"
+  echo ${line} |grep  "BEGIN CERTIFICATE"
+  	if test $? -eq 0
+  		then
+  			file=/home/certs/store/public/certs/$domain_name.crt
+  		fi
+  		 echo ${line} |grep "BEGIN RSA PRIVATE KEY"
+  		if test $? -eq 0
+  		then
+  			file=/home/certs/store/public/keys/$domain_name.key
+  		fi 
+  		
+  		echo ${line} >> $file
+  		
+  		 echo ${line} |grep "END RSA PRIVATE KEY"  		
+  		if test $? -eq 0
+  		then
+  		echo true
+  			exit
+  		fi 
+  		
+done < /dev/stdin
 
- 	
+ 
---
+++ images/03.serviceImages/certs/home/actionators/remove_cert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if test -f /home/certs/store/public/certs/$1.crt
+ then
+ 	rm /home/certs/store/public/certs/$1.crt
+ else
+ 	echo "Not Such Cert $1.crt"
+ 	exit -1
+ 	fi
+
+ 	
+ 	if test -f /home/certs/store/public/keys/$1.key
+ then
+ 	rm /home/certs/store/public/keys/$1.key
+ else
+ 	echo "No Such key $1.key"
+ 	exit -1
+ 	fi
+ 	echo true
+ 	exit
+ 	
