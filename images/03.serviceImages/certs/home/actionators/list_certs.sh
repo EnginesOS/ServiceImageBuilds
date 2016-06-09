b56e36426ab11f80955ef942f75a386240cc3614
James L Vodanovich <james@jvodan.com>
2016-06-09 12:22:24 +1000

    return cert list as json

--- images/03.serviceImages/certs/home/actionators/list_certs.sh
+++ images/03.serviceImages/certs/home/actionators/list_certs.sh
@@ -3,6 +3,18 @@
 if test -d /home/certs/store/public/certs/
  then
  cd /home/certs/store/public/certs/
- ls *.crt |sed "/\.crt/s///g"
+ certs=`ls *.crt |sed "/\.crt/s///g"`
+ echo '{"certs":['
+ i=0
+ 	for cert in $certs
+ 		do
+ 			if test $i -eq 0
+ 				i=1
+ 			else
+ 				echo ,
+ 			fi
+ 			echo '"'$cert'"'
+ 		done
+ 	echo ']}'
  fi
   
