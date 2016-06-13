2b05a5ad771b9ac6ac9b172afdf7aa1518ed47ca
James L Vodanovich <james@jvodan.com>
2016-06-13 10:05:32 +1000

    use wait not loop

--- images/03.serviceImages/mysql/home/start.sh
+++ images/03.serviceImages/mysql/home/start.sh
@@ -18,14 +18,14 @@ SIGNAL=0
 
  /usr/sbin/mysqld --defaults-file=/etc/mysql/my.cnf --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql --log-error=/var/log/mysql/error.log --pid-file=/var/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock --port=3306 &
 touch  /engines/var/run/flags/startup_complete
-
-
- while test $SIGNAL -ne 3 -a $SIGNAL -ne 15
- do
-  if test -f $PID_FILE
-  	then	
-  	sleep 600 &
-		wait 	
-  fi
- done
+wait
+#
+#while test $SIGNAL -ne 3 -a $SIGNAL -ne 15
+#do
+# if test -f $PID_FILE
+# 	then	
+# 	sleep 600 &
+#		wait 	
+# fi
+#done
  rm /engines/var/run/flags/startup_complete
