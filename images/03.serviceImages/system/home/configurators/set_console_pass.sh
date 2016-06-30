ef7851f7cfde560185d190aa6440524e9010cd7c
James L Vodanovich <james@jvodan.com>
2016-06-30 11:42:39 +1000

    tidy

--- images/03.serviceImages/system/home/configurators/set_console_pass.sh
+++ images/03.serviceImages/system/home/configurators/set_console_pass.sh
@@ -10,7 +10,7 @@ service_hash=$1
   
    	if test -n $console_password
 	then  
-		mgmt_ip=`cat /opt/engines/etc/net/management`
+		mgmt_ip=$CONTROL_IP
 	ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/update_engines_console_password engines@${mgmt_ip} /opt/engines/bin/update_engines_console_password.sh $console_password
  		exit $?	
  	fi
