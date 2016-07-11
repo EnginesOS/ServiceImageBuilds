d4ce3ca5181374046acccb82f88f968cb23ec8ce
James L Vodanovich <james@jvodan.com>
2016-07-11 10:39:47 +1000

    first run wizard

---
+++ images/04.systemApps/firstrun/home/deployment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /home/app
+#release=$RELEASE
+release=master
+git fetch origin $release
+git reset --hard FETCH_HEAD
+git pull --depth 1 origin  $release
+
+if ! test -f /home/app/Gemfile
+ then
+   cp /tmp/gf /home/app/Gemfile
+ else
+	cat /home/app/Gemfile |grep -v rubyracer >/tmp/gf
+	cp /tmp/gf  /home/app/Gemfile
+fi 
+
+cp /home/newrelic.yml /home/app/
+
+
+RAILS_ENV=production
+
+echo installing Gems
+/usr/local/rbenv/shims/bundle install --standalone 
+
