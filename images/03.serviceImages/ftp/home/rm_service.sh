b4b51ff03c21bb2ae15be9bfe179cd7c8c847037
James L Vodanovich <james@jvodan.com>
2016-09-19 17:53:38 +1000

    hash pass via stdio

--- images/03.serviceImages/ftp/home/rm_service.sh
+++ images/03.serviceImages/ftp/home/rm_service.sh
@@ -2,5 +2,5 @@
 
 service_hash=$1
 
- ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i /home/ftpd/.ssh/rm_rsa auth@auth.engines.internal /home/auth/scripts/ftp/rm_service.sh \'$service_hash\'
+echo \'$service_hash\'| ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i /home/ftpd/.ssh/rm_rsa auth@auth.engines.internal /home/auth/scripts/ftp/rm_service.sh 
  
