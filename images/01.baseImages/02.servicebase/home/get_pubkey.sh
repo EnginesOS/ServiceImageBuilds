bc869c8c1fcda08ad376efd8b4768bc97f10ab8a
James L Vodanovich <james@jvodan.com>
2016-09-19 12:19:11 +1000

    echo -n key

--- images/01.baseImages/02.servicebase/home/get_pubkey.sh
+++ images/01.baseImages/02.servicebase/home/get_pubkey.sh
@@ -2,10 +2,12 @@
 
 	if test -f ~/.ssh/id_rsa.pub
 		then
-	 		cat ~/.ssh/id_rsa.pub | awk '{print $2}'	
+	 		key=`cat ~/.ssh/id_rsa.pub | awk '{print $2}'`
+	 		echo -n $key	
  		else 				
  	 		ssh-keygen  -f ~/.ssh/id_rsa -P "" > /dev/null 	 		
- 	 		cat ~/.ssh/id_rsa.pub | awk '{print $2}' 	 	
+ 	 		key=`cat ~/.ssh/id_rsa.pub | awk '{print $2}'`
+ 	 		echo -n $key 	
  	fi
 
  
