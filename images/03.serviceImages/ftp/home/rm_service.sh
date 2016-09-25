2e8c4030fc12960929ed9ea4d5325023fc83ad71
James L Vodanovich <james@jvodan.com>
2016-09-25 12:43:43 +1000

    f

--- images/03.serviceImages/ftp/home/rm_service.sh
+++ images/03.serviceImages/ftp/home/rm_service.sh
@@ -2,5 +2,5 @@
 
 service_hash=$1
 
-echo \'$service_hash\'| ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i /home/ftpd/.ssh/rm_rsa auth@auth.engines.internal /home/auth/scripts/ftp/rm_service.sh 
+echo $service_hash| ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i /home/ftpd/.ssh/rm_rsa auth@auth.engines.internal /home/auth/scripts/ftp/rm_service.sh 
  
