4e781ee131b10f0850ac96dee91d3ceac09dc667
James L Vodanovich <james@jvodan.com>
2016-09-25 13:41:45 +1000

    reset password if recreated

--- images/03.serviceImages/auth/home/add_service.sh
+++ images/03.serviceImages/auth/home/add_service.sh
@@ -43,10 +43,11 @@ if  test $command = "access"
 		echo "command=\"/home/auth/static/scripts/$service/get_access.sh\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ssh-rsa $pubkey auth" >  /home/auth/keys/${service}_${command}_authorized_keys	
 	
 		pass=`dd if=/dev/urandom count=6 bs=1  | od -h | awk '{ print $2$3$4}'`
-		echo "
-			create user 'auth_$service'@'%' identified by '$pass';
-			GRANT SELECT on auth.* to 'auth_$service'@'%';" | mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname 
+		echo "create user 'auth_$service'@'%' identified by '$pass';;" | mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname 
+		echo "SET PASSWORD FOR 'auth_ftp'@'%' = password('$pass');" | mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname 
+		echo "GRANT SELECT on auth.* to 'auth_$service'@'%';" | mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname 
 		echo "create user 'auth_$service'@'%' identified by '$pass';
+			SET PASSWORD FOR 'auth_ftp'@'%' = password('$pass');
 			GRANT SELECT on auth.* to 'auth_$service'@'%'; | mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname " >>/tmp/add_access.log
 		#echo ":db_username=auth_$service:db_password=$pass:database_name=$dbname:db_host=$dbhost:" > /home/auth/static/access/$service/access
 		echo '{"db_username":"auth_'$service'","db_password":"'$pass'","database_name":"'$dbname'","db_host":"'$dbhost'"}' > /home/auth/static/access/$service/access
