7d56b7e443c4c1ed2dd9b5a2da66326f69874549
James L Vodanovich <james@jvodan.com>
2016-10-12 14:34:38 +1100

    f

--- images/03.serviceImages/mysql/home/start.sh
+++ images/03.serviceImages/mysql/home/start.sh
@@ -14,6 +14,7 @@ if ! test -d  /var/run/mysqld/
  	mkdir -p /var/run/mysqld/
  fi
  
+mkdir -p /var/run/mysqld/
 PID_FILE=/var/run/mysqld/mysqld.pid
 
 export PID_FILE
@@ -25,7 +26,10 @@ SIGNAL=0
  /usr/sbin/mysqld --defaults-file=/etc/mysql/my.cnf --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --user=mysql --log-error=/var/log/mysql/error.log --pid-file=/var/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock --bind-address=0.0.0.0 --port=3306 &
 touch  /engines/var/run/flags/startup_complete
 sleep 1
-wait `cat /var/run/mysqld/mysqld.pid`
+wait 
+ cat /var/log/mysql/error.log
+ 
+#`cat /var/run/mysqld/mysqld.pid`
 #
 #while test $SIGNAL -ne 3 -a $SIGNAL -ne 15
 #do
