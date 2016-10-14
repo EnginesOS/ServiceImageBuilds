72aa2d536159898d96fdb070cb7912c51ca73873
James L Vodanovich <james@jvodan.com>
2016-10-14 18:39:33 +1100

    add public and gateway host names on start

--- images/03.serviceImages/dns/home/add_service.sh
+++ images/03.serviceImages/dns/home/add_service.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-function add_to_internal_domain {
-	if test -z ${hostname}
-	then
-		echo Error:Missing hostname
-        exit 128
-    fi
-    
-    fqdn_str=${hostname}.engines.internal
-    
-  	if test -z ${ip}
-	then
-		 update_line=" update add $fqdn_str 30 CNAME ${parent_engine}.engines.internal"
-       else
-       update_line=" update add $fqdn_str 30 A $ip"        
-    fi  
-    
-	echo server 127.0.0.1 > /tmp/.dns_cmd
-	echo update delete $fqdn_str >> /tmp/.dns_cmd
-	echo send >> /tmp/.dns_cmd
-	echo $update_line >> /tmp/.dns_cmd
-	#echo update add $fqdn_str 30 A $ip >> /tmp/.dns_cmd
-	echo send >> /tmp/.dns_cmd
-	nsupdate -k /etc/bind/keys/ddns.private /tmp/.dns_cmd
-}
+. /home/dns_functions.sh
 
 if test $# -eq 0 
  then
--- images/03.serviceImages/dns/home/start.sh
+++ images/03.serviceImages/dns/home/start.sh
@@ -27,7 +27,15 @@ mkdir -p /engines/var/run/flags/
 sudo -n /home/setup.sh
 
 sudo -n syslogd  -R syslog.engines.internal:514
-sudo -n /usr/sbin/named  -c /etc/bind/named.conf -f -u bind & 
+sudo -n /usr/sbin/named  -c /etc/bind/named.conf -f -u bind &
+
+. /home/dns_functions.sh
+${hostname}=gateway
+$ip=`cat  /opt/engines/etc/net/ip`
+add_to_internal_domain
+$ip=`cat  /opt/engines/etc/net/public`
+${hostname}=public
+add_to_internal_domain
 touch /engines/var/run/flags/startup_complete
 wait  
 
