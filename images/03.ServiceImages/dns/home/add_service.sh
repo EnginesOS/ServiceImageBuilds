8da521c8be7ba49cd649d0b0a55f981eb424538e
James L Vodanovich <james@jvodan.com>
2016-10-28 17:05:35 +1100

    [_.] = -

--- images/03.ServiceImages/dns/home/add_service.sh
+++ images/03.ServiceImages/dns/home/add_service.sh
@@ -17,40 +17,41 @@ if test $ip = null
 	 	echo unset ip
 		unset ip
 	fi
-	
-	if ! test -z $domain_name
-	 then
-	  if ! test $domain_name = engines.internal
-	   then	   
-		  if test -z $ip  
-		   then
-			  	if  test  $ip_type = lan 
-   		         then   		         
-   		    		 	ip=`cat /opt/engines/etc/net/ip`
-   		         elif   test  $ip_type = gw 
-   		          then
-   		 	           ip=`cat /opt/engines/etc/net/public`     		     
-   		 	    fi
-   		 	else
-   		 		ip_type=gw
-   		 	fi
-   		 	
-   		 	it ! test -z $ip_type
-   		 	   then 		    	
-	   				touch /home/bind/domain_list/${ip_type}/${domain_name}
-	 				cat  /etc/bind/templates/config_file_zone_entry.tmpl | sed " /DOMAIN/s//${domain_name}/g" > /home/bind/engines/domains/${domain_name}
-	 				cat /etc/bind/templates/selfhosted.tmpl | sed "/DOMAIN/s//${domain_name}/g" | sed "/IP/s//${ip}/g" > /home/bind/engines/zones/named.conf.${domain_name}
-	 				cat /home/bind/engines/domains/* > /home/bind/engines/domains.hosted
-	 				kill -HUP `cat /var/run/named/named.pid`
-	 		
-				fi		   
-	    hostname=`echo $domain_name | sed "/\./s//_/g"`
-	 	domain_name=engines.internal
-	 	add_to_internal_domain
-	 	echo Success
-	 	exit 0
-	   fi
-	 fi
+
+if ! test -z $domain_name
+         then
+          if ! test $domain_name = engines.internal
+           then
+                  if test -z $ip
+                   then
+                                if  test  $ip_type = lan
+                                 then
+                                        ip=`cat /opt/engines/etc/net/ip`
+                                elif   test  $ip_type = gw
+                                then
+                                   ip=`cat /opt/engines/etc/net/public`
+                                fi
+                 fi
+
+                 if ! test -z $ip_type
+                           then
+                        touch /home/bind/domain_list/${ip_type}/${domain_name}
+                        cat  /etc/bind/templates/config_file_zone_entry.tmpl | sed " /DOMAIN/s//${domain_name}/g" > /home/bind/engines/d
+omains/${domain_name}
+                        cat /etc/bind/templates/selfhosted.tmpl | sed "/DOMAIN/s//${domain_name}/g" | sed "/IP/s//${ip}/g" > /home/bind/
+engines/zones/named.conf.${domain_name}
+                        cat /home/bind/engines/domains/* > /home/bind/engines/domains.hosted
+                        kill -HUP `cat /var/run/named/named.pid`
+                fi
+
+                hostname=`echo $domain_name | sed "/[_.]/s//-/g"`
+                domain_name=engines.internal
+echo add $hostname
+                add_to_internal_domain
+                echo Success
+                exit 0
+          fi
+fi
 
 #FIXME make engines.internal settable
 
