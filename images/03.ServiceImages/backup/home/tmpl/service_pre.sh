542ac63f9633bb93c4c58d8d4ecd95efd5aa6b10
James L Vodanovich <james@jvodan.com>
2016-11-29 17:38:36 +1100

    service backup via itself

--- images/03.ServiceImages/backup/home/tmpl/service_pre.sh
+++ images/03.ServiceImages/backup/home/tmpl/service_pre.sh
@@ -3,12 +3,7 @@
 service=$PROFILE
  
 ts=`date +%d_%m_%y`
+curl http://172.17.0.1:2380/v0/backup/service/$service > /tmp/backup_$service/backup.${ts}.gz
 
-echo $service |\
- ssh  -o UserKnownHostsFile=/dev/null\
-      -o StrictHostKeyChecking=no\
-       -i /home/.ssh/run_backup_on_service\
-        engines@mgmt.engines.internal\
-          /opt/engines/system/scripts/backup/run_backup_on_service.sh > /tmp/backup_$service/backup.${ts}.gz
 
       
