473f695ea97aa6266b23a010830264394935cb5c
James L Vodanovich <james@jvodan.com>
2016-12-15 09:52:44 +1100

    -threaded

--- images/04.SystemServices/system/home/start.sh
+++ images/04.SystemServices/system/home/start.sh
@@ -18,9 +18,9 @@ if ! test -d  /engines/var/run/flags/
 cd /home
  if test -f /opt/engines/etc/ssl/keys/system/server.key 
   then
-	thin  --threaded --ssl --ssl-key-file /opt/engines/etc/ssl/keys/system/server.key --ssl-cert-file /opt/engines/etc/ssl/certs/system/server.crt -C config.yaml -R ./config.ru start > /var/log/system.log &
+	thin  --ssl --ssl-key-file /opt/engines/etc/ssl/keys/system/server.key --ssl-cert-file /opt/engines/etc/ssl/certs/system/server.crt -C config.yaml -R ./config.ru start > /var/log/system.log &
   else
-	thin  --threaded -C config.yaml -R ./config.ru start > /var/log/system.log &
+	thin   -C config.yaml -R ./config.ru start > /var/log/system.log &
  fi
 #--threaded  
 
