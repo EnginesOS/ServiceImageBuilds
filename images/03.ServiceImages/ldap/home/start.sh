e1e9a6d47ee001a0c80b5ec0141d47557560ee5f
James L Vodanovich <james@jvodan.com>
2017-03-24 13:15:37 +1100

    set start to debug wait

--- images/03.ServiceImages/ldap/home/start.sh
+++ images/03.ServiceImages/ldap/home/start.sh
@@ -2,12 +2,10 @@
 
 if ! test -f /engines/var/run/flags/first_run
   then
-  	sudo /home/fix_permissions.sh
-  	echo Fixed perms
+
   	touch /engines/var/run/flags/first_run
   fi
 
-PID_FILE=/var/spool/postfix/pid/master.pid
 
 export PID_FILE
 . /home/trap.sh
@@ -18,27 +16,11 @@ sudo -n /home/engines/scripts/_start_syslog.sh
 
 echo started syslog
 
-if ! test -f /etc/postfix/transport 
-	then
-	 echo "	*	smtp:" >/etc/postfix/transport
-	fi 
-if ! test -f /etc/postfix/mailname
-	then
-		echo "not.set" > /etc/postfix/mailname
-	fi
-		
-sudo -n /usr/sbin/postmap /etc/postfix/transport
 
-sudo -n /usr/lib/postfix/sbin/master -w &
-dummy=$!
-echo started master
 touch  /engines/var/run/flags/startup_complete
 
-sleep 6
-while test -f  /var/spool/postfix/pid/master.pid
- do
- 	sleep 10
- done
+sleep 3600
+
 rm /engines/var/run/flags/startup_complete  
 sudo -n /home/engines/scripts/_kill_syslog.sh
 
