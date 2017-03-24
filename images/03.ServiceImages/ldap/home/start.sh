9660f7fcbc027e562fd293d6ac5ccde952a89a54
James L Vodanovich <james@jvodan.com>
2017-03-24 12:41:32 +1100

    first cut of ldap

---
+++ images/03.ServiceImages/ldap/home/start.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if ! test -f /engines/var/run/flags/first_run
+  then
+  	sudo /home/fix_permissions.sh
+  	echo Fixed perms
+  	touch /engines/var/run/flags/first_run
+  fi
+
+PID_FILE=/var/spool/postfix/pid/master.pid
+
+export PID_FILE
+. /home/trap.sh
+
+mkdir -p /engines/var/run/flags/
+
+sudo -n /home/engines/scripts/_start_syslog.sh
+
+echo started syslog
+
+if ! test -f /etc/postfix/transport 
+	then
+	 echo "	*	smtp:" >/etc/postfix/transport
+	fi 
+if ! test -f /etc/postfix/mailname
+	then
+		echo "not.set" > /etc/postfix/mailname
+	fi
+		
+sudo -n /usr/sbin/postmap /etc/postfix/transport
+
+sudo -n /usr/lib/postfix/sbin/master -w &
+dummy=$!
+echo started master
+touch  /engines/var/run/flags/startup_complete
+
+sleep 6
+while test -f  /var/spool/postfix/pid/master.pid
+ do
+ 	sleep 10
+ done
+rm /engines/var/run/flags/startup_complete  
+sudo -n /home/engines/scripts/_kill_syslog.sh
+
