071789d02120bb04603679c2adef95f7cd705f65
James L Vodanovich <james@jvodan.com>
2017-04-11 10:26:28 +1000

    add wait in startup for testing

--- images/03.ServiceImages/ipsec/home/start.sh
+++ images/03.ServiceImages/ipsec/home/start.sh
@@ -11,8 +11,9 @@ mkdir -p /engines/var/run/flags/
 
 sudo -n /home/engines/scripts/_start_syslog.sh
 
-/home/cron/sbin/fcron -f -p  /home/cron/log/cron.log &
+
 touch /engines/var/run/flags/startup_complete  
 wait 
+sleep 5000
 sudo -n  /home/engines/scripts/_kill_syslog.sh
 rm -f /engines/var/run/flags/startup_complete
