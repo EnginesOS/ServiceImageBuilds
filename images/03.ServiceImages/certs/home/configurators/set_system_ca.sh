1e4ed365c0f1f015116738534cbb5bfa07c5527c
James L Vodanovich <james@jvodan.com>
2017-04-12 10:08:18 +1000

    add support for altname

--- images/03.ServiceImages/certs/home/configurators/set_system_ca.sh
+++ images/03.ServiceImages/certs/home/configurators/set_system_ca.sh
@@ -33,9 +33,15 @@ mkdir -p /home/certs/store/public/ca/keys/
 mkdir -p /home/certs/store/public/ca/certs
 mkdir -p /home/certs/store/private/ca/keys/
 chmod og-rwx  /home/certs/store/private/ca/keys/
-
+ if ! test $altName
+  then
+  	altName=DNS:$domainname
+  else
+  	altName=DNS:$altName
+  fi
+  
 
 openssl genrsa -out /home/certs/store/private/ca/keys/system_CA.key 2048
-openssl req -x509 -new -nodes -key /home/certs/store/private/ca/keys/system_CA.key -days 1024 -out /home/certs/store/public/ca/certs/system_CA.pem < /home/configurators/saved/ca_setup
+openssl req -x509  -extensions v3_req -new -nodes -key /home/certs/store/private/ca/keys/system_CA.key -days 1024 -out /home/certs/store/public/ca/certs/system_CA.pem < /home/configurators/saved/ca_setup
         
         
