66139c4e63d3d137d7f9325190de3ebb0f36b86a
James L Vodanovich <james@jvodan.com>
2017-04-29 17:36:36 +1000

    _start via sudoers

---
+++ images/03.ServiceImages/ipsec/home/_start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+iptables -t nat -I POSTROUTING -s 10.1.1.0/24 -o eth+ -j MASQUERADE
+iptables -t nat -I POSTROUTING -s  10.1.1.0/24 -o eth+ -m policy --dir out --pol none -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
+
+sudo /usr/sbin/ipsec start 
--- images/03.ServiceImages/ipsec/home/start.sh
+++ images/03.ServiceImages/ipsec/home/start.sh
@@ -16,9 +16,8 @@ sudo -n /home/engines/scripts/_start_syslog.sh
 
 
 touch /engines/var/run/flags/startup_complete  
-sudo /usr/sbin/ipsec start --nofork &
-iptables -t nat -I POSTROUTING -s 10.1.1.0/24 -o eth+ -j MASQUERADE
-iptables -t nat -I POSTROUTING -s  10.1.1.0/24 -o eth+ -m policy --dir out --pol none -j MASQUERADE
+
+sudo -n /home/_start.sh &
 
 wait 
 sleep 500
