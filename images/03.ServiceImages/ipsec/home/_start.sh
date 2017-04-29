7f4928526603eaaa6dd8137c31b35edbe177c314
James L Vodanovich <james@jvodan.com>
2017-04-29 18:21:55 +1000

    typo

--- images/03.ServiceImages/ipsec/home/_start.sh
+++ images/03.ServiceImages/ipsec/home/_start.sh
@@ -4,5 +4,5 @@ iptables -t nat -I POSTROUTING -s 10.1.1.0/24 -o eth+ -j MASQUERADE
 iptables -t nat -I POSTROUTING -s  10.1.1.0/24 -o eth+ -m policy --dir out --pol none -j MASQUERADE
 sysctl -w net.ipv4.ip_forward=1
 
-sudo /usr/sbin/ipsec start -no fork &
+sudo /usr/sbin/ipsec start --nofork &
 wait
