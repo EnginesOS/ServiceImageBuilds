30c0d63ef1f351a75da736833cf6d4bcb09c419e
James L Vodanovich <james@jvodan.com>
2017-04-29 13:31:51 +1000

    iptables and dns config

--- images/03.ServiceImages/ipsec/home/start.sh
+++ images/03.ServiceImages/ipsec/home/start.sh
@@ -17,6 +17,9 @@ sudo -n /home/engines/scripts/_start_syslog.sh
 
 touch /engines/var/run/flags/startup_complete  
 sudo /usr/sbin/ipsec start --nofork &
+iptables -t nat -I POSTROUTING -s 10.1.1.0/24 -o eth+ -j MASQUERADE
+iptables -t nat -I POSTROUTING -s  10.1.1.0/24 -o eth+ -m policy --dir out --pol none -j MASQUERADE
+
 wait 
 sleep 500
 sudo -n  /home/engines/scripts/_kill_syslog.sh
