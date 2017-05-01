9c8c45ac7ee0355bddb883dc0c530420af71dadc
James L Vodanovich <james@jvodan.com>
2017-05-01 17:55:25 +1000

    try new ipsec ruls

--- images/03.ServiceImages/ipsec/home/_start.sh
+++ images/03.ServiceImages/ipsec/home/_start.sh
@@ -3,9 +3,12 @@
 
 iptables -t nat -I POSTROUTING -s  10.1.1.0/24 -o eth+ -m policy --dir out --pol ipsec -j ACCEPT
 iptables -t nat -I POSTROUTING -s 10.1.1.0/24 -o eth+ -j MASQUERADE
-iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.1.1.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.1.1.0/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+#below is new
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -I INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j MASQUERADE
+#above was accept no masq
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv4.conf.all.accept_redirects = 0
 sysctl -w net.ipv4.conf.all.send_redirects = 0
