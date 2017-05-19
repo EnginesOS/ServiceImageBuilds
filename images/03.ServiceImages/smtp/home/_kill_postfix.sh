16cd2435178bd0bc760a4c7dd39f87377ff43469
James L Vodanovich <james@jvodan.com>
2017-05-20 09:52:25 +1000

    kill syslog

--- images/03.ServiceImages/smtp/home/_kill_postfix.sh
+++ images/03.ServiceImages/smtp/home/_kill_postfix.sh
@@ -5,6 +5,6 @@
   then
     kill -$1 `cat /var/spool/postfix/pid/master.pid`
     echo kill -$1 `cat /var/spool/postfix/pid/master.pid`
- /home/engines/scripts/_kill_syslog.sh
+   /home/engines/scripts/_kill_syslog.sh
  fi
  
