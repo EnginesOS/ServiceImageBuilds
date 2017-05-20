5d7e0e423179e1caa29db4fe043288926d26534a
James L Vodanovich <james@jvodan.com>
2017-05-20 18:06:20 +1000

    correct paths

--- images/03.ServiceImages/avahi/home/configurators/read_default_mdns_domain.sh
+++ images/03.ServiceImages/avahi/home/configurators/read_default_mdns_domain.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if test -f /home/configurators/saved/default_domain
+if test -f /home/configurators/saved/default_mdns_domain
 	then
-		cat /home/configurators/saved/default_domain
+		cat /home/configurators/saved/default_mdns_domain
 	else
 	echo '{"default_mdns_domain":"Not Set"}'
 
--- images/03.ServiceImages/avahi/home/configurators/set_default_mdns_domain.sh
+++ images/03.ServiceImages/avahi/home/configurators/set_default_mdns_domain.sh
@@ -2,7 +2,7 @@
 
 
 
-cat - >/home/configurators/saved/default_domain
+cat - >/home/configurators/saved/default_mdns_domain
 
  
 exit 0
