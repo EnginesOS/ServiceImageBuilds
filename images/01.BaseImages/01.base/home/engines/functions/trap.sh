d9435a7c85560caeb3f743f70ba0f3ad8ebf9321
James L Vodanovich <james@jvodan.com>
2017-06-17 12:24:21 +1000

    tidy

--- images/01.BaseImages/01.base/home/engines/functions/trap.sh
+++ images/01.BaseImages/01.base/home/engines/functions/trap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-clear_stale()
- {
+clear_stale_flags()
+{
  for flag in "sig_term termed sig_hup huped sig_quit quited"
  do
    if test -f /engines/var/run/flags/$flag
@@ -12,31 +12,31 @@ clear_stale()
 } 
 
 custom_stop()
-	{
-	if test -f /home/engines/scripts/custom_stop.sh
-	 then
-	   /home/engines/scripts/custom_stop.sh
-	fi
+{
+ if test -f /home/engines/scripts/custom_stop.sh
+  then
+   /home/engines/scripts/custom_stop.sh
+ fi
 }
 
 trap_term()
-	{
-	SIGNAL=15
-	export SIGNAL
-	touch /engines/var/run/flags/sig_term
-	custom_stop
+{
+SIGNAL=15
+export SIGNAL
+touch /engines/var/run/flags/sig_term
+custom_stop
 	
-	if ! test -z $KILL_SCRIPT
-	 then
+ if ! test -z $KILL_SCRIPT
+   then
 	  $KILL_SCRIPT $SIGNAL
 	fi
 		
-	if test -f $PID_FILE  #if exists 
+ if test -f $PID_FILE  #if exists 
+  then
+    if test -f /home/_signal.sh
 	 then
-	  if test -f /home/_signal.sh
-	   then
 		sudo -n /home/_signal.sh $SIGNAL	`cat $PID_FILE`
-	   else
+	 else
 		kill -$SIGNAL `cat    $PID_FILE `	
 		pid=`cat    $PID_FILE `				
 		echo $pid |grep ^[0-9]
@@ -48,74 +48,84 @@ trap_term()
            wait $pid   >& /dev/null
 		 fi			
 	  fi
-	 touch /engines/var/run/flags/termed	 			
-	fi
+   touch /engines/var/run/flags/termed	 			
+ fi
 }
 	
 trap_hup()
-	{
-	SIGNAL=1
-	export SIGNAL
-	touch /engines/var/run/flags/sig_hup
+{
+SIGNAL=1
+export SIGNAL
+touch /engines/var/run/flags/sig_hup
 	
-	if ! test -z $HUP_SCRIPT
+ if ! test -z $HUP_SCRIPT
+	then
+		  $HUP_SCRIPT $SIGNAL
+ fi
+if test -f $PID_FILE
+	then
+	  if test -f /home/_signal.sh
 		then
-		   $HUP_SCRIPT $SIGNAL
-		fi
-		if test -f $PID_FILE
-			then
-				if test -f /home/_signal.sh
-					then
-						sudo -n /home/_signal.sh $SIGNAL	$PID_FILE
-					else
-						kill -$SIGNAL `cat  $PID_FILE  `	
-				fi
-			 touch /engines/var/run/flags/huped			
-		fi		
-	}
+		  sudo -n /home/_signal.sh $SIGNAL	$PID_FILE
+		else
+		   kill -$SIGNAL `cat  $PID_FILE  `	
+	   fi
+  touch /engines/var/run/flags/huped			
+ fi		
+}
 
 trap_quit()
-	{
-	SIGNAL=15
-	export SIGNAL
-	touch /engines/var/run/flags/sig_quit
-	custom_stop
+{
+SIGNAL=15
+export SIGNAL
+touch /engines/var/run/flags/sig_quit
+custom_stop
 	
-	if ! test -z $KILL_SCRIPT
-		then
-		   $KILL_SCRIPT $SIGNAL
-		fi
-		if test -f $PID_FILE
-			then
-				if test -f /home/_signal.sh
-					then
-						sudo	/home/_signal.sh $SIGNAL	$PID_FILE	
-					else
-						kill -$SIGNAL `cat  $PID_FILE  `
-				        pid=`cat    $PID_FILE `				
-					    echo $pid |grep ^[0-9]
- 	
-							if test $? -ne 0
-        						then
-                					echo no wait for \"$pid\"
-        					else
-                					echo wait \"$pid\"
-                					wait $pid   
-							fi
-				fi				
-			 touch /engines/var/run/flags/quited
-		fi
-	
-	}
+ if ! test -z $KILL_SCRIPT
+	then
+	  $KILL_SCRIPT $SIGNAL
+ fi
+ if test -f $PID_FILE
+	then
+		if test -f /home/_signal.sh
+		  then
+		    sudo	/home/_signal.sh $SIGNAL	$PID_FILE	
+		 else
+		    kill -$SIGNAL `cat  $PID_FILE  `
+			pid=`cat    $PID_FILE `				
+			echo $pid |grep ^[0-9]
+ 	  		  if test $? -ne 0
+        		then
+                  echo no wait for \"$pid\"
+        		else
+                  echo wait \"$pid\"
+                  wait $pid   
+			  fi
+		  fi				
+	touch /engines/var/run/flags/quited
+ fi	
+}
 
-clear_stale	
+clear_stale_flags	
+
 trap trap_term 15 
 trap trap_hup  1
 trap trap_quit 3
-	
-if test -f $PID_FILE
- then
-   echo "Warning stale $PID_FILE"
-   rm -f $PID_FILE 1&>/dev/null
- fi
+
+if ! test -z $PID_FILE
+then
+	if ! test -d `dirname $PID_FILE`
+	 then
+	   mkdir -p `dirname $PID_FILE`
+	fi   	
+    if test -f $PID_FILE
+     then
+      #  echo "Warning stale $PID_FILE"
+      kill -0 `cat $PID_FILE &> /dev/null`
+        if test $? -ne 0
+         then
+           rm -f $PID_FILE &>/dev/null
+        fi
+    fi
+fi
 	 			
