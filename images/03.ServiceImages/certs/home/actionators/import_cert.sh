37e02f2be032e9bbaa8e5b4eb584536a050b6144
James L Vodanovich <james@jvodan.com>
2017-06-30 14:00:42 +1000

    remove old

--- images/03.ServiceImages/certs/home/actionators/import_cert.sh
+++ images/03.ServiceImages/certs/home/actionators/import_cert.sh
@@ -27,26 +27,3 @@ rm /home/certs/store/public/keys/$domain_name.key &>/dev/null
 echo ${key} > /home/certs/store/public/keys/$domain_name.key
 echo ${certificate} > /home/certs/store/public/certs/$domain_name.crt
  
-#while read line; do
-## echo "reading: ${line}"
-# echo ${line} |grep  "BEGIN CERTIFICATE" >/dev/null
-# 	if test $? -eq 0
-# 	 then
-# 		file=/home/certs/store/public/certs/$domain_name.crt
-# 	fi
-# echo ${line} |grep "BEGIN RSA PRIVATE KEY">/dev/null
-# 	if test $? -eq 0
-# 	 then
-# 		file=/home/certs/store/public/keys/$domain_name.key
-# 	fi 
-# 		
-# echo ${line} >> $file	
-# echo ${line} |grep "END RSA PRIVATE KEY"  		>/dev/null
-# 	if test $? -eq 0
-# 	 then
-# 	   echo true
-# 	   exit 0
-# 	fi 
-#done < /dev/stdin
-#
-#
