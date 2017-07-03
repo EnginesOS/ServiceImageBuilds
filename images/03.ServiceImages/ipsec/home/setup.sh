ad57403c1f48f6b9a7e5709f0f7ee62e8d9b7cde
James L Vodanovich <james@jvodan.com>
2017-07-04 09:39:30 +1000

    sep mask from subnet

--- images/03.ServiceImages/ipsec/home/setup.sh
+++ images/03.ServiceImages/ipsec/home/setup.sh
@@ -6,14 +6,21 @@ cp /usr/local/share/ca-certificates/engines_internal_ca.crt  /etc/ipsec.d/cacert
 
 domain=`cat /etc/ssl/certs/ipvpn.crt | openssl x509 -noout -subject  |sed "/^.*CN=/s///"`
 
-if test -f /home/configurators/rw_subnet
+if test -f /home/configurators/saved/rw_subnet
 then
- subnet=`cat /home/configurators/rw_subnet`
+ subnet=`cat /home/configurators/saved/rw_subnet`
 else
- subnet=10.1.1.0/24
+ subnet=10.1.1.0
+fi
+if test -f /home/configurators/saved/rw_mask
+then
+ mask=`cat /home/configurators/saved/rw_mask`
+else
+ mask=24
 fi
 
-cat /home/tmpls/ipsec.conf.tmpl | sed "/COMMON_NAME/s/$domain//"\
- | sed "/RW_SUBNET/s/$subnet//" > /etc/ipsec.conf
+
+cat /home/tmpls/ipsec.conf.tmpl | sed "/COMMON_NAME/s//$domain/"\
+ | sed "/RW_SUBNET/s//$subnet/"   | sed "/RW_MASK/s//$mask/"> /etc/ipsec.conf
 chmod og-rwx /etc/ipsec.d/private/ipvpn.key /etc/ipsec.conf
  
