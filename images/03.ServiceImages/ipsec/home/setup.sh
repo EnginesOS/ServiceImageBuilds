13f63b4d52793bc12f5f44ca866d9dcbe1759f51
James L Vodanovich <james@jvodan.com>
2017-07-03 20:08:35 +1000

    improve

--- images/03.ServiceImages/ipsec/home/setup.sh
+++ images/03.ServiceImages/ipsec/home/setup.sh
@@ -1,7 +1,19 @@
 #/bin/bash
-cp /engines/ssl/public/keys/ipvpn.key /etc/ipsec.d/private/
-cp /engines/ssl/public/certs/ipvpn.crt /etc/ipsec.d/certs/  
+
+cp /etc/ssl/keys/ipvpn.key /etc/ipsec.d/private/
+cp /etc/ssl/certs/ipvpn.crt /etc/ipsec.d/certs/  
 cp /usr/local/share/ca-certificates/engines_internal_ca.crt  /etc/ipsec.d/cacerts/     
 
-chmod og-rwx /etc/ipsec.d/private/ipvpn.key
+domain=`cat /etc/ssl/certs/ipvpn.crt | openssl x509 -noout -subject  |sed "/^.*CN=/s///"`
+
+if test -f /home/configurators/rw_subnet
+then
+ subnet=`cat /home/configurators/rw_subnet`
+else
+ subnet=10.1.1.0/24
+fi
+
+cat /home/tmpls/ipsec.conf.tmpl | sed "/COMMON_NAME/s/$domain//"\
+ | sed "/RW_SUBNET/s/$subnet//" > /etc/ipsec.conf
+chmod og-rwx /etc/ipsec.d/private/ipvpn.key /etc/ipsec.conf
  
