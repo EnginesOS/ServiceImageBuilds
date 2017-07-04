d531c6d37ca9cee8236f3d881aa4a193862fd9bb
James L Vodanovich <james@jvodan.com>
2017-07-04 10:13:15 +1000

    domain_name from cn

--- images/03.ServiceImages/certs/home/actionators/import_cert.sh
+++ images/03.ServiceImages/certs/home/actionators/import_cert.sh
@@ -3,11 +3,11 @@
 . /home/engines/functions/params_to_env.sh
 parms_to_env
 
-if test -z ${domain_name}
- then
-  echo Missig cert_name
-  exit 255
-fi
+#if test -z ${domain_name}
+# then
+#  echo Missing domain_name
+#  exit 255
+#fi
 
 if test -z ${certificate}
  then
@@ -16,7 +16,7 @@ if test -z ${certificate}
 fi
 if test -z ${key}
  then
-  echo Missig key
+  echo Missing key
   exit 255
 fi
 
@@ -25,7 +25,14 @@ mkdir -p /home/certs/store/public/certs/imported /home/certs/store/public/keys/i
 rm /home/certs/store/public/certs/${domain_name}.crt &>/dev/null
  
 rm /home/certs/store/public/keys/${domain_name}.key &>/dev/null
- 
+domain_name=`echo ${certificate} | openssl x509 -noout -subject  |sed "/^.*CN=/s///"`
+
+if test -z ${domain_name}
+ then
+  echo Missing domain_name
+  exit 255
+fi
+
 echo ${key} > /home/certs/store/public/keys/imported/${domain_name}.key
 echo ${certificate} > /home/certs/store/public/certs/imported/${domain_name}.crt
  
