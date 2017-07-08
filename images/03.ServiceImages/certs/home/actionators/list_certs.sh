20359c4a702d6cac3b65a8c8438f9c02e73968f0
James L Vodanovich <james@jvodan.com>
2017-07-08 19:46:38 +1000

    dont search reg for uploaded

--- images/03.ServiceImages/certs/home/actionators/list_certs.sh
+++ images/03.ServiceImages/certs/home/actionators/list_certs.sh
@@ -17,7 +17,7 @@ if test -d /home/certs/store/public/certs/
  	else
  		echo -n ,
  	fi
- 	store=`dirname $cert |sed "/^\./s///"`
+ 	store=`dirname $cert |sed "/^\.\//s///"`
  	cert=`basename $cert`
     echo -n '{"cert_name":"'$cert'","store":"'$store'"}'
   done
