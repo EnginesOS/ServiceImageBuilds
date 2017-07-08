20e5518760e24b4390b6172aaa1a5c0a42168711
James L Vodanovich <james@jvodan.com>
2017-07-08 10:25:03 +1000

    f

--- images/03.ServiceImages/certs/home/actionators/remove_cert.sh
+++ images/03.ServiceImages/certs/home/actionators/remove_cert.sh
@@ -22,14 +22,16 @@ if ! test -f /home/certs/store/public/certs/$store/$cert_name
     fi
 
 
-   sudo -n /home/remove.sh certs/$store/$cert_name 
+   sudo -n /home/remove.sh certs/$store/${cert_name}.crt 
    
      if test $? -ne 0
      then
        echo "Failed to Delete Cert $cert_name"
        exit 255
     fi
-   sudo -n /home/remove.sh keys/$store/$cert_name
+    
+   sudo -n /home/remove.sh keys/$store/${cert_name}.key
+   
      if test $? -ne 0
      then
        echo "Failed to Delete Key $cert_name"
