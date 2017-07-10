6f8795d71b2fc4c24ade2d0cfaebcdc4b5622479
James L Vodanovich <james@jvodan.com>
2017-07-10 15:43:43 +1000

    f

--- images/03.ServiceImages/certs/home/remove.sh
+++ images/03.ServiceImages/certs/home/remove.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if test $1 = service
+if test "$1" = service
  then
   if test $# -eq 3
    then
-     rm "/home/certs/store/${1}s/$2
+     rm "/home/certs/store/${1}s/$2"
    fi
 else
    rm "/home/certs/store/public/$1"
