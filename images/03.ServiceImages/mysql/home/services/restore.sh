7264919f99d99bf0385c41907911b6808068179c
James L Vodanovich <james@jvodan.com>
2017-07-12 15:51:15 +1000

    tidy

--- images/03.ServiceImages/mysql/home/services/restore.sh
+++ images/03.ServiceImages/mysql/home/services/restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-tar -xzpf -  2>/tmp/tar.errs
+tar -xzpf - 2>/tmp/tar.errs
 
-cat /tmp/mysql_server/backup.*gz |gzip -d | mysql -h 127.0.0.1 -u rma  --all-databases --single-transaction  2>/tmp/mysqlimport.errs
+cat /tmp/mysql_server/backup.*gz |gzip -d | mysql -h 127.0.0.1 -u rma 2> /tmp/mysqlimport.errs
 if test $? -ne 0
  then 
-   cat  /tmp/mysqldump.errs  >&2
+   cat  /tmp/mysqlimport.errs >&2
    exit -1
 fi
  
