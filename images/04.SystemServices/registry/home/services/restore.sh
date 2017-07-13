62f070fbe8289a2ccbf1bac94d25383691ba6045
James L Vodanovich <james@jvodan.com>
2017-07-13 11:16:51 +1000

    backup existing

--- images/04.SystemServices/registry/home/services/restore.sh
+++ images/04.SystemServices/registry/home/services/restore.sh
@@ -2,15 +2,13 @@
 
 cat - >/tmp/new_reg
 
-
 while test -f /tmp/registry.lock
  do
   sleep 1
- done
+done
  
 touch /tmp/registry.lock
-
-cp /tmp/new_reg /opt/engines/run/service_manager/services.yaml.res
-
+cp /opt/engines/run/service_manager/services.yaml /opt/engines/run/service_manager/services.yaml.pre_restore
+cp /tmp/new_reg /opt/engines/run/service_manager/services.yaml
 
 rm /tmp/registry.lock
