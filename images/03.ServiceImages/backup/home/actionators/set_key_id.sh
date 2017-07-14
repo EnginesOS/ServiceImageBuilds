95c416f6a034c0ddccd7bd29cb4b4badcfd4ea89
James L Vodanovich <james@jvodan.com>
2017-07-14 13:38:34 +1000

    set key id actonator

---
+++ images/03.ServiceImages/backup/home/actionators/set_key_id.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+if ! test -z $disabled 
+ then
+   if $disabled = yes
+    then
+    	rm /home/backup/.gnupg/key_id
+    	exit 0
+    fi
+fi
+
+if test -z $key_id
+ then
+ 	echo "missing key is"
+ 	exit 255
+ fi
+ 
+ if ! test -f /home/backup/.gnupg/pass_${key_id}
+  then
+   echo "missing password"
+ 	exit 255
+ fi
+ 
+ echo ${key_id} > /home/backup/.gnupg/key_id
+   
