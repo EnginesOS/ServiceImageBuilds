9b162ed7266e4a4400e83de6f70349a0827208e3
James L Vodanovich <james@jvodan.com>
2017-07-15 10:46:26 +1000

    registry is a single file

--- images/03.ServiceImages/backup/home/actionators/_restore_registry.sh
+++ images/03.ServiceImages/backup/home/actionators/_restore_registry.sh
@@ -3,5 +3,5 @@ duply registry restore /tmp/registry $from_date
 
 CURL_OPTS="-k -X PUT --header "Content-Type:application/octet-stream" --data-binary  @-"
 
-cat /tmp/*registry* |curl $CURL_OPTS https://172.17.0.1:2380/v0/restore/registry
-rm -r /tmp/*registry*
+cat /tmp/registry/backup.*.gz |curl $CURL_OPTS https://172.17.0.1:2380/v0/restore/registry
+rm -r /tmp/registry
