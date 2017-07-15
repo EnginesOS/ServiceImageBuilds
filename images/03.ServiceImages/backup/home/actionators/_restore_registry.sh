ed2f54544d22a038ea99f3149af38421b9c505a7
James L Vodanovich <james@jvodan.com>
2017-07-15 10:44:01 +1000

    dont use tar as single file

--- images/03.ServiceImages/backup/home/actionators/_restore_registry.sh
+++ images/03.ServiceImages/backup/home/actionators/_restore_registry.sh
@@ -3,5 +3,5 @@ duply registry restore /tmp/registry $from_date
 
 CURL_OPTS="-k -X PUT --header "Content-Type:application/octet-stream" --data-binary  @-"
 
-tar -czpf - /tmp/*registry* |curl $CURL_OPTS https://172.17.0.1:2380/v0/restore/registry
+cat /tmp/*registry* |curl $CURL_OPTS https://172.17.0.1:2380/v0/restore/registry
 rm -r /tmp/*registry*
--- images/04.SystemServices/registry/home/services/restore.sh
+++ images/04.SystemServices/registry/home/services/restore.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cat - | tar -xzpf -
+cat - | gzip -d > /tmp/registry_restored 
 
-while test -f /tmp/registry.lock
+while test -f /tmp/registry.lock 
  do
   sleep 1
 done
  
 touch /tmp/registry.lock
 cp /opt/engines/run/service_manager/services.yaml /opt/engines/run/service_manager/services.yaml.pre_restore
-cp  /tmp/registry/backup.* /opt/engines/run/service_manager/services.yaml
+mv  /tmp/registry_restored /opt/engines/run/service_manager/services.yaml
 
 rm /tmp/registry.lock
