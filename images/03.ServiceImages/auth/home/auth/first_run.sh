9bb5f10185c46ee37eb458c9857646bd8f13ac79
James L Vodanovich <james@jvodan.com>
2017-07-29 10:57:53 +1000

    sudo component of first run

--- images/03.ServiceImages/auth/home/auth/first_run.sh
+++ images/03.ServiceImages/auth/home/auth/first_run.sh
@@ -2,20 +2,6 @@
 
 mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname < /home/auth/create_tables.sql
 
-if test -f /home/home_dir/.ssh/krb.pass
- then 
-  pass=`cat /home/home_dir/.ssh/krb.pass`
-else
-  pass=`dd if=/dev/urandom count=12 bs=1  | od -h | awk '{ print $2$3$4$5$6}'`
-  echo pass > /home/home_dir/.ssh/krb.pass
-  chmod og-rwx /home/home_dir/.ssh/krb.pass
-fi
+sudo -n /home/_first_run.sh
 
-
-cp -rp /var/lib/krb5kdc.init /var/lib/krb5kdc
-cp -rp /etc/krb5kdc.orig /etc/krb5kdc  
-	
-	
-export pass 
-expect /home/auth/kerobos_init.expect
 
