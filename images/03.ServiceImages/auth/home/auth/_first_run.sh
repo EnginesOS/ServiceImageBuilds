f8d1b1683edb1e357b7c4089c07cf48aae6190a8
James L Vodanovich <james@jvodan.com>
2017-07-29 11:45:20 +1000

    f

--- images/03.ServiceImages/auth/home/auth/_first_run.sh
+++ images/03.ServiceImages/auth/home/auth/_first_run.sh
@@ -5,13 +5,10 @@ if test -f /home/home_dir/.ssh/krb.pass
   pass=`cat /home/home_dir/.ssh/krb.pass`
 else
   pass=`dd if=/dev/urandom count=12 bs=1  | od -h | awk '{ print $2$3$4$5$6}'`
-  echo pass > /home/home_dir/.ssh/krb.pass
+  echo $pass > /home/home_dir/.ssh/krb.pass
   chmod og-rwx /home/home_dir/.ssh/krb.pass
 fi
 
-
-cp -rp /var/lib/krb5kdc.init /var/lib/krb5kdc
-cp -rp /etc/krb5kdc.orig /etc/krb5kdc  
 	
 	
 export pass 
