e49adae814b2f1a584f64fe78807518ec42723c9
James L Vodanovich <james@jvodan.com>
2017-07-30 15:35:43 +1000

    kerberos for all

--- images/03.ServiceImages/ldap/home/configure_kerobos.sh
+++ images/03.ServiceImages/ldap/home/configure_kerobos.sh
@@ -9,3 +9,7 @@ mkdir /tmp/ldif_output
 slapcat -f /home/schema_convert.conf -F /tmp/ldif_output -n0 -s \
 "cn={12}kerberos,cn=schema,cn=config" > /tmp/cn=kerberos.ldif
 
+l=`wc -l /tmp/cn=kerberos.ldif`
+l=`expr $l - 7`
+head -$l /tmp/cn=kerberos.ldif > /tmp/cn
+cat /tmp/cn |sed 
