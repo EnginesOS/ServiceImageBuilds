2c1419163d21c82c618e4931e1fc563aaae45a1d
James L Vodanovich <james@jvodan.com>
2017-08-02 10:51:40 +1000

    fmt

--- images/03.ServiceImages/auth/home/_first_run.sh
+++ images/03.ServiceImages/auth/home/_first_run.sh
@@ -16,7 +16,12 @@ fi
 	
 export pass 
 expect /home/auth/kerobos_init.expect
+expect /home/auth/kerobos_stash_key.expect
+ 
 
+#
+#
+#
 
 #kdb5_ldap_util -D  cn=admin,dc=engines,dc=internal create -subtrees dc=engines,dc=internal -r ENGINES.INTERNAL -s -H ldap://ldap.engines.internal
 kdb5_ldap_util  -sf /etc/krb5kdc/ldap/service.keyfile  create -subtrees dc=engines,dc=internal -r ENGINES.INTERNAL -s -H ldap://ldap.engines.internal
