d57630b99ba805a99aad4be3be01dfc8090e7fba
James L Vodanovich <james@jvodan.com>
2017-08-03 16:33:59 +1000

    start with base ldif

--- images/03.ServiceImages/ldap/home/_first_run.sh
+++ images/03.ServiceImages/ldap/home/_first_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#cp -r /home/slapd.d /etc/ldap/
-mkdir -p /etc/ldap/slapd.d
-slaptest -f /home/tmpls/slapd.conf  -F /etc/ldap/slapd.d
-chown -R openldap /etc/ldap/slapd.d
+cp -r /home/slapd.d /etc/ldap/
+#mkdir -p /etc/ldap/slapd.d
+#slaptest -f /home/tmpls/slapd.conf  -F /etc/ldap/slapd.d
+#chown -R openldap /etc/ldap/slapd.d
