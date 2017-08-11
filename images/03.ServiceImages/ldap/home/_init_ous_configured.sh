3d13d51e8e79ea609933306e1335e732fdf67182
James L Vodanovich <james@jvodan.com>
2017-08-11 11:08:23 +1000

    u

--- images/03.ServiceImages/ldap/home/_init_ous_configured.sh
+++ images/03.ServiceImages/ldap/home/_init_ous_configured.sh
@@ -3,6 +3,9 @@
 #Create dc=engines,dc=internal
 ldapadd -Y EXTERNAL -H ldapi:/// -f /home/tmpls/init.ldif
 
+ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/cosine.ldif
+ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/ldap/schema/inetorgperson.ldif
+
 #tree root groups and ous 
 ldapadd -Y EXTERNAL -H ldapi:/// -f /home/tmpls/initial_ous.ldif
 
