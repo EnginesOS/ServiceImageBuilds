9b9e4d4c03aa60a2080ab32c658c9a3b9a9705bf
James L Vodanovich <james@jvodan.com>
2017-08-16 10:08:40 +1000

    remove old

--- images/03.ServiceImages/auth/home/start.sh
+++ images/03.ServiceImages/auth/home/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 PID_FILE=/var/run/krb5kdc.pid 
 export PID_FILE
 . /home/engines/functions/trap.sh
@@ -13,35 +11,12 @@ if ! test -f /engines/var/run/flags/first_run.done
 	/home/first_run.sh		
 	touch /engines/var/run/flags/first_run.done
  fi
-	
-#echo dbflavor=$dbflavor >/home/auth/.dbenv
-#echo dbhost=$dbhost >>/home/auth/.dbenv
-#echo dbname=$dbname >>/home/auth/.dbenv
-#echo dbpasswd=$dbpasswd >>/home/auth/.dbenv
-#echo dbuser=$dbuser >>/home/auth/.dbenv
-
-#chmod og-rwx  /home/auth/.dbenv
-
-
-
 
 SIGNAL=0
 sudo -n /home/engines/scripts/_start_syslog.sh
 #sudo -n /home/_start_sshd.sh
 sudo -n /home/_start_kerobos.sh 
-
-
-while test $SIGNAL -ne 3 -a $SIGNAL -ne 15
-do
- if test -f $PID_FILE
- 	then
- 	echo "waiting"
- 	sleep 500 &
-		wait 
-		#echo $SIGNAL
- fi
-done
-
+exit_code=$?
 sudo /home/engines/scripts/_kill_syslog.sh
 rm -f /engines/var/run/flags/startup_complete
 exit $exit_code
