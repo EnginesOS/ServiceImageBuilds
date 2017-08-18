ac3517f8c5e690fa24eb5fb24a2f567bc88d637a
James L Vodanovich <james@jvodan.com>
2017-08-18 12:07:38 +1000

    f

--- images/03.ServiceImages/ftp/home/add_service.sh
+++ images/03.ServiceImages/ftp/home/add_service.sh
@@ -19,11 +19,10 @@ if ! test -z
 fi   
 cat /home/tmpls/new_user.ldif \
  | sed "s/SN/$service_handle/" \
- | sed "s/ENGINE/$engines_name/" \
- | sed "s/UID/$service_handle_${service_container_name}/" \
- | sed "s/USER/${username}\/_${service_container_name}/" > /tmp/newuser.ldif
+ | sed "s/UID/${service_handle}\/${service_container_name}/" \
+ | sed "s/USER/${username}/" > /tmp/newuser.ldif
  
- echo /ftp/$engine_name/$volume/$folder >> /tmp/newuser.ldif
+ echo /ftp/$access/$parent_engine/$volume/$folder >> /tmp/newuser.ldif
 
  cat /tmp/newuser.ldif | ldapadd -H ldap://ldap/
  
