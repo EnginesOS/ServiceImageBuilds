2a76c100b3fdb76cb3e0b73b396504c83d1cc52e
James L Vodanovich <james@jvodan.com>
2017-08-21 10:05:34 +1000

    ignore modify output

--- images/03.ServiceImages/ftp/home/next_id.sh
+++ images/03.ServiceImages/ftp/home/next_id.sh
@@ -4,6 +4,6 @@
 echo "dn: cn=uidNext,dc=engines,dc=internal
 changetype: modify
 increment: uidNumber
-uidNumber: 1" |  ldapmodify -H ldap://ldap/
+uidNumber: 1" |  ldapmodify -H ldap://ldap/ &>/null
 
 ldapsearch -H ldap://ldap/ -b cn=uidNext,dc=engines,dc=internal |grep uidNu | awk '{print $2}'
