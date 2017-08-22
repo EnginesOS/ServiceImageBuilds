d7acf64b79d0af64aefe5e29aa2b55b3043fc4b3
James L Vodanovich <james@jvodan.com>
2017-08-22 11:21:05 +1000

    use latet version as repo version is old

--- images/03.ServiceImages/openid/home/start.sh
+++ images/03.ServiceImages/openid/home/start.sh
@@ -5,6 +5,10 @@ PID_FILE=/var/run/nginx/nginx.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
 
+mkdir /var/log/apache2
+mkdir /var/run/apache2
+  
+ 
 sleep 3600 &
 
 touch  /engines/var/run/flags/startup_complete
