f572c8c0b8f5798c7de4dc629777e62a9e1e79be
James L Vodanovich <james@jvodan.com>
2017-08-29 14:13:39 +1000

    f

--- images/03.ServiceImages/email/home/services/restore.sh
+++ images/03.ServiceImages/email/home/services/restore.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-cd /
-tar -xpf -  2>/tmp/tar.errs
-
-. /home/auth/.dbenv
-
-cat /tmp/email/backup.* | mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname
-r=$?
-rm  /tmp/auth/backup.*
-exit $r
