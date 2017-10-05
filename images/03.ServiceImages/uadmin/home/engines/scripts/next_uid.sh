fb7ebb143ebf3ce48faaf5a2c71e9841065d30fb
James L Vodanovich <james@jvodan.com>
2017-10-05 16:52:05 +1100

    correct typos

--- images/03.ServiceImages/uadmin/home/engines/scripts/next_uid.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/next_uid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -n /home/engines/sudo/_next_uid.sh
+sudo -n /home/engines/scripts/sudo/_next_uid.sh
--- images/03.ServiceImages/uadmin/home/engines/scripts/sudo/_next_uid.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/sudo/_next_uid.sh
@@ -2,5 +2,5 @@
 
 kinit -kt /etc/krb5kdc/keys/uadmin.keytab  >& /dev/null
 ldapmodify -h ldap /home/templates/incre_uid.ldif >& /dev/null
-ldapsearch -h ldap -b "cn=uidNext,dc=engines,dc=internal" -LLL uidNumber |grep uidNumber |cut -f2.-d:
+ldapsearch -h ldap -b "cn=uidNext,dc=engines,dc=internal" -LLL uidNumber |grep uidNumber |cut -f2 -d:
 kdestroy
