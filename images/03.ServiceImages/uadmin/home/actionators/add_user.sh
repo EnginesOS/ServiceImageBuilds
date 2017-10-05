cf77ca9c5cf38536a0c5c4953eb7189548575920
James L Vodanovich <james@jvodan.com>
2017-10-05 15:01:22 +1100

    add user to ldap ldifs and scripts

--- images/03.ServiceImages/uadmin/home/actionators/add_user.sh
+++ images/03.ServiceImages/uadmin/home/actionators/add_user.sh
@@ -2,9 +2,15 @@
 . /home/engines/functions/params_to_env.sh
 parms_to_env
 
-cat /home/templates/add_user.ldif |  while read LINE
+cat /home/templates/add_user.ldif | while read LINE
 do
-eval echo $LINE > /tmp/ldif
+ eval echo $LINE >> /tmp/ldif
 done
+
+ldapmodify -h ldap /home/templates/incre_uid.ldif
+
+uidnumber=`ldapsearch -h ldap -b "cn=uidNext,dc=engines,dc=internal" -LLL uidNumber   |grep uidNumber |cut -f2.-d:`
+
+echo uidnumber:$uidnumber >> /tmp/ldif
 
 cat /tmp/ldif | /home/engines/scripts/ldapadd.sh -h ldap 
