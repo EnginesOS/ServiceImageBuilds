df5c5c9ccde279b846608bf1d39d06cbb00ca796
James L Vodanovich <james@jvodan.com>
2017-10-05 15:07:36 +1100

    f

--- images/03.ServiceImages/uadmin/home/actionators/add_user.sh
+++ images/03.ServiceImages/uadmin/home/actionators/add_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . /home/engines/functions/params_to_env.sh
 parms_to_env
-
+rm /tmp/ldif
 cat /home/templates/add_user.ldif | while read LINE
 do
  eval echo $LINE >> /tmp/ldif
@@ -9,7 +9,7 @@ done
 
 ldapmodify -h ldap /home/templates/incre_uid.ldif
 
-uidnumber=`ldapsearch -h ldap -b "cn=uidNext,dc=engines,dc=internal" -LLL uidNumber   |grep uidNumber |cut -f2.-d:`
+uidnumber=`ldapsearch -h ldap -b "cn=uidNext,dc=engines,dc=internal" -LLL uidNumber |grep uidNumber |cut -f2.-d:`
 
 echo uidnumber:$uidnumber >> /tmp/ldif
 
