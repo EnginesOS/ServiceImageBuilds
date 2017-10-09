9328c11fadf8a58370a0eee3f891f18d1ee186e0
James L Vodanovich <james@jvodan.com>
2017-10-09 16:38:18 +1100

    error handling take II

--- images/03.ServiceImages/uadmin/home/engines/scripts/ldapdelete.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/ldapdelete.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
 sudo /home/engines/scripts/sudo/_ldapdelete.sh $* &> /tmp/ldap.add.out
-if test $ldap_result -eq 0
- then
-  echo '{"Result":"OK","ReturnCode",0}'
-  else
-   err=`cat /tmp/ldap.add.out`
-   echo  '{"Result":"FAIL","ReturnCode":'$ldap_result':"Output":"'$err'"}'
- fi
+#if test $ldap_result -eq 0
+# then
+#  echo '{"Result":"OK","ReturnCode",0}'
+#  else
+out=`cat /tmp/ldap.add.out`
+#   echo  '{"Result":"FAIL","ReturnCode":'$ldap_result':"Output":"'$err'"}'
+# fi
+#
+echo  '{"Result":"OK","ReturnCode":0,"Message":"'$out'"}'
--- images/03.ServiceImages/uadmin/home/engines/scripts/ldapmodify.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/ldapmodify.sh
@@ -2,10 +2,12 @@
 
 
 sudo /home/engines/scripts/sudo/_ldapmodify.sh $* &> /tmp/ldap.add.out
-if test $ldap_result -eq 0
- then
-  echo '{"Result":"OK","ReturnCode",0}'
-  else
-   err=`cat /tmp/ldap.add.out`
-   echo  '{"Result":"FAIL","ReturnCode":'$ldap_result':"Output":"'$err'"}'
- fi
+#if test $ldap_result -eq 0
+# then
+#  echo '{"Result":"OK","ReturnCode",0}'
+#  else
+out=`cat /tmp/ldap.add.out`
+#   echo  '{"Result":"FAIL","ReturnCode":'$ldap_result':"Output":"'$err'"}'
+# fi
+
+echo  '{"Result":"OK","ReturnCode":0,"Message":"'$out'"}'
