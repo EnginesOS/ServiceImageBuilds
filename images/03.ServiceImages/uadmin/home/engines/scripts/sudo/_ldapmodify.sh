1fe14841d3d50c2f8eeb3d05549d98ec2ceb2b7c
James L Vodanovich <james@jvodan.com>
2017-10-09 15:50:27 +1100

    error checking and rec

--- images/03.ServiceImages/uadmin/home/engines/scripts/sudo/_ldapmodify.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/sudo/_ldapmodify.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
 kinit -kt /etc/krb5kdc/keys/uadmin.keytab 
-/usr/bin/ldapmodify -h ldap $*
+/usr/bin/ldapmodify -h ldap $* >& /tmp/ldap.mod.out
+ldap_result=$?
+export ldap_result
+if test $ldap_result -eq 0
+ then
+  echo '{"Result":"OK","ReturnCode",0}'
+  else
+   err=`cat /tmp/ldap.add.out`
+   echo  '{"Result":"FAIL","ReturnCode":"'$ldap_result'":"Output":"'$err'"}'
+ fi
 kdestroy
