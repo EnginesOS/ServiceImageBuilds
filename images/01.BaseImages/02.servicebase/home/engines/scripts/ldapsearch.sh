2f35d5bcd5f9ee62051db06da27ab9ea990f69e2
James L Vodanovich <james@jvodan.com>
2017-10-13 16:18:57 +1100

    move ldap scripts to service base, standardise sudoers file addiion

---
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/ldapsearch.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+
+sudo /home/engines/scripts/sudo/_ldapsearch.sh "$1" $2 $3 $4 $5 $6 $7 $8 
---
+++ images/03.ServiceImages/uadmin/home/actionators/whose_email.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. /home/engines/functions/params_to_env.sh
+params_to_env
+
+if test -z $email_address
+ then
+  echo '{"error":"No email_address specified"}'
+  exit 127
+fi
+
+. /home/actionators/x400_to_json.sh
+
+/home/engines/scripts/ldapsearch.sh "ou=People,dc=engines,dc=internal" mailacceptinggeneralid=$email_address uid> $LDAP_FILE
+
+echo '{"user":'
+ldap_to_json
+echo '}'
