db48fbe72cfcd4558632002d5a5ebc88c4fc2a67
James L Vodanovich <james@jvodan.com>
2017-10-14 16:00:45 +1100

    create logdir if needed

--- images/03.ServiceImages/nginx/home/start.sh
+++ images/03.ServiceImages/nginx/home/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-
+if ! test -d /var/log/nginx
+ then
+  mkdir -p  /var/log/nginx
+fi
+	
 PID_FILE=/var/run/nginx/nginx.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
@@ -9,7 +13,7 @@ export PID_FILE
 
 /usr/sbin/nginx &
 
-touch  /engines/var/run/flags/startup_complete
+touch /engines/var/run/flags/startup_complete
 
 wait
 exit_code=$?
