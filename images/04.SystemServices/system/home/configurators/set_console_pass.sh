2004499b72c455ec8fe70b0f9224388113ccaaf8
James L Vodanovich <james@jvodan.com>
2017-10-17 08:48:58 +1100

    change ssh key dir to sensible name

--- images/04.SystemServices/system/home/configurators/set_console_pass.sh
+++ images/04.SystemServices/system/home/configurators/set_console_pass.sh
@@ -8,7 +8,7 @@ cat - | /home/engines/bin/json_to_env >/tmp/.env
    	if test -n $console_password
 	then  
 		mgmt_ip=$CONTROL_IP
-	ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/mgmt/update_engines_console_password engines@${mgmt_ip} /opt/engines/bin/update_engines_console_password.sh $console_password
+	ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/engines/.ssh/system/update_engines_console_password engines@${mgmt_ip} /opt/engines/bin/update_engines_console_password.sh $console_password
  		exit $?	
  	fi
  
