935e3901a6f171ab31b8abb134ee8746c86b2874
James L Vodanovich <james@jvodan.com>
2017-10-18 08:40:31 +1100

    +x perms

--- images/03.ServiceImages/mysql/home/engines/scripts/mysql_server/_mkdir_rundir.sh
+++ images/03.ServiceImages/mysql/home/engines/scripts/mysql_server/_mkdir_rundir.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
 if ! test -d  /var/run/mysqld/
  then
  	echo "setup run dir"
  	mkdir -p /var/run/mysqld/
  	chown mysql /var/run/mysqld/
 fi
-
 
 if ! test -d  /var/log/mysqld/
  then
