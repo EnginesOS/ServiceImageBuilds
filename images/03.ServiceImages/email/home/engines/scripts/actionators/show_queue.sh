4933bb72d05fa0c1428522ef48c29340d4842139
James L Vodanovich <james@jvodan.com>
2017-10-23 12:42:27 +1100

    tidy showqueue

--- images/03.ServiceImages/email/home/engines/scripts/actionators/show_queue.sh
+++ images/03.ServiceImages/email/home/engines/scripts/actionators/show_queue.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
-postqueue -j 2> /tmp/err
+TMP_FILE=`mktemp`
+postqueue -j 2> $TMP_FILE.err 1> $TMP_FILE
 result=$?
-string_for_json=`cat /tmp/err`
-rm /tmp/err
-string_for_json=`echo $string_for_json | tr -d "'\r\"\t\f\b\n\v"`
+
 if test $result -ne 0
  then
-   echo '{"Result":"FAILED","ReturnCode":"'$result'","Error":"'$string_for_json'"}'  
-  fi 
+   string_for_json=`cat $TMP_FILE.err`
+   rm /tmp/err
+   string_for_json=`echo $string_for_json | tr -d "'\r\"\t\f\b\n\v"`
+   echo '{"Result":"FAILED","ReturnCode":"'$result'","Error":"'$string_for_json'"}'
+else
+  cat $TMP_FILE    
+fi 
+
+rm /tmp/$TMP_FILE.err $TMP_FILE
