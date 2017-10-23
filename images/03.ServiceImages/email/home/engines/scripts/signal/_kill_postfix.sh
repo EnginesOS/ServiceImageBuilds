bf4de2d1282e0f7a9354ff86d60527d3221712ed
James L Vodanovich <james@jvodan.com>
2017-10-24 09:52:10 +1100

    shutdown scriptsfor sleep/postfix

--- images/03.ServiceImages/email/home/engines/scripts/signal/_kill_postfix.sh
+++ images/03.ServiceImages/email/home/engines/scripts/signal/_kill_postfix.sh
@@ -4,8 +4,5 @@
  if  test -f /var/spool/postfix/pid/master.pid
   then
     kill -$1 `cat /var/spool/postfix/pid/master.pid`
-    echo kill -$1 `cat /var/spool/postfix/pid/master.pid`
-    /home/engines/scripts/_kill_syslog.sh
-    rm /var/spool/postfix/pid/master.pid
  fi
  
---
+++ images/03.ServiceImages/smtp/home/engines/scripts/signal/kill_sleep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ sudo -n /home/engines/scripts/signal/_kill_sleep.sh $1
+ 
