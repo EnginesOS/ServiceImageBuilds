9c61133cd8d5da1319070f16eca8949d53667852
James L Vodanovich <james@jvodan.com>
2017-10-23 12:07:41 +1100

    typo

--- images/03.ServiceImages/email/home/engines/scripts/first_run/first_run.sh
+++ images/03.ServiceImages/email/home/engines/scripts/first_run/first_run.sh
@@ -3,8 +3,8 @@
 touch /home/email/smarthost_passwd 
 touch /home/email/generic 
 touch /home/email/transport
-sudo -n /home/engines/scipts/engine/_postmap.sh transport 
-sudo -n /home/engines/scipts/engine/_postmap.sh smarthost_passwd
-sudo -n /home/engines/scipts/engine/_postmap.sh generic
+sudo -n /home/engines/scripts/engine/_postmap.sh transport 
+sudo -n /home/engines/scripts/engine/_postmap.sh smarthost_passwd
+sudo -n /home/engines/scripts/engine/_postmap.sh generic
  
 sudo -n /home/engines/scripts/first_run/_setup_dirs.sh
