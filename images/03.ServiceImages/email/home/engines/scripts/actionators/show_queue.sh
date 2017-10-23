3868eec35db153121beec8f39b5b0ef02bd86b9f
James L Vodanovich <james@jvodan.com>
2017-10-23 13:04:38 +1100

    improve json result str

--- images/03.ServiceImages/email/home/engines/scripts/actionators/show_queue.sh
+++ images/03.ServiceImages/email/home/engines/scripts/actionators/show_queue.sh
@@ -10,7 +10,25 @@ if test $result -ne 0
    string_for_json=`echo $string_for_json | tr -d "'\r\"\t\f\b\n\v"`
    echo '{"Result":"FAILED","ReturnCode":"'$result'","Error":"'$string_for_json'"}'
 else
-  cat $TMP_FILE    
-fi 
+  echo '{"email_queue":['
+  one=1
+    cat $TMP_FILE | while read LINE
+     do
+       if test $one -eq 1
+         then
+          one=0
+       else
+          echo -n ","
+       fi
+      echo  -n $LINE
+    done
+  echo ']}'
+fi
 
-rm /tmp/$TMP_FILE.err $TMP_FILE
+for FILE in $TMP_FILE.err $TMP_FILE
+ do
+  if test -f $FILE
+   then
+     rm $FILE
+  fi
+done
