38ec86d06143023c75ab3f5eb44bb78fe65ba10a
James L Vodanovich <james@jvodan.com>
2017-10-24 18:07:58 +1100

    firs path

--- images/03.ServiceImages/email/home/engines/scripts/engine/_postmap.sh
+++ images/03.ServiceImages/email/home/engines/scripts/engine/_postmap.sh
@@ -3,5 +3,5 @@ if ! test -f "/home/email/$1"
  then
   touch "/home/email/$1" 
  fi 
-cp "/home/email/$1" /etc/postfix/
-postmap  /etc/postfix/"$1" 
+cp "/home/email/$1" /etc/postfix/maps
+postmap  /etc/postfix/maps/"$1" 
