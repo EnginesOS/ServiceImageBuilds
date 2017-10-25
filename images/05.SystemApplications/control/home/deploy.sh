c8409fb97692baaac3fa61f0eef451c5bc3a487d
James L Vodanovich <james@jvodan.com>
2017-10-25 14:09:38 +1100

    correct port

--- images/05.SystemApplications/control/home/deploy.sh
+++ images/05.SystemApplications/control/home/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-cd /home/app
+
 release=$RELEASE
 release=master
-
-git clone --depth 1 --branch $release https://github.com/lachdoug/admin_gui /home/app/
+if ! test -d /home/app
+ then
+	git clone --depth 1 --branch $release https://github.com/lachdoug/admin_gui /home/app/
+ else
+   cd /home/app
+   git pull	
+ fi	
 cd /home/app 
 echo installing Gems
 bundle install --standalone   
