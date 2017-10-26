3d7cf00b4b552fc6dc4f4ad80d23daf7fcf8aee1
James L Vodanovich <james@jvodan.com>
2017-10-26 22:24:15 +1100

    continuos deployment

--- images/05.SystemApplications/control/home/start.sh
+++ images/05.SystemApplications/control/home/start.sh
@@ -3,9 +3,9 @@
 PID_FILE=/engines/var/run/control.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
-
+/home/deploy.sh
 cd /home/app/
-bundle exec thin --threaded --ssl --ssl-key-file /engines/ssl/keys/engines.key --ssl-cert-file /engines/ssl/certs/engines.crt -C /home/config.yaml -R /home/config.ru start 1&2> /var/log/control_.log &
+bundle exec thin --threaded --ssl --ssl-key-file /engines/ssl/keys/engines.key --ssl-cert-file /engines/ssl/certs/engines.crt -C /home/config.yaml -R /home/config.ru start &
 
 touch /engines/var/run/flags/startup_complete 
 wait 
