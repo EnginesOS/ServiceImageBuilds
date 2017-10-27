e621d57dc673b00670705d9c92afdee7a4afaee3
James L Vodanovich <james@jvodan.com>
2017-10-28 10:00:26 +1100

    f

--- images/03.ServiceImages/imap/home/engines/scripts/signal/stop_dovecot.sh
+++ images/03.ServiceImages/imap/home/engines/scripts/signal/stop_dovecot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo _stop_dovecot.sh $SIGNAL $PIDFILE
-sudo -n /home/engines/scripts/signal/_stop_dovecot.sh $SIGNAL $PIDFILE
+echo _stop_dovecot.sh $SIGNAL $PID_FILE
+sudo -n /home/engines/scripts/signal/_stop_dovecot.sh $SIGNAL $PID_FILE
