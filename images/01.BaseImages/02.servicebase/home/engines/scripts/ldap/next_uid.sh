b68fead863f3bba3b592bd08d85a0ea3a7acc5db
James L Vodanovich <james@jvodan.com>
2017-10-27 13:05:24 +1100

    fix missing templates

--- images/01.BaseImages/02.servicebase/home/engines/scripts/ldap/next_uid.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/ldap/next_uid.sh
@@ -2,7 +2,7 @@
 
 #sudo -n /home/engines/scripts/sudo/_next_uid.sh
 
-/home/engines/scripts/ldapmodify.sh -f /home/templates/incre_uid.ldif >/dev/null
+/home/engines/scripts/ldap/ldapmodify.sh -f /home/engines/functions/templates/ldap/incre_uid.ldif >/dev/null
 uid=`/home/engines/scripts/ldap/ldapsearch.sh "cn=uidNext,dc=engines,dc=internal" uidNumber |grep uidNumber |cut -f2 -d:`
 
 echo $uid
