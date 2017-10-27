e4d51755f5fcc32d77e307e2aa263ed76faa4fb3
James L Vodanovich <james@jvodan.com>
2017-10-28 09:56:15 +1100

    debug stp

--- images/03.ServiceImages/imap/home/engines/scripts/signal/_stop_dovecot.sh
+++ images/03.ServiceImages/imap/home/engines/scripts/signal/_stop_dovecot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+echo kill -$1 cat $2
  kill -$1 `cat $2`
--- images/03.ServiceImages/imap/home/engines/scripts/signal/stop_dovecot.sh
+++ images/03.ServiceImages/imap/home/engines/scripts/signal/stop_dovecot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+echo _stop_dovecot.sh $SIGNAL $PIDFILE
 sudo -n /home/engines/scripts/signal/_stop_dovecot.sh $SIGNAL $PIDFILE
