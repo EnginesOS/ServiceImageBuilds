02da4d0e9a7b3cf0dc5f4916298854402afdc688
James L Vodanovich <james@jvodan.com>
2017-10-28 09:44:16 +1100

    setup kill script

---
+++ images/03.ServiceImages/imap/home/engines/scripts/signal/stop_dovecot.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo -n /home/engines/scripts/signal/_stop_dovecot.sh $SIGNAL $PIDFILE
--- images/03.ServiceImages/imap/home/start.sh
+++ images/03.ServiceImages/imap/home/start.sh
@@ -2,18 +2,13 @@
 
 PID_FILE=/var/run/dovecot/master.pid
 export PID_FILE
+
+KILL_SCRIPT=/home/engines/scripts/signal/stop_dovecot.sh
+export KILL_SCRIPT
+
 . /home/engines/functions/trap.sh
 
-mkdir -p /home/engines/run/flags
-
-if ! test -f /home/engines/run/flags/first_run_ran 
- then
-  /home/engines/scripts/first_run/first_run.sh
-  if test $? -eq 0
-   then 
-     touch /home/engines/run/flags/first_run_ran 
-  fi   
- fi
+service_first_run_check
 
 sudo -n /usr/sbin/dovecot -F &
 touch  /home/engines/run/flags/startup_complete
