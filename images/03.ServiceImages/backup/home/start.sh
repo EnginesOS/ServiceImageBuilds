6b6450a45858ddfcfc2e793a5bf11c55cbd58f85
James L Vodanovich <james@jvodan.com>
2017-10-28 08:43:12 +1100

    correct path

--- images/03.ServiceImages/backup/home/start.sh
+++ images/03.ServiceImages/backup/home/start.sh
@@ -7,16 +7,17 @@ export PID_FILE
 . /home/engines/functions/trap.sh
 
 
-mkdir -p /home/backup/sql_dumps/
-
 /home/backup/fcron/sbin/fcron -f &
 /home/backup/fcron/bin/fcrontab -u backup  -z 
 touch /home/engines/run/flags/startup_complete
-rm `find  /home/backup/.cache/duplicity/ -name lockfile.lock`
+if test -d home/backup/.cache/duplicity/
+ then
+	rm `find  /home/backup/.cache/duplicity/ -name lockfile.lock`
+fi
 wait 
 exit_code=$?
 
-rm -f /home/engines/run/flags/startup_complete
+rm /home/engines/run/flags/startup_complete
 
 exit $exit_code
 
