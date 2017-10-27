800d9e63601e766f57c33e6f1bffc0b438c451a6
James L Vodanovich <james@jvodan.com>
2017-10-28 09:44:46 +1100

    check is running before kill

--- images/03.ServiceImages/email/home/engines/scripts/signal/_kill_postfix.sh
+++ images/03.ServiceImages/email/home/engines/scripts/signal/_kill_postfix.sh
@@ -3,6 +3,10 @@
 
  if  test -f /var/spool/postfix/pid/master.pid
   then
-    kill $1 `cat /var/spool/postfix/pid/master.pid`
+   kill -0 `cat /var/spool/postfix/pid/master.pid` 
+    if test $? -eq 0
+     then
+    	kill $1 `cat /var/spool/postfix/pid/master.pid`
+     fi
  fi
  
