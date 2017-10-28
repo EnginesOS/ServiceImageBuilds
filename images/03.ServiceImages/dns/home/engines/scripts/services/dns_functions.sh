de239f3104359117b3359c3ab88292db7a9df0e3
James L Vodanovich <james@jvodan.com>
2017-10-28 12:37:36 +1100

    use tempfile

--- images/03.ServiceImages/dns/home/engines/scripts/services/dns_functions.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/services/dns_functions.sh
@@ -11,6 +11,8 @@ host=`echo ${hostname} | sed "/[_.]/s//-/g"`
 engine=`echo ${parent_engine} | sed "/[_.]/s///g"`
 fqdn_str=${host}.engines.internal
     
+dns_cmd_file=`mktemp`
+   
 if test -z ${ip}
  then
     update_line=" update add $fqdn_str 30 CNAME ${engine}.engines.internal"
@@ -20,18 +22,19 @@ fi
 
 ip_reversed=`echo $ip |awk  ' BEGIN {  FS="."} {print $4 "." $3 "." $2 "." $1}'`
     
-echo server 127.0.0.1 > /tmp/.dns_cmd
-echo update delete $fqdn_str >> /tmp/.dns_cmd
-echo send >> /tmp/.dns_cmd
-echo $update_line >> /tmp/.dns_cmd
-echo send >> /tmp/.dns_cmd
+echo server 127.0.0.1 > $dns_cmd_file
+echo update delete $fqdn_str >> $dns_cmd_file
+echo send >> $dns_cmd_file
+echo $update_line >> $dns_cmd_file
+echo send >> $dns_cmd_file
 if test -z $no_inarpra 
  then
-	echo update add ${ip_reversed}.in-addr.arpa. 30 PTR $fqdn_str >> /tmp/.dns_cmd
-	echo send >> /tmp/.dns_cmd
+	echo update add ${ip_reversed}.in-addr.arpa. 30 PTR $fqdn_str >> $dns_cmd_file
+	echo send >> $dns_cmd_file
 fi
 
-nsupdate -k /etc/bind/keys/ddns.private /tmp/.dns_cmd
+nsupdate -k /etc/bind/keys/ddns.private $dns_cmd_file
 
+#rm $dns_cmd_file
 
 }
