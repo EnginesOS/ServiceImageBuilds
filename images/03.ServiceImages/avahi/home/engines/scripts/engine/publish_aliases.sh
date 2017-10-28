364c1124bbed6a31fea60b08332ff652b4c35b56
James L Vodanovich <james@jvodan.com>
2017-10-28 12:09:18 +1100

    tidy ouut fix templ location

--- images/03.ServiceImages/avahi/home/engines/scripts/engine/publish_aliases.sh
+++ images/03.ServiceImages/avahi/home/engines/scripts/engine/publish_aliases.sh
@@ -2,5 +2,5 @@
 
 while test 1 -ne 0
  do
-   /home/engines/scripts/engine/avahi-alias.py > /var/log/publish.out 2>/dev/null
+   /home/engines/scripts/engine/avahi-alias.py 1>> /var/log/publish.out 2>> /var/log/publish.err
 done
--- images/03.ServiceImages/avahi/home/start.sh
+++ images/03.ServiceImages/avahi/home/start.sh
@@ -19,7 +19,7 @@ ext_interface=`netstat -nr |grep ^0.0.0.0 | awk '{print $8}' |head -1`
 #`cat /home/net/gateway_interface`
 interfaces="${ext_interface} , docker0"
 echo binding to $interfaces
-cat /home/engines/templates/avahiavahi-daemon.conf.tmpl | sed "/INTERFACES/s//$interfaces/" > /tmp/avahi-daemon.conf
+cat /home/engines/templates/avahi/avahi-daemon.conf.tmpl | sed "/INTERFACES/s//$interfaces/" > /tmp/avahi-daemon.conf
 cp /tmp/avahi-daemon.conf /etc/avahi/avahi-daemon.conf
 
 
@@ -34,7 +34,7 @@ touch /home/avahi/hosts/engines.local
 touch /home/avahi/hosts/avahi.local
 
 ls /home/avahi/hosts/ > /home/avahi/hosts_list
-/home/engines/scripts/engine/publish_aliases.sh &
+/home/engines/scripts/engine/publish_aliases.sh  2>/dev/null &
 
 
 touch /home/engines/run/flags/startup_complete
