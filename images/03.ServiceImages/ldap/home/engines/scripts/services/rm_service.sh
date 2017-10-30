545fd207a773becb4b878b48319824fa142e7987
James L Vodanovich <james@jvodan.com>
2017-10-31 10:09:54 +1100

    rm service

--- images/03.ServiceImages/ldap/home/engines/scripts/services/rm_service.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/rm_service.sh
@@ -4,11 +4,24 @@
 . /home/engines/functions/params_to_env.sh
 params_to_env
 set > /tmp/full_env
-kinit -t /etc/krb5kdc/keys/ldap.keytab 
+ if test $container_type = container
+  then
+   top_ou=applications
+ elif $container_type = services     
+ then 
+ 	top_ou=servics
+ else
+   echo '{"Result":"Failed","Error Mesg":"Invalid container type"}'
+   exit 127	
+  fi
+ if test -z $cn
+  then
+   dn="ou=$parent_engine,ou=$container_types,ou=Groups,dc=engines,dc=internal"
+ else
+   dn=cn=$cn,ou=$parent_engine,ou=$top_ou,ou=Groups,dc=engines,dc=internal"
+ fi
+ 
+ /home/engines/scripts/ldap/ldapdelete.sh "$dn"
 
-dn=cn=$cn,ou=$parent_engine,ou=$container_types,ou=Groups,dc=engines,dc=internal"
-dn="ou=$parent_engine,ou=$container_types,ou=Groups,dc=engines,dc=internal"
 
-
-kdestroy
   
