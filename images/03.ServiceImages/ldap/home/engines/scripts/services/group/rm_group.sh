0f77109d193db2b5f368e695209ee1ee8d02d08b
James L Vodanovich <james@jvodan.com>
2017-10-31 10:52:53 +1100

    missing "

--- images/03.ServiceImages/ldap/home/engines/scripts/services/group/rm_group.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/group/rm_group.sh
@@ -4,7 +4,7 @@ if test -z $cn
   then
    dn="ou=$parent_engine,ou=$container_types,ou=Groups,dc=engines,dc=internal"
  else
-   dn=cn=$cn,ou=$parent_engine,ou=$top_ou,ou=Groups,dc=engines,dc=internal"
+   dn="cn=$cn,ou=$parent_engine,ou=$top_ou,ou=Groups,dc=engines,dc=internal"
  fi
  
  /home/engines/scripts/ldap/ldapdelete.sh "$dn"
