162625df45a0e43b74a4a65cb69cd43d1be58257
James L Vodanovich <james@jvodan.com>
2017-10-31 19:35:59 +1100

    fix path

--- images/01.BaseImages/02.servicebase/home/engines/scripts/ldap/next_gid.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/ldap/next_gid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-/home/engines/scripts/ldapmodify.sh -f /home/engines/functions/templates/ldap/incre_gid.ldif >/dev/null
+/home/engines/scripts/ldap/ldapmodify.sh -f /home/engines/functions/templates/ldap/incre_gid.ldif >/dev/null
 gid=`/home/engines/scripts/ldap/ldapsearch.sh "cn=gidNext,dc=engines,dc=internal" gidNumber |grep gidNumber |cut -f2 -d:`
 
 echo $gid
