e5a997ebdf5873d0a639f8932216b73d4cc92f06
James L Vodanovich <james@jvodan.com>
2017-11-01 15:35:16 +1100

    add dbg

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/set_default.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/set_default.sh
@@ -4,5 +4,5 @@ params_to_env
 
 required_values="cert_name store target"
 check_required_values
-
+echo "set default" /_install_target.sh ${target} ${store}/${cert_name} default
 sudo -n  /home/engines/scripts/engine/_install_target.sh ${target} ${store}/${cert_name} default
