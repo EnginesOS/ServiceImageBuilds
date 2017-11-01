8829b912ebb40d94b966f2b47f7bf85efe45730a
James L Vodanovich <james@jvodan.com>
2017-11-01 16:43:28 +1100

    export install_target for create_cert to use

--- images/03.ServiceImages/certs/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/services/add_service.sh
@@ -6,6 +6,6 @@ params_to_env
 required_values="cert_name container_type parent_engine domainname country state city organisation person"
 check_required_values
 
-export cert_name container_type parent_engine domainname country state city organisation person wild alt_names hostname 
+export cert_name container_type parent_engine domainname country state city organisation person wild alt_names hostname install_target
 
 /home/engines/scripts/engine/create_cert.sh
