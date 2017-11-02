314f28623d115e22fca00cbaf70b8cd59a190fe1
James L Vodanovich <james@jvodan.com>
2017-11-02 14:37:08 +1100

    tidy start

--- images/05.SystemApplications/mgmt/home/engines/scripts/engine/deployment.sh
+++ images/05.SystemApplications/mgmt/home/engines/scripts/engine/deployment.sh
@@ -6,6 +6,10 @@ git fetch origin $release
 git reset --hard FETCH_HEAD
 git pull --depth 1 origin  $release
 
+if ! test -f /home/app/app/config/mail.yml
+ then
+	cat /home/engines/templates/mail.yml.tmpl | sed "s/FQDN_PORT/$FQDN_PORT/" | sed "s/DOMAIN/$DOMAIN/" > /home/app/app/config/mail.yml
+fi
 
 cp /home/newrelic.yml /home/app/
 
