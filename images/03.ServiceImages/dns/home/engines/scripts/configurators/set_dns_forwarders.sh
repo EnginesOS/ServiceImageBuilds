7c6776031e1d3976dd4f87effc718db1d6a2b805
James L Vodanovich <james@jvodan.com>
2017-11-03 11:17:56 +1100

    simple verificaton on dns servers

--- images/03.ServiceImages/dns/home/engines/scripts/configurators/set_dns_forwarders.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/configurators/set_dns_forwarders.sh
@@ -4,20 +4,27 @@
 PARAMS_FILE=/home/engines/scripts/configurators/saved/dns_forwarders
 parms_to_file_and_env
 
+ if test -z $dns_server
+  then
+   $dns_server=" "
+ fi 
  
-if test -z $dns_server
- then
-   echo "Clearing forwarders"
-   echo "" > /home/bind/engines/forwarders
-else 
+if test ${#dns_server} -ge 7
+ then 
    echo "forwarders {
 	$dns_server; " > /home/bind/engines/forwarders
 
     if ! test -z $dns_server2
      then
-       echo "	$dns_server2; " >> /home/bind/engines/forwarders
+       if test ${#dns_server2} -ge 7
+        then
+         echo "	$dns_server2; " >> /home/bind/engines/forwarders
+        fi 
     fi 
   echo "}; " >> /home/bind/engines/forwarders
+  else
+    echo "Clearing forwarders"
+   echo "" > /home/bind/engines/forwarders 
 fi
   
 echo Success
