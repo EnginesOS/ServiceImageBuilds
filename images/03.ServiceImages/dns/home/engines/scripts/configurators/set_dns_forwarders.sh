78ac45f96bc80dca684d2debb45c32e654834667
James L Vodanovich <james@jvodan.com>
2017-11-03 11:02:02 +1100

    blank forwarders if none set

--- images/03.ServiceImages/dns/home/engines/scripts/configurators/set_dns_forwarders.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/configurators/set_dns_forwarders.sh
@@ -4,26 +4,22 @@
 PARAMS_FILE=/home/engines/scripts/configurators/saved/dns_forwarders
 parms_to_file_and_env
 
-
  
- if test -z $dns_server
+if test -z $dns_server
  then
    echo "Clearing forwarders"
    echo "" > /home/bind/engines/forwarders
- fi
- 
-echo "forwarders {
+else 
+   echo "forwarders {
 	$dns_server; " > /home/bind/engines/forwarders
 
- if ! test -z $dns_server2
-  then
-    echo "	$dns_server2; " >> /home/bind/engines/forwarders
- fi
- 
-echo "}; " >> /home/bind/engines/forwarders
+    if ! test -z $dns_server2
+     then
+       echo "	$dns_server2; " >> /home/bind/engines/forwarders
+    fi 
+  echo "}; " >> /home/bind/engines/forwarders
+fi
   
 echo Success
 exit 0
  
- 
-
