d299887773516cdd21756c7d41523283e8dcd542
James L Vodanovich <james@jvodan.com>
2017-11-05 10:42:37 +1100

    pid file

--- images/03.ServiceImages/backup/home/start.sh
+++ images/03.ServiceImages/backup/home/start.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-rm -f /home/engines/run/flags/*
-
-PID_FILE=/home/cron/fcron.pid
+PID_FILE=/var/run/fcron.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
 
 
 /home/backup/fcron/sbin/fcron -f &
+echo $! > /var/run/fcron.pid
 /home/backup/fcron/bin/fcrontab -u backup  -z 
 
 startup_complete
