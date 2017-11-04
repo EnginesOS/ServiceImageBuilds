3bb71a705d8602a9a98a584e16c20fd6ffccf62c
James L Vodanovich <james@jvodan.com>
2017-11-05 10:25:21 +1100

    move traps and env vars to sudo script

--- images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-. /home/engines/functions/system_functions.sh
+
+
+PID_FILE=/var/run/krb5kdc.pid 
+export PID_FILE
+
+KILL_SCRIPT=/home/engines/scripts/signal/kill_kerberos.sh
+export KILL_SCRIPT
+
+. /home/engines/functions/trap.sh
 
 /usr/sbin/krb5kdc -P /var/run/krb5kdc.pid -n &
 kpid=$! 
@@ -10,15 +18,10 @@ echo -n " $pid" >> /var/run/krb5kdc.pid
 startup_complete
 
 #sleep 5
-# if test -f /home/engines/run/flags/first_run.done
-#  then
+if test -f /home/engines/run/flags/first_run.done
+  then
 	wait $kpid 	
 	exit_code=$?
 	/home/engines/scripts/signal/_kill_kerberos.sh TERM
-	
+fi	
     shutdown_complete
-#	
-#	export exit_code
-#  else
-#   export kpid 	
-# fi
--- images/03.ServiceImages/auth/home/start.sh
+++ images/03.ServiceImages/auth/home/start.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-PID_FILE=/var/run/krb5kdc.pid 
-export PID_FILE
 
-KILL_SCRIPT=/home/engines/scripts/signal/kill_kerberos.sh
-export KILL_SCRIPT
-
-. /home/engines/functions/trap.sh
-
+. /home/engines/functions/system_functions.sh
 service_first_run_check
 
 sudo -n /home/engines/scripts/startup/_start.sh 	
 
-shutdown_complete
