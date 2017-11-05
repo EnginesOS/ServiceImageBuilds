5ccf44b5b46fae04a76c6a823f4499ff4944429f
James L Vodanovich <james@jvodan.com>
2017-11-06 10:19:34 +1100

    use seperate create keys function out side of first run to gen keys

---
+++ images/03.ServiceImages/auth/home/engines/scripts/first_run/create_keys_func.sh
@@ -0,0 +1,25 @@
+
+
+function gen_service_key {
+echo addprinc -randkey host/$service.engines.internal@ENGINES.INTERNAL | kadmin.local 
+mkdir -p /etc/krb5kdc/services/$service 
+   
+echo ktadd -k /etc/krb5kdc/services/$service/$service.keytab host/$service.engines.internal@ENGINES.INTERNAL | kadmin.local 
+
+}
+
+function create_keys {
+
+for service in `cat /home/engines/scripts/first_run/key_list`
+ do
+  gen_service_key
+ done 
+ 
+echo addprinc -randkey ldap/ldap.engines.internal@ENGINES.INTERNAL | kadmin.local
+mkdir /etc/krb5kdc/services/ldap
+echo ktadd -k /etc/krb5kdc/services/ldap/ldap.keytab ldap/ldap.engines.internal@ENGINES.INTERNAL | kadmin.local
+ 
+echo addprinc -randkey uadmin/admin@ENGINES.INTERNAL | kadmin.local
+echo ktadd -k /etc/krb5kdc/services/uadmin/uadmin_kadmin.keytab uadmin/admin@ENGINES.INTERNAL | kadmin.local
+ 
+}
--- images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
@@ -8,24 +8,24 @@ export KILL_SCRIPT
 
 . /home/engines/functions/trap.sh
  
+
+/usr/sbin/krb5kdc -P /var/run/krb5kdc.pid -n &
+kpid=$! 
+/usr/sbin/kadmind -P /var/run/krb5admin.pid -nofork  &
+pid=$!
+echo -n " $pid" >> /var/run/krb5kdc.pid
+
+startup_complete
 if test -f /home/engines/run/flags/first_just_run
  then
+   sleep 5
+  . /home/engines/scripts/first_run/create_keys_func.sh  
+   create_init_keys
    rm /home/engines/run/flags/first_just_run
-else   
- /usr/sbin/krb5kdc -P /var/run/krb5kdc.pid -n &
- kpid=$! 
- /usr/sbin/kadmind -P /var/run/krb5admin.pid -nofork  &
- pid=$!
- echo -n " $pid" >> /var/run/krb5kdc.pid
- 
- startup_complete
-
- #sleep 5
- if test -f /home/engines/run/flags/first_run.done
-   then
-	 wait $kpid 	
-	 exit_code=$?
-	 /home/engines/scripts/signal/_kill_kerberos.sh TERM
- fi	
 fi 
+
+ wait $kpid 	
+ exit_code=$?
+ /home/engines/scripts/signal/_kill_kerberos.sh TERM	
+
 shutdown_complete
