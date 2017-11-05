76ce81fb0e0eb4af4337807c17c6f6e74e1b21d0
James L Vodanovich <james@jvodan.com>
2017-11-06 09:26:27 +1100

    log first run, dont start kerberos if post firstrun stop

--- images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 PID_FILE=/var/run/krb5kdc.pid 
 export PID_FILE
 
@@ -8,20 +7,25 @@ KILL_SCRIPT=/home/engines/scripts/signal/kill_kerberos.sh
 export KILL_SCRIPT
 
 . /home/engines/functions/trap.sh
-
-/usr/sbin/krb5kdc -P /var/run/krb5kdc.pid -n &
-kpid=$! 
-/usr/sbin/kadmind -P /var/run/krb5admin.pid -nofork  &
-pid=$!
-echo -n " $pid" >> /var/run/krb5kdc.pid
  
-startup_complete
+if test -f /home/engines/run/flags/first_just_run
+ then
+   rm /home/engines/run/flags/first_just_run
+else   
+ /usr/sbin/krb5kdc -P /var/run/krb5kdc.pid -n &
+ kpid=$! 
+ /usr/sbin/kadmind -P /var/run/krb5admin.pid -nofork  &
+ pid=$!
+ echo -n " $pid" >> /var/run/krb5kdc.pid
+ 
+ startup_complete
 
-#sleep 5
-if test -f /home/engines/run/flags/first_run.done
-  then
-	wait $kpid 	
-	exit_code=$?
-	/home/engines/scripts/signal/_kill_kerberos.sh TERM
-fi	
-    shutdown_complete
+ #sleep 5
+ if test -f /home/engines/run/flags/first_run.done
+   then
+	 wait $kpid 	
+	 exit_code=$?
+	 /home/engines/scripts/signal/_kill_kerberos.sh TERM
+ fi	
+fi 
+shutdown_complete
