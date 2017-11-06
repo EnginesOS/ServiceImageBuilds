dd7da02f6a0f9301351a8690e3c6ebe3ca452ed2
James L Vodanovich <james@jvodan.com>
2017-11-06 15:38:54 +1100

    correct certname

--- images/05.SystemApplications/control/home/start.sh
+++ images/05.SystemApplications/control/home/start.sh
@@ -5,7 +5,7 @@ export PID_FILE
 . /home/engines/functions/trap.sh
 /home/deploy.sh
 cd /home/app/
-bundle exec thin --threaded --ssl --ssl-key-file /engines/ssl/keys/engines.key --ssl-cert-file /engines/ssl/certs/engines.crt -C /home/config.yaml -R /home/config.ru start &
+bundle exec thin --threaded --ssl --ssl-key-file /engines/ssl/keys/control.key --ssl-cert-file /engines/ssl/certs/control.crt -C /home/config.yaml -R /home/config.ru start &
 
 touch /home/engines/run/flags/startup_complete 
 wait 
