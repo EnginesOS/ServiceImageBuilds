3e9e9b8148d42d369c88054dd37be9cbe4b0ed52
James L Vodanovich <james@jvodan.com>
2017-11-07 14:01:27 +1100

    read ca resturns certs setup not cert

--- images/03.ServiceImages/certs/home/engines/scripts/configurators/read_system_ca.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/configurators/read_system_ca.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat /home/certs/store/public/ca/certs/system_CA.pem 
-#/home/engines/scripts/configurators/saved/ca_params
+#cat /home/certs/store/public/ca/certs/system_CA.pem 
+cat /home/engines/scripts/configurators/saved/ca_params
