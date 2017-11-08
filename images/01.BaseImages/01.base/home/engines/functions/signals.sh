a433751e47a446a92713b72e11035aaeaefefd6e
James L Vodanovich <james@jvodan.com>
2017-11-08 15:45:24 +1100

    reinstate custom hup script

--- images/01.BaseImages/01.base/home/engines/functions/signals.sh
+++ images/01.BaseImages/01.base/home/engines/functions/signals.sh
@@ -3,32 +3,31 @@ kill_syslog()
 {
 if test -f /home/engines/etc/SYSLOG
  then
-   sudo -n  /home/engines/scripts/_kill_syslog.sh
+   sudo -n /home/engines/scripts/_kill_syslog.sh
 fi
 }
 
 wait_for_pid_exit()
 {
 kill -0 $pid &>/dev/null
-
- if test $? -eq 0
-  then
-   count=30
-   n=0
-   kill -0 $pid &>/dev/null
-    while test $? -eq 0
-     do
-      if test $count -lt $n
-       then
-        echo timeout shutting down $CONTAINER_NAME >> /home/engines/run/errors
-        break
-       fi
-       n=`expr $n + 1` 
-      sleep 1
-      kill -0 $pid &>/dev/null
-     done
- fi  
- }
+if test $? -eq 0
+ then
+  count=30
+  n=0
+  kill -0 $pid &>/dev/null
+   while test $? -eq 0
+    do
+     if test $count -lt $n
+      then
+       echo timeout shutting down $CONTAINER_NAME >> /home/engines/run/errors
+       break
+      fi
+      n=`expr $n + 1` 
+     sleep 1
+     kill -0 $pid &>/dev/null
+    done
+fi  
+}
  
 rm_pid_file()
 {
@@ -46,47 +45,63 @@ if test -z $PID_FILES
 fi
    
  for PID_FILE in $PID_FILES
-  do
-   if test -f $PID_FILE  
-    then
-     pids=`cat $PID_FILE`
-      for pid in $pids
-       do
-        kill -0 $pid	
-         if test $? -eq 0
-          then
-   	        kill -$SIGNAL $pid	
-   	        echo "-$SIGNAL $pid" >>  /home/engines/run/flags/signals   
-             if ! test $SIGNAL = HUP
-              then 
-                echo wait $pid >>  /home/engines/run/flags/signals
-                wait_for_pid_exit   
-                rm_pid_file
-             fi
-         else
-            rm_pid_file    
-         fi
-       done	 
-   fi
- done 
+do
+ if test -f $PID_FILE  
+  then
+   pids=`cat $PID_FILE`
+    for pid in $pids
+     do
+      kill -0 $pid	
+       if test $? -eq 0
+        then
+ 	       kill -$SIGNAL $pid	
+ 	       echo "-$SIGNAL $pid" >>  /home/engines/run/flags/signals   
+          if ! test $SIGNAL = HUP
+           then 
+             echo wait $pid >>  /home/engines/run/flags/signals
+             wait_for_pid_exit   
+             rm_pid_file
+          fi
+       else
+          rm_pid_file    
+       fi
+     done	 
+ fi
+done 
 }
  
 process_signal()
 {
 done=0
 
-if ! test -z $KILL_SCRIPT
+if test $SIGNAL = HUP
  then
-  echo  termed $KILL_SCRIPT $SIGNAL >> /home/engines/run/flags/signals
-   if test -f  $KILL_SCRIPT
+if ! test -z $HUP_SCRIPT
     then
-     echo  $KILL_SCRIPT $SIGNAL >> /home/engines/run/flags/signals
-     $KILL_SCRIPT $SIGNAL
-     done=1
-   else  
-     echo missing Script $KILL_SCRIPT >> /home/engines/run/flags/signals
-   fi
-fi     	
+     echo  termed $HUP_SCRIPT $SIGNAL >> /home/engines/run/flags/signals
+      if test -f $HUP_SCRIPT
+       then
+        echo  $HUP_SCRIPT $SIGNAL >> /home/engines/run/flags/signals
+        $HUP_SCRIPT 
+        done=1
+      else  
+        echo Missing script $HUP_SCRIPT >> /home/engines/run/flags/signals
+      fi
+   fi     	   
+else
+   if ! test -z $KILL_SCRIPT
+    then
+     echo  termed $KILL_SCRIPT $SIGNAL >> /home/engines/run/flags/signals
+      if test -f $KILL_SCRIPT
+       then
+        echo  $KILL_SCRIPT $SIGNAL >> /home/engines/run/flags/signals
+        $KILL_SCRIPT $SIGNAL
+        done=1
+      else  
+        echo Missing script $KILL_SCRIPT >> /home/engines/run/flags/signals
+      fi
+   fi     	
+fi
 
 if test $done -eq 0
  then
