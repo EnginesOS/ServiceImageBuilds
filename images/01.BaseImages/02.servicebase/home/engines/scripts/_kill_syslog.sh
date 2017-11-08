863faa0ab5b67911a8ea030159b7d78f2d0df0a1
James L Vodanovich <james@jvodan.com>
2017-11-08 15:20:42 +1100

    dbg

--- images/01.BaseImages/02.servicebase/home/engines/scripts/_kill_syslog.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/_kill_syslog.sh
@@ -2,13 +2,14 @@
 
 if test -f /var/run/rsyslogd.pid  
 	then
-		pid=`cat  /var/run/rsyslogd.pid  `
+		pid=`cat /var/run/rsyslogd.pid`
 		kill -TERM  $pid 
+		 echo kill -TERM  $pid syslog >>  /home/engines/run/flags/signals 
 		kill -0 $pid &>/dev/null
  	  while test $? -eq 0
         do             
 	      sleep 1 # cant wait on pid as not a child of this shell
-	      echo wait on syslog >>  /home/engines/run/flags/signals 
+	      echo wait on syslog $pid >>  /home/engines/run/flags/signals 
 	      kill -0 $pid &>/dev/null
 	  done 	
 	  
