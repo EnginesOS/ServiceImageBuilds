1bc631a2e56046f11107b48ef4c40eebad139b2b
James L Vodanovich <james@jvodan.com>
2017-11-08 19:34:40 +1100

    drop db

--- images/01.BaseImages/02.servicebase/home/engines/scripts/_start_syslog.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/_start_syslog.sh
@@ -5,5 +5,3 @@ if test -f /var/run/rsyslogd.pid
 fi 
 rsyslogd  -i /var/run/rsyslogd.pid 
 echo $!> /var/run/rsyslogd.pid 
-echo started rsysog pid $!>>  /home/engines/run/flags/signals 
-echo With record pid as `cat  /var/run/rsyslogd.pid`  >>  /home/engines/run/flags/signals 
