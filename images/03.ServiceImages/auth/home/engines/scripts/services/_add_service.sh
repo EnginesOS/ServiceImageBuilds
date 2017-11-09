8db77236fb7b01c1a899fbd0f8aa4115a4c775c3
James L Vodanovich <james@jvodan.com>
2017-11-09 20:18:01 +1100

    container_type container now app

--- images/03.ServiceImages/auth/home/engines/scripts/services/_add_service.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/services/_add_service.sh
@@ -7,8 +7,8 @@ r=$?
   then
    echo "Failed to create principle  host/$parent_engine.engines.internal"
  else 
-  mkdir /etc/krb5kdc/services/$parent_engine 
-  echo ktadd -k /etc/krb5kdc/services/$parent_engine/$parent_engine.keytab host/$parent_engine.engines.internal@ENGINES.INTERNAL | kadmin.local
+  mkdir -p /etc/krb5kdc//${container_type}s/$parent_engine 
+  echo ktadd -k /etc/krb5kdc/${container_type}s/$parent_engine/$parent_engine.keytab host/$parent_engine.engines.internal@ENGINES.INTERNAL | kadmin.local
   r=$?
    if test $r -ne 0
   then
