a806b02e82880759e949a7c4accf49401bcf8948
James L Vodanovich <james@jvodan.com>
2017-11-20 15:24:42 +1100

    rm cp err

--- images/03.ServiceImages/nginx/home/engines/scripts/configurators/read_default_site.sh
+++ images/03.ServiceImages/nginx/home/engines/scripts/configurators/read_default_site.sh
@@ -8,22 +8,4 @@ if test -f /home/engines/scripts/configurators/saved/default_site_url
 
 fi
 
-exit 0#!/bin/bash
-
-. /home/engines/functions/params_to_env.sh
-PARAMS_FILE=/home/engines/scripts/configurators/saved/default_site_url
-parms_to_file_and_env
-
-required_values="default_site_url"
-check_required_values    
-  
-if test  ${#default_site_url} -gt 4
- then  
-  cat /etc/nginx/templates/default_site.tmpl | sed "/FQDN/s//$default_site_url/" > /tmp/site.fqdn
-  cp /tmp/site.fqdn /etc/nginx/sites-enabled/default
-else
-  cp /etc/nginx/templates/empty_default /etc/nginx/sites-enabled/default 	
-fi
-
-kill -HUP `cat /run/nginx/nginx.pid`
 exit 0
