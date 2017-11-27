05b536d80ae2a4822543f3a72d7d3829ece2d270
James L Vodanovich <james@jvodan.com>
2017-11-27 19:53:17 +1100

    f

--- images/03.ServiceImages/auth/home/engines/scripts/backup/_restore.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/backup/_restore.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
-cd /
-tar -xpf - kdb5_util load -update - 2>/tmp/tar.errs
+#!/bin/sh
+
+cat - | kdb5_util load -update - 2>/tmp/tar.errs
+r=$?
+if test $r -ne 0
+ then
+  echo /tmp/tar.errs 
+  exit $r
+fi  
 
