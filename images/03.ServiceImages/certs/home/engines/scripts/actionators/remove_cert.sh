94ab1d27c7bebd661d4739ad5749298ff3d2534d
James L Vodanovich <james@jvodan.com>
2017-12-04 09:00:48 +1100

    fix scrpit name fr remove

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/remove_cert.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/remove_cert.sh
@@ -10,7 +10,7 @@ check_required_values
 if ! test -f /home/certs/store/public/certs/$store/${cert_name}.crt 
  then
  	 echo "Missing  $store/$cert_name"
-       exit 255
+       exit 127
     fi
 
 domain_name=`cat /home/certs/store/public/certs/$store/${cert_name}.crt  | openssl x509 -noout -subject  |sed "/^.*CN=/s///"| sed "/\*/s///"`
@@ -20,21 +20,21 @@ domain_name=`cat /home/certs/store/public/certs/$store/${cert_name}.crt  | opens
      if test $? -ne 0
      then
        echo "Failed to Delete Cert $cert_name"
-       exit 255
+       exit 127
     fi
     
-   sudo -n /home/engines/scripts/engines/_remove.sh keys/$store/${cert_name}.key
+   sudo -n /home/engines/scripts/engines/_remove_cert.sh keys/$store/${cert_name}.key
    
      if test $? -ne 0
      then
        echo "Failed to Delete Key $cert_name"
-       exit 255
+       exit 127
     fi
     
 if test -f /home/certs/store/services/wap/certs/${domain_name}.crt
  then
-  sudo -n /home/engines/scripts/engines/_remove.sh service wap/certs/${domain_name}.crt
-  sudo -n /home/engines/scripts/engines/_remove.sh service wap/keys/${domain_name}.key
+  sudo -n /home/engines/scripts/engines/_remove_cert.sh service wap/certs/${domain_name}.crt
+  sudo -n /home/engines/scripts/engines/_remove_cert.sh service wap/keys/${domain_name}.key
 fi
 
 exit 0
