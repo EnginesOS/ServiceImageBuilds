de22081ec0c45d1a4567b3388706fe565629327e
James L Vodanovich <james@jvodan.com>
2017-12-04 08:54:49 +1100

    cert_name defaults to domain name

--- images/03.ServiceImages/certs/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/services/add_service.sh
@@ -4,6 +4,10 @@
 . /home/engines/functions/params_to_env.sh
 params_to_env
 
+if test -z cert_name
+ then
+  cert_name=$domain_name
+fi
 
 required_values="cert_name container_type parent_engine domain_name country state city organisation person"
 check_required_values
