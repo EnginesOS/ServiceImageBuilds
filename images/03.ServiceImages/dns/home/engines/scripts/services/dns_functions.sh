3ddf3cf5e0a2a2b5832c15c6f5fc86a863556475
James L Vodanovich <james@jvodan.com>
2017-12-15 10:28:52 +1100

    support custom records

--- images/03.ServiceImages/dns/home/engines/scripts/services/dns_functions.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/services/dns_functions.sh
@@ -13,22 +13,30 @@ fqdn_str=${host}.engines.internal
     
 dns_cmd_file=`mktemp`
    
-if test -z ${ip}
+if test -z $record_type
  then
+  if test -z ${ip}
+   then
     update_line=" update add $fqdn_str 30 CNAME ${engine}.engines.internal"
- else
-   update_line=" update add $fqdn_str 30 A $ip"        
-fi  
+   else
+     update_line=" update add $fqdn_str 30 A $ip"        
+  fi  
+elif test $record_type = custom
+ then
+    no_inarpra=y
+   update_line="update add $fqdn_str $record"
+fi 
 
-ip_reversed=`echo $ip |awk  ' BEGIN {  FS="."} {print $4 "." $3 "." $2 "." $1}'`
     
 echo server 127.0.0.1 > $dns_cmd_file
 echo update delete $fqdn_str >> $dns_cmd_file
 echo send >> $dns_cmd_file
 echo $update_line >> $dns_cmd_file
 echo send >> $dns_cmd_file
+
 if test -z $no_inarpra 
  then
+    ip_reversed=`echo $ip |awk  ' BEGIN {  FS="."} {print $4 "." $3 "." $2 "." $1}'`
 	echo update add ${ip_reversed}.in-addr.arpa. 30 PTR $fqdn_str >> $dns_cmd_file
 	echo send >> $dns_cmd_file
 fi
