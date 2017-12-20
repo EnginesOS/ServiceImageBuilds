730e3f5fe5e719f1ce68c4c160b984920fc3f039
James L Vodanovich <james@jvodan.com>
2017-12-20 15:46:50 +1100

    correct use of keys

--- images/03.ServiceImages/ftp/home/engines/scripts/services/rm_service.sh
+++ images/03.ServiceImages/ftp/home/engines/scripts/services/rm_service.sh
@@ -4,17 +4,13 @@
 . /home/engines/functions/params_to_env.sh
 params_to_env
 set > /tmp/full_env
-kinit -t /etc/krb5kdc/keys/ftp.keytab 
+
 
 required_values="service_handle"
 check_required_values
 
+export service_handle
+sudo -n /home/engines/scripts/servics/_rm_service.sh
 
-dn="cn=$service_handle,ou=ftp,ou=Service Accounts,ou=Engines,dc=engines,dc=internal"
-uid=`ldapsearch "$dn" |grep -i uid: | awk '{print $2'}`
-ldapdelete "$dn" -H ldap://ldap/
- 
-/home/engines/scripts/services/del_from_ftp_group.sh $uid
- #dont leave ticket open
-kdestroy
+
   
