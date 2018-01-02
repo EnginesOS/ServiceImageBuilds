3d9d8d3a139658ee80151b39596f8312c2626406
James L Vodanovich <james@jvodan.com>
2018-01-03 08:13:07 +1100

    debug

--- images/05.SystemApplications/fsconfigurator/home/setup_engine.sh
+++ images/05.SystemApplications/fsconfigurator/home/setup_engine.sh
@@ -6,19 +6,22 @@ echo Setup Engine user $fw_user
 #VOLUME /home/fs
 #VOLUME /dest/fs
 
-echo Contents of /client/var/log
+echo Contents of /client/var/log >> /client/var/log/fs_setup.log
 ls /client/var/log > /client/var/log/fs_setup.log
 
-echo Contents of /client/log 
+echo Contents of /client/log  >> /client/var/log/fs_setup.log
+
 ls /client/log >> /client/var/log/fs_setup.log
 
-echo Contents of /client/state
+echo Contents of /client/state >> /client/var/log/fs_setup.log
 ls /client/state >> /client/var/log/fs_setup.log
 
-echo Contents of dest/fs
+echo Contents of dest/fs >> /client/var/log/fs_setup.log
+
 ls /dest/fs >> /client/var/log/fs_setup.log
 
-echo Contents of /home/fs
+echo Contents of /home/fs >> /client/var/log/fs_setup.log
+
 ls /home/fs >> /client/var/log/fs_setup.log
 
 
@@ -41,21 +44,33 @@ chmod g+w  -R /client/state
 	echo "moving fs src "  >> /client/var/log/fs_setup.log
 	ls /dest/fs/  >> /client/var/log/fs_setup.log
 	
+	for file in `cat /home/fs_src/vol_dir_maps`
+	 do
+	  volume=`grep "$file " /home/fs_src/vol_dir_maps | awk '{print $2}'`
+	  echo Install dir $file in /dest/fs/$volume >>/home/fs_src/test.out
+	  ls  /dest/fs/ >>/home/fs_src/test.out
+	 done
+	 for file in `cat /home/fs_src/vol_file_maps`
+	 do
+	  volume=`grep "$file " /home/fs_src/vol_dir_maps | awk '{print $2}'`
+	  echo Install file $file in /dest/fs/$volume >>/home/fs_src/test.out
+	  ls  /dest/fs/ >>/home/fs_src/test.out
+	 done
 	for dest_dir in `ls /dest/fs/`
 	 do	 
-	 if test -f /dest/fs/$dest_dir/.persistent_lock
+	   if test -f /dest/fs/$dest_dir/.persistent_lock
  		then
- 		echo "Already persistent $dest_dir"  >> /client/var/log/fs_setup.log
+ 		 echo "Already persistent $dest_dir"  >> /client/var/log/fs_setup.log
  		 chown -R $fw_user /dest/fs/$dest_dir
-  		  chmod g+w -R  /dest/fs/$dest_dir
- 		continue
+  		 chmod g+w -R  /dest/fs/$dest_dir
+ 		 continue
  		fi
-	   src_dir=`echo $dest_dir | sed "/_/s//\//g" | sed " /\/home\/fs/s//\/home\/fs_src/" `
-	   cp -rpn $src_dir/. /dest/fs/$dest_dir
-	   	   touch /dest/fs/$dest_dir/.persistent_lock
-	   chown -R ${fw_user}.${data_gid}  /dest/fs/$dest_dir
-	   echo "setup $dest_dir" >> /client/var/log/fs_setup.log
-	 done
+	 src_dir=`echo $dest_dir | sed "/_/s//\//g" | sed " /\/home\/fs/s//\/home\/fs_src/" `
+	 cp -rpn $src_dir/. /dest/fs/$dest_dir
+	 touch /dest/fs/$dest_dir/.persistent_lock
+	 chown -R ${fw_user}.${data_gid}  /dest/fs/$dest_dir
+	 echo "setup $dest_dir" >> /client/var/log/fs_setup.log
+	done
 
 	#if no presistance dirs/files need to set permission here
 	
@@ -71,7 +86,8 @@ chmod g+w  -R /client/state
 			echo "Setup app persist"  >> /client/var/log/fs_setup.log
     fi
 
-sleep 30
+
+sleep 300
 touch /client/state/flags/volume_setup_complete
 echo setup complete   >> /client/var/log/fs_setup.log
  exit 0
