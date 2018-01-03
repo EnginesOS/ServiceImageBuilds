15a94c9cd01f6fe6d8dd97d3a9dddf27f72d52df
James L Vodanovich <james@jvodan.com>
2018-01-03 11:32:20 +1100

    fix logging dest

--- images/05.SystemApplications/fsconfigurator/home/setup_engine.sh
+++ images/05.SystemApplications/fsconfigurator/home/setup_engine.sh
@@ -1,39 +1,39 @@
 #!/bin/sh -x
 echo Setup Engine user $fw_user
-#VOLUME /client/var/log
+#VOLUME /client/log
 #VOLUME /client/log
 #VOLUME /client/state
 #VOLUME /home/fs
 #VOLUME /dest/fs
 
-echo Contents of /client/var/log >> /client/var/log/fs_setup.log
-ls /client/var/log > /client/var/log/fs_setup.log
+echo Contents of /client/log >> /client/log/fs_setup.log
+ls /client/log > /client/log/fs_setup.log
 
-echo Contents of /client/log  >> /client/var/log/fs_setup.log
+echo Contents of /client/log  >> /client/log/fs_setup.log
 
-ls /client/log >> /client/var/log/fs_setup.log
+ls /client/log >> /client/log/fs_setup.log
 
-echo Contents of /client/state >> /client/var/log/fs_setup.log
-ls /client/state >> /client/var/log/fs_setup.log
+echo Contents of /client/state >> /client/log/fs_setup.log
+ls /client/state >> /client/log/fs_setup.log
 
-echo Contents of dest/fs >> /client/var/log/fs_setup.log
+echo Contents of dest/fs >> /client/log/fs_setup.log
 
-ls /dest/fs >> /client/var/log/fs_setup.log
+ls /dest/fs >> /client/log/fs_setup.log
 
-echo Contents of /home/fs >> /client/var/log/fs_setup.log
+echo Contents of /home/fs >> /client/log/fs_setup.log
 
-ls /home/fs >> /client/var/log/fs_setup.log
+ls /home/fs >> /client/log/fs_setup.log
 
 
 logs=`ls /var/log/`
 echo logs
 for log in $logs
  do
-	cp -rp /var/log/$log  /client/var/log
+	cp -rp /var/log/$log  /client/log
 done
 
 chown $fw_user -R /client/log/
-chown $fw_user -R /client/var/log
+chown $fw_user -R /client/log
 mkdir -p /client/state/flags
 chown $fw_user -R /client/state
 chgrp 22020  -R /client/state
@@ -41,26 +41,26 @@ chmod g+w  -R /client/state
 
 
 	cd /home/fs_src/
-	echo "moving fs src "  >> /client/var/log/fs_setup.log
-	ls /dest/fs/  >> /client/var/log/fs_setup.log
+	echo "moving fs src "  >> /client/log/fs_setup.log
+	ls /dest/fs/  >> /client/log/fs_setup.log
 	
 	for file in `cat /home/fs_src/vol_dir_maps`
 	 do
 	  volume=`grep "$file " /home/fs_src/vol_dir_maps | awk '{print $2}'`
-	  echo Install dir $file in /dest/fs/$volume >>/home/fs_src/test.out
-	  ls  /dest/fs/ >>/home/fs_src/test.out
+	  echo Install dir $file in /dest/fs/$volume >>/client/var/test.out
+	  ls  /dest/fs/ >>/client/var/test.out
 	 done
 	 for file in `cat /home/fs_src/vol_file_maps`
 	 do
 	  volume=`grep "$file " /home/fs_src/vol_dir_maps | awk '{print $2}'`
-	  echo Install file $file in /dest/fs/$volume >>/home/fs_src/test.out
-	  ls  /dest/fs/ >>/home/fs_src/test.out
+	  echo Install file $file in /dest/fs/$volume >>/client/var/test.out
+	  ls  /dest/fs/ >>/client/var/test.out
 	 done
 	for dest_dir in `ls /dest/fs/`
 	 do	 
 	   if test -f /dest/fs/$dest_dir/.persistent_lock
  		then
- 		 echo "Already persistent $dest_dir"  >> /client/var/log/fs_setup.log
+ 		 echo "Already persistent $dest_dir"  >> /client/log/fs_setup.log
  		 chown -R $fw_user /dest/fs/$dest_dir
   		 chmod g+w -R  /dest/fs/$dest_dir
  		 continue
@@ -69,7 +69,7 @@ chmod g+w  -R /client/state
 	 cp -rpn $src_dir/. /dest/fs/$dest_dir
 	 touch /dest/fs/$dest_dir/.persistent_lock
 	 chown -R ${fw_user}.${data_gid}  /dest/fs/$dest_dir
-	 echo "setup $dest_dir" >> /client/var/log/fs_setup.log
+	 echo "setup $dest_dir" >> /client/log/fs_setup.log
 	done
 
 	#if no presistance dirs/files need to set permission here
@@ -83,11 +83,11 @@ chmod g+w  -R /client/state
 			cp -rp /home/app_src/.  /dest/fs/_home_app_/			
 			chown -R ${fw_user}.${data_gid}  /dest/fs/_home_app_/			
 			touch /dest/fs/_home_app_/.persistent
-			echo "Setup app persist"  >> /client/var/log/fs_setup.log
+			echo "Setup app persist"  >> /client/log/fs_setup.log
     fi
 
 
 sleep 300
 touch /client/state/flags/volume_setup_complete
-echo setup complete   >> /client/var/log/fs_setup.log
+echo setup complete   >> /client/log/fs_setup.log
  exit 0
