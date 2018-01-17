ae845c759b7f8084d90fc5cf42a83bcf05edd850
James L Vodanovich <james@jvodan.com>
2018-01-17 12:55:15 +1100

    dbg

--- images/03.ServiceImages/volmgr/home/engines/scripts/services/_delete_volume.sh
+++ images/03.ServiceImages/volmgr/home/engines/scripts/services/_delete_volume.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-rm -r  /var/fs/local/${parent_engine}/${service_name}
+if test -d /var/fs/local/${parent_engine}/${service_name} 
+ then
+	rm -r  /var/fs/local/${parent_engine}/${service_name}
+  else
+  	echo "No such dir /var/fs/local/${parent_engine}/${service_name}
+  fi
+exit 0	
 
 
