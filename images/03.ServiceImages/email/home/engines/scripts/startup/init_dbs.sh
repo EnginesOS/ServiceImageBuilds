18b9f08a79e664fde51b9f7d31d88326addea5dc
James L Vodanovich <james@jvodan.com>
2018-01-19 09:29:50 +1100

    setup aliases

--- images/03.ServiceImages/email/home/engines/scripts/startup/init_dbs.sh
+++ images/03.ServiceImages/email/home/engines/scripts/startup/init_dbs.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-for map_file in transport generic smarthost_passwd 
+for map_file in transport generic smarthost_passwd aliases/aliases
  do 
   if ! test -f /etc/postfix/${map_file}.db
    then
     sudo -n /home/engines/scripts/engine/_postmap.sh ${map_file}
   fi  
  done
- 
-if ! test -d  /etc/postfix/maps/aliases/
- then
-   mkdir /etc/postfix/maps/aliases/
-fi
-
-if ! test -f /etc/postfix/maps/aliases/aliases
-   then
-    touch /etc/postfix/maps/aliases/aliases
-fi
-
-sudo -n /home/engines/scripts/engine/_postmap.sh aliases/aliases
-
-chown postfix /etc/postfix/maps/aliases/
-
