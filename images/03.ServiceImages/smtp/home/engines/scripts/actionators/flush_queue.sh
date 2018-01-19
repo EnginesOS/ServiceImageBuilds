f2f85bd81f775b93216c1fd5c23ef77f8002f6c7
James L Vodanovich <james@jvodan.com>
2018-01-19 11:11:47 +1100

    tidy

--- images/03.ServiceImages/smtp/home/engines/scripts/actionators/flush_queue.sh
+++ images/03.ServiceImages/smtp/home/engines/scripts/actionators/flush_queue.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-postqueue -f &> /tmp/err
+fq=`mktemp`
+postqueue -f &> $fq
+
 result=$?
-string_for_json=`cat /tmp/err`
-rm /tmp/err
+
+string_for_json=`cat $fq`
+
+rm $fq
+
 string_for_json=`echo $string_for_json | tr -d "'\r\"\t\f\b\n\v"`
+
 if test $result -eq 0
  then
   echo '{"Result":"OK"}'
-  else
-   echo '{"Result":"FAILED","ReturnCode":"'$result'","Error":"'$string_for_json'"}'
-  
-  fi 
+else
+ echo '{"Result":"FAILED","ReturnCode":"'$result'","Error":"'$string_for_json'"}'
+fi 
