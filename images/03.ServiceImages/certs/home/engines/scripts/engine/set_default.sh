3219a34028c196f23ff73bf4e2e946d866ca1a9b
James L Vodanovich <james@jvodan.com>
2018-01-22 10:48:59 +1100

    refac

--- images/03.ServiceImages/certs/home/engines/scripts/engine/set_default.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/engine/set_default.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-#set_default.sh ${target} ${cert_type} ${cert_name} 
+#set_default.sh ${install_target} ${cert_type} ${cert_name} 
+
+function install_service {
+
+dest_name=`basename $install_target`
+err=`sudo -n  /home/engines/scripts/engine/_install_target.sh ${install_target} ${cert_type} ${cert_name} ${install_target}
+r=$?
+ if $r -ne 0
+  then
+  	echo '{"Result":"Failed","ErrorMesg":"'$err'","ExitCode":"'$r'"}'
+fi
+exit $r
+
+}
+
+targets="system_services/system services/smtp services/ftp services/emailservices/ services/mysql services/pgsql services/mgmt services/wap services/control"
+case $install_target in $targets
+
+all)
+   for install_target in $targets
+    do    
+      install_service
+    done  
+  ;;
+*)
+   install_service
+   ;;
+esac
