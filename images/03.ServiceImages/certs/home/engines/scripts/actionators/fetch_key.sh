bd473df741bf4be4f7c75f3e0100efa954ec38e3
James L Vodanovich <james@jvodan.com>
2018-01-26 09:34:22 +1100

    change order

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/fetch_key.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/fetch_key.sh
@@ -4,17 +4,19 @@ params_to_env
 
 required_values="key_name store cert_type"
 check_required_values
-if test $cert_type = user
- then
-  cert_type=generated 
-  store=user
-fi  
+ 
 
 if test $cert_type = imported -o $cert_type = generated
  then
   echo "Cannot export key from cert type $cert_type"
   exit
 fi  
+
+if test $cert_type = user
+ then
+  cert_type=generated 
+  store=user
+fi 
 
 if test -f /home/certs/store/$cert_type/keys/${store}/${key_name}.key
   then
