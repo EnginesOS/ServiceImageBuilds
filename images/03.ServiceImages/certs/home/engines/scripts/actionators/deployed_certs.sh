1d2616da94f3533e0ec4848f5550396d2cbe2e09
James L Vodanovich <james@jvodan.com>
2018-01-26 10:56:16 +1100

    "" not []

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/deployed_certs.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/deployed_certs.sh
@@ -2,38 +2,44 @@
 
 function find_certs 
 {
-echo '['
-for consumer in `ls /home/certs/store/live/$consumer_type_path/`
- do
-  certs=`find /home/certs/store/live/$consumer_type_path/$consumer/certs/ -name "*.crt" |sed "s/\.crt//"`
- for cert_name in $certs 
+n=0
+if ! test -d /home/certs/store/live/$consumer_type_path/
+ then
+   echo '""' 
+else
+  echo '['
+  for consumer in `ls /home/certs/store/live/$consumer_type_path/`
    do
-     cert_name=`basename $cert_name`
-     domain=`cat /home/certs/store/live/$consumer_type_path/$consumer/certs/$cert_name.crt \
-     | openssl x509 -noout -subject |sed "/^.*CN=/s///" | sed "/\*/s///" `
-  
-    if test $n = 1
+    certs=`find /home/certs/store/live/$consumer_type_path/$consumer/certs/ -name "*.crt" |sed "s/\.crt//"`
+   for cert_name in $certs 
+     do
+       cert_name=`basename $cert_name`
+       domain=`cat /home/certs/store/live/$consumer_type_path/$consumer/certs/$cert_name.crt \
+       | openssl x509 -noout -subject |sed "/^.*CN=/s///" | sed "/\*/s///" `
+    
+      if test $n -eq 1
+        then
+         echo -n ,
+      fi
+     if test -f /home/certs/store/live/$consumer_type_path/${consumer}/certs/store.$cert_name
       then
-       echo -n ,
-    fi
-   if test -f /home/certs/store/live/$consumer_type_path/${consumer}/certs/store.$cert_name
-    then
-    	store=`cat /home/certs/store/live/$consumer_type_path/${consumer}/certs/store.$cert_name`
-    	else 
-    	 store='""'
-    fi
-     if test $consumer = $cert_name
-      then
-       default=true
-      else
-       default=false
-     fi   
-     consumer_type=`echo $consumer_type_path | sed "s/s$//"`
-   echo -n '{"consumer":'$consumer'","consumer_type":"'$consumer_type'","cert_name":"'$domain'","store":'$store',"default":"'$default'"}'
-   n=1
- done
-done
-echo ']'
+      	store=`cat /home/certs/store/live/$consumer_type_path/${consumer}/certs/store.$cert_name`
+      	else 
+      	 store='""'
+      fi
+       if test $consumer = $cert_name
+        then
+         default=true
+        else
+         default=false
+       fi   
+       consumer_type=`echo $consumer_type_path | sed "s/s$//"`
+     echo -n '{"consumer":'$consumer'","consumer_type":"'$consumer_type'","cert_name":"'$domain'","store":'$store',"default":"'$default'"}'
+     n=1
+   done
+  done
+  echo ']'
+fi
 }
 function apps_certs 
 {
