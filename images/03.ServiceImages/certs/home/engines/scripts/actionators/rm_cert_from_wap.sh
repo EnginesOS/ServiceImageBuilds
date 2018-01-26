8d1d969ee07e0632615ebbb303e05e28c9e18652
James L Vodanovich <james@jvodan.com>
2018-01-26 13:11:36 +1100

    clear store mao

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/rm_cert_from_wap.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/rm_cert_from_wap.sh
@@ -8,6 +8,7 @@ check_required_values
 if test -f /home/certs/store/live/services/wap/certs/${fqdn}.crt
  then
   sudo -n /home/engines/scripts/engine/_remove_cert.sh live/service wap/certs/${fqdn}.crt
+  sudo -n /home/engines/scripts/engine/_remove_cert.sh live/service wap/certs/store.${fqdn}
   sudo -n /home/engines/scripts/engine/_remove_cert.sh live/service wap/keys/${fqdn}.key
  else
    	echo '{"Result":"Failed","ErrorMesg":"No Such Cert '${fqdn}'","ExitCode":"127"}'
