441ef4cf70190b92719c3c7dccdb311356150cc5
James L Vodanovich <james@jvodan.com>
2018-01-27 13:48:45 +1100

    f shutdown

--- images/03.ServiceImages/ipsec/home/engines/scripts/signal/_shutdown.sh
+++ images/03.ServiceImages/ipsec/home/engines/scripts/signal/_shutdown.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-PID_FILE=/tmp/ipsec.pid
-
-. /home/engines/functions/signals.sh
-
-default_signal_processor
+/usr/sbin/ipsec stop
 exit 0
--- images/03.ServiceImages/ipsec/home/start.sh
+++ images/03.ServiceImages/ipsec/home/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PID_FILE=/tmp/ipsec.pid
+PID_FILE=/var/run/starter.charon.pid
 export PID_FILE
 KILL_SCRIPT=/home/engines/scripts/signal/shutdown.sh
 
@@ -10,7 +10,14 @@ export KILL_SCRIPT
 service_first_run_check
 
 
-sudo -n /home/engines/scripts/startup/_start.sh 
+sudo -n /home/engines/scripts/startup/_start.sh & 
 
 
+
+startup_complete 
+
+wait
+exit_code=$?
+
+shutdown_complete
 
