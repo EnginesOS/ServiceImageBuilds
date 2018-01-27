1a5d11053f41bba32f4c9042495dc14355c3ae0c
James L Vodanovich <james@jvodan.com>
2018-01-28 09:39:51 +1100

    fix restore

--- images/03.ServiceImages/mysql/home/engines/scripts/backup/restore.sh
+++ images/03.ServiceImages/mysql/home/engines/scripts/backup/restore.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+#usage -y|n {section}  
+#defaults to -n (delete existing data)
+# -y will merge
+ 
 echo $0 $1 $2 >/tmp/called
 replace=$1
 section=$2
@@ -7,26 +10,31 @@ section=$2
 #--replace
 if ! test -z $replace
  then
-  opts=
+  opts=--delete
+ elif test $replace = -y
+  then
+   opts=--replace
+  else
+    opts=--delete
 fi
+
 if ! test -z $section
  then
  opts="$opts --one-database $section"
  fi
  
-echo "cat /tmp/mysqld/backup.* | mysql -B $opts -h 127.0.0.1 -u rma  2> /tmp/mysqlimport.errs" >/tmp/restore.run
-#--delete
-rm -fr /tmp/mysqld
-tar -xpf - 2>/tmp/tar.errs
+cat - | mysqlimport -B $opts -h 127.0.0.1 -u rma  2> /tmp/mysqlimport.errs" >/tmp/restore.run
 
-cat /tmp/mysqld/backup.* | mysql -B $opts -h 127.0.0.1 -u rma 2> /tmp/mysqlimport.errs
+
+#cat /tmp/mysqld/backup.* | mysql -B $opts -h 127.0.0.1 -u rma 2> /tmp/mysqlimport.errs
 if test $? -ne 0
  then 
+   cat  /tmp/restore.run
    cat  /tmp/mysqlimport.errs >&2
-	rm -fr /tmp/mysqld
-   exit -1
+   exit 127
 fi
-rm -fr /tmp/mysqld
+
+
 
 mysqladmin -u rma flush-privileges
  
