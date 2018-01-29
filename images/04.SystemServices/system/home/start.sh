7abdbdb9ee543a8957159fe812e32b69e71b0065
James L Vodanovich <james@jvodan.com>
2018-01-29 11:39:54 +1100

    optional puma server

--- images/04.SystemServices/system/home/start.sh
+++ images/04.SystemServices/system/home/start.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+start_thin()
+{
+ if test -f /home/engines/etc/ssl/keys/system.key 
+  then
+	thin --threaded --ssl --ssl-key-file /home/engines/etc/ssl//keys/system.key --ssl-cert-file /home/engines/etc/ssl/certs/system.crt -C /home/app/config.yaml -R /home/app/config.ru start > /var/log/system.log &
+  else
+	thin --threaded -C /home/app/config.yaml -R /home/app/config.ru start > /var/log/system.log &
+ fi
+}
+
+start_puma()
+{  
+if test -f /home/engines/etc/ssl/keys/system.key 
+  then
+    options="-C /home/app/puma.rb"
+   else
+      options="-C /home/app/puma_nocert.rb"
+   fi   
+puma $options &
+
+}
 export RUBY_GC_MALLOC_LIMIT_GROWTH_FACTOR=1.1
 
 /home/engines/scripts/startup/clear_flags.sh
@@ -12,13 +33,12 @@ export PID_FILE
 . /home/engines/functions/trap.sh
 
 cd /home
-
- if test -f /home/engines/etc/ssl//keys/system.key 
-  then
-	thin --threaded --ssl --ssl-key-file /home/engines/etc/ssl//keys/system.key --ssl-cert-file /home/engines/etc/ssl/certs/system.crt -C /home/app/config.yaml -R /home/app/config.ru start > /var/log/system.log &
-  else
-	thin --threaded -C /home/app/config.yaml -R /home/app/config.ru start > /var/log/system.log &
- fi
+if test -f /home/engines/run/flags/puma
+ then
+  start_puma
+ else 
+  start_thin
+fi  
 
 #touch /home/engines/run/flags/startup_complete  done in code
 wait 
