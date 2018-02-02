91bdb47bb80656fab6652b79b511e663701b66ad
James L Vodanovich <james@jvodan.com>
2018-02-02 18:35:40 +1100

    -k

--- images/03.ServiceImages/cron/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/cron/home/engines/scripts/services/add_service.sh
@@ -16,16 +16,16 @@ fi
 
 if test $action_type = "web"
  then
-    cmd="curl http://${parent_engine}.engines.internal:8000$cron_job  -o /tmp/out"
+    cmd="curl http://${parent_engine}.engines.internal:8000$cron_job"
 elif test $action_type = "command"
  then
- 	cmd="curl https://172.17.0.1:2380/v0/cron/engine/${parent_engine}/$title/run"
+ 	cmd="curl -k https://172.17.0.1:2380/v0/cron/engine/${parent_engine}/$title/run"
 elif test $action_type = "schedule"
  then
-    cmd="curl https://172.17.0.1:2380/v0/schedule/${container_type}/${parent_engine}/$cron_job"
+    cmd="curl -k  https://172.17.0.1:2380/v0/schedule/${container_type}/${parent_engine}/$cron_job"
 elif test $action_type = "action"
  then
-    cmd="curl https://172.17.0.1:2380/v0/schedule/${container_type}/${parent_engine}/$title/run"
+    cmd="curl -k https://172.17.0.1:2380/v0/schedule/${container_type}/${parent_engine}/$title/run"
 fi
 
 echo "$when $cmd " > /home/cron/entries/${parent_engine}/$title
