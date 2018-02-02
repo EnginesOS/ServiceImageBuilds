fa75c18823b3528551d6800889134f7be9d7fd83
James L Vodanovich <james@jvodan.com>
2018-02-02 15:28:13 +1100

    o-rwx

--- images/03.ServiceImages/volmgr/home/engines/scripts/services/_create_volume.sh
+++ images/03.ServiceImages/volmgr/home/engines/scripts/services/_create_volume.sh
@@ -3,4 +3,4 @@
 mkdir -p /var/fs/local/${parent_engine}/${service_name}
 chown -R ${user}.${group} /var/fs/local/${parent_engine}/${service_name}
 chmod g+w /var/fs/local/${parent_engine}/${service_name}
-chmod o-rw /var/fs/local/${parent_engine}/${service_name}
+chmod o-rxw /var/fs/local/${parent_engine}/${service_name}
