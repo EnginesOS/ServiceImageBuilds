3cabb5cb883e2d6905b4980160c8abd9502712bf
James L Vodanovich <james@jvodan.com>
2018-02-02 18:58:40 +1100

    f

--- images/03.ServiceImages/certs/home/engines/scripts/services/rm_service.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/services/rm_service.sh
@@ -10,7 +10,7 @@ store=${container_type}s/${parent_engine}/
 if ! test -f /home/certs/store/generated/certs/$store/${cert_name}.crt 
  then
  	 echo "Missing Cert $store/$cert_name"
-     exit 127
+     exit 126
 fi
 
 domain_name=`cat /home/certs/store/generated/certs/$store/${cert_name}.crt  | openssl x509 -noout -subject  |sed "/^.*CN=/s///"| sed "/\*\./s///"`
@@ -27,7 +27,7 @@ fi
 if test $? -ne 0
  then
   echo "Failed to Delete Key $cert_name"
-  exit 127
+  exit 125
 fi
     
 if test -f /home/certs/store/services/wap/certs/${domain_name}.crt
