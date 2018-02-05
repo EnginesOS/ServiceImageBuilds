19b651e6faaae75f62cb40fb1effc61d3f3e282d
James L Vodanovich <james@jvodan.com>
2018-02-05 16:48:05 +1100

    rm log option as it removed logs

--- images/05.SystemApplications/control/home/start.sh
+++ images/05.SystemApplications/control/home/start.sh
@@ -16,7 +16,6 @@ bundle exec thin --threaded\
 	  --ssl-cert-file /home/engines/etc/ssl//certs/control.crt\
 	   -C /home/config.yaml \
 	   -R /home/config.ru \
-	   -l /var/log/control.log \
 	   start &
 echo -n $! $memcache_pid > /home/engines/run/control.pid 
 
