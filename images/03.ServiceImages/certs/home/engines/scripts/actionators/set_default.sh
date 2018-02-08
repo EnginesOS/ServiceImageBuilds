8597a06c859583fc04ae8a889ecad11a8dbaaac4
James L Vodanovich <james@jvodan.com>
2018-02-08 17:33:29 +1100

    provide for optional cert_src

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/set_default.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/set_default.sh
@@ -5,5 +5,9 @@ params_to_env
 required_values="common_name cert_type install_target"
 check_required_values
 echo "set default" /_install_target.sh ${install_target} ${cert_type} ${common_name}
-
+if ! test -z $cert_src
+ then
+  common_name=$cert_src/${common_name}
+fi
+ 
 /home/engines/scripts/engine/set_default.sh ${install_target} ${cert_type} ${common_name} 
