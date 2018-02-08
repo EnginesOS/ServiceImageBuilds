2d64f69ee62d45f64549079118e009336bb62aa2
James L Vodanovich <james@jvodan.com>
2018-02-08 20:29:23 +1100

    unsure all serivces have the system ca in their cert bundle

--- images/03.ServiceImages/mongo/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/mongo/home/engines/scripts/startup/start.sh
@@ -5,7 +5,11 @@ PID_FILE=/var/run/engines/mongodb.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
 
+
 mkdir -p /var/log/mongodb/
+
+service_first_run_check
+
  
 mongod -v  -f /etc/mongod.conf  --directoryperdb  --dbpath /data/db/  --journal &
 pid=$!
