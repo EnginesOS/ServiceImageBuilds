5e458e953b7366bdd1b4e760323a79ee180cd1d1
James L Vodanovich <james@jvodan.com>
2018-02-09 16:49:14 +1100

    correct dirs struct

--- images/03.ServiceImages/radius/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/radius/home/engines/scripts/startup/start.sh
@@ -3,10 +3,10 @@
 PID_FILE=/tmp/sleep.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
-
+ sleep 400
 
 startup_complete
-sudo /usr/sbin/freeradius  -fX &
+/usr/bin/freeradius  -fX &
 wait
 exit_code=$?
 
