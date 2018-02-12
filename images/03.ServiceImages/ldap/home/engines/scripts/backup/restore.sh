fb23c46aad152eac19f46a8c7888a14803f30030
James L Vodanovich <james@jvodan.com>
2018-02-12 14:21:18 +1100

    dbg

--- images/03.ServiceImages/ldap/home/engines/scripts/backup/restore.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/backup/restore.sh
@@ -3,14 +3,14 @@
 touch /home/engines/run/flags/backup
 exit_code=0
 
-cat -  | sudo -n /home/engines/scripts/backup/_restore.sh >& /tmp/restore.errs
+cat - | sudo -n /home/engines/scripts/backup/_restore.sh >& /tmp/restore.errs
 
 if test $? -ne 0
  then 
    cat  /tmp/restore.errs  >&2
-   exit_code=0
+   exit_code=1
 fi
 #KLUDGE FIX ME this is to give time to read buffers
-sleep 10
-rm  /home/engines/run/flags/backup
+#sleep 10
+#rm  /home/engines/run/flags/backup
 exit $exit_code
