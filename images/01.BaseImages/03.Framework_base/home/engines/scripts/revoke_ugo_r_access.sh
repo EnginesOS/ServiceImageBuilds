7660c7d107caf04b681805daea01df314e711821
James L Vodanovich <james@jvodan.com>
2018-02-22 11:12:43 +1100

    grant/revoke oug-r

---
+++ images/01.BaseImages/03.Framework_base/home/engines/scripts/revoke_ugo_r_access.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if test $# -eq 2
+ then
+   entered_path=$2
+   recursive=-R
+else
+	entered_path=$1
+	recursive=""
+fi
+
+path=`echo $entered_path |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
+sudo -n -u data-user /home/engines/scripts/_revoke_ugo_rw_access.sh $recursive $path
