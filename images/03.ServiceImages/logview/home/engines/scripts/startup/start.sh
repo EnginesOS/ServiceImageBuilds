1b518b92575294a9c54f8efefc55cb0056333079
James L Vodanovich <james@jvodan.com>
2018-02-22 18:45:54 +1100

    tidy pids

--- images/03.ServiceImages/logview/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/logview/home/engines/scripts/startup/start.sh
@@ -5,7 +5,7 @@ PID_FILE=/home/engines/run/apache.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
 mkdir -p /var/log/log_viewer
-export APACHE_PID_FILE=$PID_FILE
+export APACHE_PID_FILE $PID_FILE
 /usr/sbin/apache2ctl  -DFOREGROUND &
 
 startup_complete
