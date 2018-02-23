eeed690e8994aa21c9771e96d01ccd8697411af6
James L Vodanovich <james@jvodan.com>
2018-02-23 14:33:35 +1100

    fix

--- images/03.ServiceImages/ftp/home/engines/scripts/services/_add_service.sh
+++ images/03.ServiceImages/ftp/home/engines/scripts/services/_add_service.sh
@@ -11,10 +11,13 @@ cat /home/engines/templates/ftp/new_user.ldif \
  | sed "s/IDNUMBER/$uid/" \
  | sed "s/GID/${ftp_gid}/" \
  | sed "s/PASSWORD/${password}/" \
- | sed "s/UID/${service_handle}\/${service_container_name}/" \
- | sed "s/USER/${username}/" > $new_user_ldif
+ | sed "s/UID/${service_handle}\/${service_container_name}/" > $new_user_ldif
+ # \
+ #| sed "s/USER/${username}/" > $new_user_ldif
  
- echo /ftp/$access/$parent_engine/$volume/$folder >> $new_user_ldif
+#set the volume
+
+echo /ftp/$access/$parent_engine/$volume/$folder >> $new_user_ldif
 
 cat $new_user_ldif | ldapadd -H ldap://ldap/
 /home/engines/scripts/services/add_to_ftp_group.sh ${service_handle}/${service_container_name}
--- images/03.ServiceImages/ftp/home/engines/scripts/services/_rm_service.sh
+++ images/03.ServiceImages/ftp/home/engines/scripts/services/_rm_service.sh
@@ -2,9 +2,24 @@
 
 kinit -t /etc/krb5kdc/keys/ftp.keytab 
 dn="cn=$service_handle,ou=ftp,ou=Service Accounts,ou=Engines,dc=engines,dc=internal"
-uid=`ldapsearch "$dn" |grep -i uid: | awk '{print $2'}`
-ldapdelete "$dn" -H ldap://ldap/
- 
-/home/engines/scripts/services/del_from_ftp_group.sh $uid
- #dont leave ticket open
+echo "No Found"
+
+uid=`ldapsearch -h ldap "$dn" |grep -i uid: | awk '{print $2'}`
+exit_code=$?
+
+if test $exit_code -eq 0 
+ then 
+   ldapdelete "$dn" -H ldap://ldap/
+   exit_code=$?
+  if test $exit_code -eq 0 
+   then 
+    /home/engines/scripts/services/del_from_ftp_group.sh $uid
+    exit_code=$?
+  else
+    echo "Failed to delete"
+    exit_code=2
+  fi
+fi
+#dont leave ticket open
 kdestroy
+exit $exit_code
