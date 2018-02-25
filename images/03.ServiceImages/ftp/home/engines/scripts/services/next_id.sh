9112524a4c59405e2a59a3b47d0b4bfe2d602376
James L Vodanovich <james@jvodan.com>
2018-02-25 19:29:03 +1100

    ftp handles its on uid

--- images/03.ServiceImages/ftp/home/engines/scripts/services/next_id.sh
+++ images/03.ServiceImages/ftp/home/engines/scripts/services/next_id.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/home/engines/scripts/ldap/next_uid.sh
+
+/home/engines/scripts/ldap/ldapmodify.sh -f /home/engines/templates/ldap/incre_uid.ldif >/dev/null
+uid=`/home/engines/scripts/ldap/ldapsearch.sh "cn=uidNext,ftp,ou=Services,ou=Containers,ou=Engines,dc=engines,dc=internal" uidNumber |grep uidNumber |cut -f2 -d:`
+
+echo $uid
