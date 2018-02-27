98cebe4f71f1e7b1917fab7d6d04354b572faa60
James L Vodanovich <james@jvodan.com>
2018-02-27 17:42:02 +1100

    need two spaces in front

--- images/03.ServiceImages/ldap/home/engines/scripts/services/access/add_access.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/access/add_access.sh
@@ -21,28 +21,28 @@ id="cn=${parent_engine},ou=hosts,ou=engines,dc=engines,dc=internal"
 
 if test $read_access = all
  then
-  read_acl="by * read"
+  read_acl="  by * read"
 elif test $read_access = private
  then
-  read_acl="by * none"
+  read_acl="  by * none"
 elif test $read_access = authenticated
  then  
-  read_acl="by * authenticated"
+  read_acl="  by * authenticated"
 else
-   	read_acl=$read_access
+   	read_acl=" by dn="$read_access"
 fi
 
 if test $write_access = all
  then
-  write_acl="by * write"
+  write_acl="  by * write"
 elif test $write_access = private
  then
   write_acl=""
 elif test $write_access = authenticated
  then  
-  write_acl="by authenticated write" 
+  write_acl="  by authenticated write" 
 else
-   	write_acl=$write_access
+   	write_acl="  by dn="$write_access""
 fi
 
 
@@ -54,8 +54,8 @@ LDAP_OUTF=`mktemp`
      eval echo $LINE >> $LDIF_FILE
    done
 
-echo by dn=cn=admin,ou=People,ou=Engines,dc=engines,dc=internal manage  >> $LDIF_FILE
-echo by dn="cn=uadmin,ou=hosts,ou=Engines,dc=engines,dc=internal" manage  >> $LDIF_FILE
+echo "  by dn=cn=admin,ou=People,ou=Engines,dc=engines,dc=internal manage"  >> $LDIF_FILE
+echo "  by dn=cn=uadmin,ou=hosts,ou=Engines,dc=engines,dc=internal manage"  >> $LDIF_FILE
 it ! test -z $write_acl
  then
 	echo  $write_acl >> $LDIF_FILE
