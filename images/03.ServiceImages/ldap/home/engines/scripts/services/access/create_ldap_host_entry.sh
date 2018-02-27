31689036fb1c9ff678e7d655305f220209fcd744
James L Vodanovich <james@jvodan.com>
2018-02-28 10:05:09 +1100

    -dbg

--- images/03.ServiceImages/ldap/home/engines/scripts/services/access/create_ldap_host_entry.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/access/create_ldap_host_entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LDIF_FILE=`mktemp`
 sha_pass=`echo $password | sha1sum | awk '{ print $1}'`
-echo Sa pass is $sha_pass
+
 cat /home/engines/templates/ldap/services/add_host_entry.ldif | while read LINE
 do
  eval echo $LINE >> $LDIF_FILE
