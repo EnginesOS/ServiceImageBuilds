07b7332975ca17a63403c7a6a4d99deae9464ba1
James L Vodanovich <james@jvodan.com>
2018-02-28 14:16:53 +1100

    break first run into parts

--- images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/startup/_start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
- 
-sleep 600
+
+if ! test -f /home/engines/run/flags/first_run.done
+ then
+   sudo -n /home/engines/scripts/first_run/_first_run.sh >/var/log/first_run.log
+   sleep 4
+   touch /home/engines/run/flags/first_run.stage1_done
+fi   
+
 /usr/sbin/krb5kdc -P /home/engines/run/krb5kdc.pid -n &
 kpid=$! 
 /usr/sbin/kadmind -P /home/engines/run/krb5admin.pid -nofork  &
@@ -8,19 +14,17 @@ kpid=$!
 . /home/engines/functions/system_functions.sh
 
 
-if ! test -f /home/engines/run/flags/first_run.done
+if test -f /home/engines/run/flags/first_run.stage1_done
  then
- 	/home/engines/scripts/first_run/_first_run.sh >/var/log/first_run.log
-   sleep 5
   . /home/engines/scripts/first_run/create_keys_func.sh  
    echo addprinc -pw password  admin@ENGINES.INTERNAL | kadmin.local 
    create_init_keys
+   rm /home/engines/run/flags/first_run.stage1_done
    touch /home/engines/run/flags/first_run.done
 fi 
 
-
-
 wait $kpid 	
+startup_complete
 
 exit_code=$?
  
