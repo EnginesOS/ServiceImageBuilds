e7f5d32154788ee0aba25149a18ff65848ae6dd9
James L Vodanovich <james@jvodan.com>
2018-02-28 17:16:32 +1100

    correct password formation

--- images/03.ServiceImages/ldap/home/engines/scripts/services/access/create_ldap_host_entry.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/access/create_ldap_host_entry.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 LDIF_FILE=`mktemp`
-sha_pass=`echo $password | sha1sum | awk '{ print $1}'`
-
+sha_pass=`slappasswd -h {SHA} -s $password`
 cat /home/engines/templates/ldap/services/add_host_entry.ldif | while read LINE
 do
  eval echo $LINE >> $LDIF_FILE
