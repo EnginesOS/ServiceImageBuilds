2c3419b83a37297014f96178f7bbd88fd77e41d7
James L Vodanovich <james@jvodan.com>
2018-03-01 11:10:43 +1100

    support nill access

--- images/03.ServiceImages/ldap/home/engines/scripts/services/access/add_access.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/access/add_access.sh
@@ -18,33 +18,37 @@ fi
 id="cn=${parent_engine},ou=hosts,ou=engines,dc=engines,dc=internal"
 
 
-
-if test $read_access = all
+if ! test -z $read_access 
  then
-  read_acl="  by * read"
-elif test $read_access = private
- then
-  read_acl="  by * none"
-elif test $read_access = authenticated
- then  
-  read_acl="  by * authenticated"
-else
-   	read_acl=" by dn="$read_access"
+  if test $read_access = all
+   then
+    read_acl="  by * read"
+  elif test $read_access = private
+   then
+    read_acl="  by * none"
+  elif test $read_access = authenticated
+   then  
+    read_acl="  by * authenticated"
+  else
+     	read_acl=" by dn="$read_access"
+  fi
 fi
 
-if test $write_access = all
+if ! test -z $write_access 
  then
-  write_acl="  by * write"
-elif test $write_access = private
- then
-  write_acl=""
-elif test $write_access = authenticated
- then  
-  write_acl="  by authenticated write" 
-else
-   	write_acl="  by dn="$write_access"
+  if test $write_access = all
+   then
+    write_acl="  by * write"
+  elif test $write_access = private
+   then
+    write_acl=""
+  elif test $write_access = authenticated
+   then  
+    write_acl="  by authenticated write" 
+  else
+     	write_acl="  by dn="$write_access"
+  fi
 fi
-
 
 LDIF_FILE=`mktemp`
 LDAP_OUTF=`mktemp`
@@ -54,14 +58,17 @@ LDAP_OUTF=`mktemp`
      eval echo $LINE >> $LDIF_FILE
    done
 
-echo "  by dn=cn=admin,ou=People,ou=Engines,dc=engines,dc=internal manage"  >> $LDIF_FILE
-echo "  by dn=cn=uadmin,ou=hosts,ou=Engines,dc=engines,dc=internal manage"  >> $LDIF_FILE
+echo "  by dn=cn=admin,ou=People,ou=Engines,dc=engines,dc=internal manage" >> $LDIF_FILE
+echo "  by dn=cn=uadmin,ou=hosts,ou=Engines,dc=engines,dc=internal manage" >> $LDIF_FILE
+
 it ! test -z $write_acl
  then
 	echo  $write_acl >> $LDIF_FILE
 fi
+
 echo $read_acl >> $LDIF_FILE
- cat $LDIF_FILE |sudo /home/engines/scripts/ldap/sudo/_ldapmodify.sh  &> $LDAP_OUTF
- cp $LDIF_FILE /tmp/access
- rm $LDIF_FILE
+cat $LDIF_FILE |sudo /home/engines/scripts/ldap/sudo/_ldapmodify.sh  &> $LDAP_OUTF
+cp $LDIF_FILE /tmp/access
+rm $LDIF_FILE
+
  
