d5041273c0532208d745f822a8a8128b3ae200e1
James L Vodanovich <james@jvodan.com>
2018-03-01 11:54:43 +1100

    correct password entry

--- images/03.ServiceImages/ldap/home/engines/scripts/services/access/create_ldap_host_entry.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/access/create_ldap_host_entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LDIF_FILE=`mktemp`
-sha_pass=`slappasswd -h {SHA} -s $password`
+sha_pass=`slappasswd -h {SHA} -s $ldap_password`
 cat /home/engines/templates/ldap/services/add_host_entry.ldif | while read LINE
 do
  eval echo $LINE >> $LDIF_FILE
