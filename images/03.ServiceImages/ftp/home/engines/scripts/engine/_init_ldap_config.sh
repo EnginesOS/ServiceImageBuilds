6909197c09cdd8102a09a9e2153501032267b725
James L Vodanovich <james@jvodan.com>
2018-03-06 17:02:00 +1100

    tidy

--- images/03.ServiceImages/ftp/home/engines/scripts/engine/_init_ldap_config.sh
+++ images/03.ServiceImages/ftp/home/engines/scripts/engine/_init_ldap_config.sh
@@ -7,6 +7,6 @@ for file in $files
  do
   cat /home/engines/templates/ftp/$file |\
   	 sed "s/LDAP_BIND_DN/$ldap_dn/"|\
-  	  sed "s/LDAP_BIND_PW/$ldap_password/"\
-  	  > /etc/proftpd/$file
+  	 sed "s/LDAP_BIND_PW/$ldap_password/"\
+  	 > /etc/proftpd/$file
  done
