a5253ef8b5cdb258b705f954661eaf759212f940
James L Vodanovich <james@jvodan.com>
2018-03-06 14:51:15 +1100

    bind to dn

---
+++ images/03.ServiceImages/email/home/engines/scripts/engine/_init_ldap_config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+files="ldap-aliases.cf ldap-groups.cf ldap-virtual-domains.cf ldap-vmailbox.cf"
+
+for file in files
+ do
+  cat /home/engines/templates/email/ldap/$file |\
+  	 sed "s/LDAP_BIND_DN/$ldap_dn/"|\
+  	  sed "s/LDAP_BIND_PW/$ldap_password/"\
+  	  > /etc/postfix/ldap
+ done
--- images/03.ServiceImages/email/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/email/home/engines/scripts/startup/start.sh
@@ -24,6 +24,8 @@ fi
 
 
 /home/engines/scripts/startup/init_dbs.sh
+/home/engines/scripts/engine/init_ldap_config.sh
+
 sudo -n /home/engines/scripts/startup/_start_postfix.sh &
 startup_complete
 
