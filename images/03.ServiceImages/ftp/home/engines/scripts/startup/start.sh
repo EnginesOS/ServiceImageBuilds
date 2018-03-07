a3dddead346b5ed9f88a190b1a813c40f6d52ad1
James L Vodanovich <james@jvodan.com>
2018-03-07 16:11:48 +1100

    logs oo grp containers that can read

--- images/03.ServiceImages/ftp/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/ftp/home/engines/scripts/startup/start.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-
 PID_FILE=/home/engines/run/ftpd.pid
 export PID_FILE
 . /home/engines/functions/trap.sh
 
 /home/engines/scripts/engine/init_ldap_config.sh
 
-sudo -n  /usr/sbin/proftpd -n &
+sudo -n /usr/sbin/proftpd -n &
 
 startup_complete
+
+chgrp containers /var/log/proftpd/ldap.log 
 
 wait 
 exit_code=$?
