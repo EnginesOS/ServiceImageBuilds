15205977bbcd2006823809b497bd434dfd7b3b90
James L Vodanovich <james@jvodan.com>
2018-03-14 18:56:15 +1100

    clear old default domain records

--- images/03.ServiceImages/avahi/home/engines/scripts/configurators/set_default_mdns_domain.sh
+++ images/03.ServiceImages/avahi/home/engines/scripts/configurators/set_default_mdns_domain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-rm /home/avahi/hosts/*
+default_mdns_domain=`cat /home/engines/scripts/configurators/saved/default_mdns_domain`
+rm /home/avahi/hosts/*.${default_mdns_domain}
 
 cat - >/home/engines/scripts/configurators/saved/default_mdns_domain
 /home/engines/scripts/signal/kill_avahi.sh
