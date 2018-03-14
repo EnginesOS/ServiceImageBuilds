a488a190137e8e7521049b9f7da0dd99fe3bfa76
James L Vodanovich <james@jvodan.com>
2018-03-14 15:18:10 +1100

    remove from wan or lan dir or both when removind domain

--- images/03.ServiceImages/dns/home/engines/scripts/services/rm_service.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/services/rm_service.sh
@@ -8,7 +8,7 @@ params_to_env
      if ! test ${domain_name} = engines.internal
       then
   	   rm /home/bind/engines/domains/${domain_name}
-  	   rm  /home/bind/domain_list/${ip_type}/${domain_name}
+  	   rm  /home/bind/domain_list/*/${domain_name}
   	   rm /home/bind/engines/zones/named.conf.${domain_name}
   	   cat /home/bind/engines/domains/* > /home/bind/engines/domains.hosted
   	   kill -HUP `cat /home/engines/run/named.pid`
