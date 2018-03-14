a5e6cccc5c0dfd9ab3ea0eef508f8d2b165cdbfe
James L Vodanovich <james@jvodan.com>
2018-03-14 12:55:30 +1100

    avoid comparing nil type when not set

--- images/03.ServiceImages/dns/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/services/add_service.sh
@@ -4,9 +4,11 @@
 
 . /home/engines/functions/params_to_env.sh
 params_to_env
- 
-if test $type = "domain"
-  then        
+
+if ! test -z $type
+ then
+  if test $type = "domain"
+   then        
      if test $wan_or_lan = lan
         then
           ip=`cat /home/engines/system/net/ip`
@@ -14,16 +16,16 @@ if test $type = "domain"
          then
            ip=`cat /home/engines/system/net/public`
       fi
-   touch /home/bind/domain_list/${wan_or_lan}/${domain_name}   
-   cat /home/engines/templates/dns/config_file_zone_entry.tmpl | sed " /DOMAIN/s//${domain_name}/g" > /home/bind/engines/domains/${domain_name}
-   cat /home/engines/templates/dns/selfhosted.tmpl | sed "/DOMAIN/s//${domain_name}/g"\
+    touch /home/bind/domain_list/${wan_or_lan}/${domain_name}   
+    cat /home/engines/templates/dns/config_file_zone_entry.tmpl | sed " /DOMAIN/s//${domain_name}/g" > /home/bind/engines/domains/${domain_name}
+    cat /home/engines/templates/dns/selfhosted.tmpl | sed "/DOMAIN/s//${domain_name}/g"\
          | sed "/IP/s//${ip}/g" > /home/bind/engines/zones/named.conf.${domain_name}
-   cat /home/bind/engines/domains/* > /home/bind/engines/domains.hosted
-   kill -HUP `cat /home/engines/run/named.pid`
-   echo Success
-   exit 0
+    cat /home/bind/engines/domains/* > /home/bind/engines/domains.hosted
+    kill -HUP `cat /home/engines/run/named.pid`
+    echo Success
+    exit 0
+  fi
 fi
-
 
 domain_name=engines.internal
 add_to_internal_domain
