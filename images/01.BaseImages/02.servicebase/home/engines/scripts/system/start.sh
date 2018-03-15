fd54b16c737814c9df5c7994af2ad72af9f6120f
James L Vodanovich <james@jvodan.com>
2018-03-15 11:45:40 +1100

    sort out perms during /system/start.sh

--- images/01.BaseImages/02.servicebase/home/engines/scripts/system/start.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/system/start.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+if ! test -w /home/engines/run
+ then
+  chown -R $ContUser.containers /home/engines/run
+fi  
+
 if ! test -f /home/engines/run/flags/ca-update
  then
    sudo -n /home/engines/scripts/system/_update_ca.sh		
 fi
+
+sudo -n /home/engines/scripts/system/_setup_logging.sh		
 
 exec /home/engines/scripts/startup/start.sh
