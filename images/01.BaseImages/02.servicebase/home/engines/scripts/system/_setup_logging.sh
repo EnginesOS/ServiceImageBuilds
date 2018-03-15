d45aa8c380b6e7952fb1ade70f3e65ea53365217
James L Vodanovich <james@jvodan.com>
2018-03-15 12:12:48 +1100

    start syslog in system start and not trap.sh

--- images/01.BaseImages/02.servicebase/home/engines/scripts/system/_setup_logging.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/system/_setup_logging.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 
-if test -f /home/engines/etc/LOG_DIR
- then
-	log_dir=`cat /home/engines/etc/LOG_DIR`
-else
- 	log_dir=/var/log/
-fi
- 	
-if ! test -d $log_dir 
- then    
-  mkdir -p $log_dir 
-fi 
-
 if ! test -f /home/engines/run/flags/log_setup
- then
-  chown -R $ContUser $log_dir 
-  touch /home/engines/run/flags/log_setup
+ if test -f /home/engines/etc/LOG_DIR
+  then
+	log_dirs=`cat /home/engines/etc/LOG_DIR`
+   else
+ 	log_dirs=/var/log/
+  fi
+
+  for log_dir in $log_dirs
+   do
+    if ! test -d $log_dir 
+     then    
+     mkdir -p $log_dir 
+     chown -R $ContUser $log_dir 
+    fi
+  done   
 fi
+
+if test -f /home/engines/etc/SYSLOG
+   if test -f /home/engines/run/rsyslogd.pid 
+    then
+     rm /home/engines/rsyslogd.pid
+   fi 
+
+  rsyslogd -i /home/engines/run/rsyslogd.pid
+fi 
 
   
   
