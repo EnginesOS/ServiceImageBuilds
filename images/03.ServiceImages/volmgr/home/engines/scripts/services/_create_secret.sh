7db7042e35c507f8af33fa50c3ad48bcb515c44d
James L Vodanovich <james@jvodan.com>
2018-03-16 15:45:00 +1100

    typo

--- images/03.ServiceImages/volmgr/home/engines/scripts/services/_create_secret.sh
+++ images/03.ServiceImages/volmgr/home/engines/scripts/services/_create_secret.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-if ! test -d  /var/secrets/${container_type/}${parent_engine}/
+if ! test -d  /var/secrets/${container_type}/${parent_engine}/
  then
-   mkdir -p /var/secrets/${container_type/}${parent_engine}/
+   mkdir -p /var/secrets/${container_type}/${parent_engine}/
 fi   
 
 dd if=/dev/urandom count=16 bs=1  | od -h | awk '{ print $2$3$4$6$6$7$8$9}' >/var/secrets/${container_type}/${parent_engine}/${service_handle}
