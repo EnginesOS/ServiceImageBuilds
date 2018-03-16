4c4ac9fef1b8cf40725cef48f7831993a93c66e1
James L Vodanovich <james@jvodan.com>
2018-03-16 12:18:04 +1100

    missing a then

--- images/01.BaseImages/02.servicebase/home/engines/scripts/system/_setup_logging.sh
+++ images/01.BaseImages/02.servicebase/home/engines/scripts/system/_setup_logging.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 if ! test -f /home/engines/run/flags/log_setup
- if test -f /home/engines/etc/LOG_DIR
-  then
-	log_dirs=`cat /home/engines/etc/LOG_DIR`
-   else
- 	log_dirs=/var/log/
+ then
+  if test -f /home/engines/etc/LOG_DIR
+   then
+	 log_dirs=`cat /home/engines/etc/LOG_DIR`
+  else
+ 	 log_dirs=/var/log/
   fi
 
   for log_dir in $log_dirs
    do
     if ! test -d $log_dir 
      then    
-     mkdir -p $log_dir 
-     chown -R $ContUser $log_dir 
+      mkdir -p $log_dir 
+      chown -R $ContUser $log_dir 
     fi
   done   
 fi
