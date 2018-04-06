f5b7c984fc0a62a1418407baab933742aad539dd
James L Vodanovich <james@jvodan.com>
2018-04-06 12:43:40 +1000

    sha1 password ldap

--- images/03.ServiceImages/uadmin/home/engines/scripts/actionators/add_user.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/actionators/add_user.sh
@@ -5,6 +5,7 @@ params_to_env
 required_values="uid password last_name given_name domain_name"
 check_required_values 
 
+shapass=`echo -n $password  |sha1sum |cut -f1 -d" "`
 cat /home/engines/templates/add_user.ldif | while read LINE
 do
  eval echo "$LINE" >> $LDIF_FILE
@@ -15,19 +16,19 @@ uidnumber=`/home/engines/scripts/ldap/next_uid.sh`
 echo uidnumber:$uidnumber >> $LDIF_FILE
 err_file=`mktemp`
 r=`cat $LDIF_FILE | /home/engines/scripts/ldap/ldapadd.sh` 
-if test $? -eq 0
- then
-  echo "addprinc -pw $password  $uid" | sudo -n /home/engines/scripts/actionators/sudo/_add_kerberos_princ.sh &> $err_file
-fi   
-e=$?
-if test $e -eq 0
- then
-   echo $r
- else
-   echo '{"uid":"'$uid'"}' | /home/engines/scripts/actionators/del_user.sh   
-    mesg=`cat $err_file`
-   echo '{"Error":"failed to add k user","Result":"Failed","exit":"'$e',"mesg":"'$mesg'"}'   
- fi
+#if test $? -eq 0
+# then
+#  echo "addprinc -pw $password  $uid" | sudo -n /home/engines/scripts/actionators/sudo/_add_kerberos_princ.sh &> $err_file
+#fi   
+#e=$?
+#if test $e -eq 0
+# then
+#   echo $r
+# else
+#   echo '{"uid":"'$uid'"}' | /home/engines/scripts/actionators/del_user.sh   
+#    mesg=`cat $err_file`
+#   echo '{"Error":"failed to add k user","Result":"Failed","exit":"'$e',"mesg":"'$mesg'"}'   
+# fi
   
 rm  $err_file
    
