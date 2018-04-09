9a7f105ac18abc7d2d521dd81871144f4dd01823
James L Vodanovich <james@jvodan.com>
2018-04-09 12:30:22 +1000

    f

--- images/03.ServiceImages/uadmin/home/engines/scripts/actionators/change_user_password.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/actionators/change_user_password.sh
@@ -6,6 +6,8 @@ check_required_values
 
 shapass=`echo -n $password  | openssl dgst -sha1 -binary | openssl enc -base64`
 . /home/engines/functions/ldap/support_functions.sh
+cn=`/home/engines/scripts/ldap/ldapsearch.sh   ou=People,dc=engines,dc=internal uid=ctest cn |grep ^cn | sed "s/cn: //"`
+export cn
 
 cat /home/engines/templates/chg_user_pass.ldif | while read LINE
 do
