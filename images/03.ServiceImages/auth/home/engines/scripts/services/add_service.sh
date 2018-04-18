1a4d929efa9194d17b2082dff0b6cb9553c9014e
James L Vodanovich <james@jvodan.com>
2018-04-18 15:54:48 +1000

    f

--- images/03.ServiceImages/auth/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/auth/home/engines/scripts/services/add_service.sh
@@ -5,7 +5,7 @@ params_to_env
 
 required_values="parent_engine container_type owner"
 check_required_values
-export parent_engine container_type owner
+export parent_engine container_type owner prefix
 err_log=`mktemp`
 sudo -n /home/engines/scripts/services/_add_service.sh &> $err_log
 r=$?
