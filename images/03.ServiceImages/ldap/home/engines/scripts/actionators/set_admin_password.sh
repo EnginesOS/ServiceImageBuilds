f409d4c5c1015bb7c6d6938c5c940cd34e0bc082
James L Vodanovich <james@jvodan.com>
2018-05-21 13:52:44 +1000

    fix keyname

--- images/03.ServiceImages/ldap/home/engines/scripts/actionators/set_admin_password.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/actionators/set_admin_password.sh
@@ -3,12 +3,12 @@
 . /home/engines/functions/params_to_env.sh
 params_to_env
 
-required_values="password"
+required_values="new_password"
 check_required_values
 
 . /home/engines/functions/ldap/support_functions.sh
 
-echo -n "$password" | sudo -n /home/engines/scripts/actionators/_set_admin_password.sh  >$LDAP_OUTF
+echo -n "$new_password" | sudo -n /home/engines/scripts/actionators/_set_admin_password.sh  >$LDAP_OUTF
 result=$?
 
 process_ldap_result
