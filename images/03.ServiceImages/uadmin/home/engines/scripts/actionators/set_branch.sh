f6f877c178523518b5e1d886d8f8aab378883afb
James L Vodanovich <james@jvodan.com>
2018-07-10 16:42:45 +1000

    typo

--- images/03.ServiceImages/uadmin/home/engines/scripts/actionators/set_branch.sh
+++ images/03.ServiceImages/uadmin/home/engines/scripts/actionators/set_branch.sh
@@ -7,13 +7,13 @@ cd /home/app
 required_values="branch"
 check_required_values 
 
-if test $release=`git branch | grep \* | cut -d ' ' -f2`
+if test $branch = `git branch | grep \* | cut -d ' ' -f2`
  then
   echo "Already release $branch"
   exit 0
 fi
 git pull https://github.com/lachdoug/user_admin.git $branch
-get checkout $branch
+git checkout $branch
 
 export RACK_ENV production
 
