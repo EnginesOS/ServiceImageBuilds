2e01d9372fb79bb43f7a8384a07adc9b2aadd7ac
James L Vodanovich <james@jvodan.com>
2018-08-04 15:02:57 +1000

    remove mysql from backup and restore as mysql no longer used for imap

--- images/03.ServiceImages/imap/home/engines/scripts/backup/restore.sh
+++ images/03.ServiceImages/imap/home/engines/scripts/backup/restore.sh
@@ -4,7 +4,7 @@ cd /
 tar -xpf - 2>/tmp/tar.errs
 tar -xpf /tmp/imap/backup.* /var/lib/dovecot /tmp/database.sql /var/mail
 
-cat /tmp/database.sql |mysql -h $dbhost -u $dbuser --password=$dbpasswd $dbname 
+
 r=$?
 
 rm /tmp/database.sql /tmp/imap/backup.*
