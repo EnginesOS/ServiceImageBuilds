c470deae4a47604b7e3c6ce0606c88fe35e7566c
James L Vodanovich <james@jvodan.com>
2018-09-27 14:22:43 +1000

    f

--- images/03.ServiceImages/ldap/home/engines/scripts/services/group/rm_group.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/group/rm_group.sh
@@ -1,23 +1,18 @@
 #/!bin/bash
 
-if test -z $cn
-  then
-   dn="ou=$parent_engine,ou=$top_ou,ou=Groups,dc=engines,dc=internal"
-   delou=1
- else
-   dn="cn=$cn,ou=$parent_engine,ou=$top_ou,ou=Groups,dc=engines,dc=internal"
- fi
+
+dn="cn=$cn,ou=$parent_engine,ou=$top_ou,ou=Groups,dc=engines,dc=internal"
+
  
 /home/engines/scripts/ldap/ldapdelete.sh "$dn"
 /home/engines/scripts/services/access/rm_access.sh "$dn"
  
  
-if ! test $delou
- then
-  /home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Groups,dc=engines,dc=internal ou=$parent_engine  | grep cn
-   if test $? -ne 0
-    then
-     dn="ou=$parent_engine,ou=${top_ou},ou=Groups,dc=engines,dc=internal"
-      /home/engines/scripts/ldap/ldapdelete.sh "$dn"
-   fi
-fi   
+
+/home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Groups,dc=engines,dc=internal ou=$parent_engine  | grep cn
+  if test $? -ne 0
+   then
+    dn="ou=$parent_engine,ou=${top_ou},ou=Groups,dc=engines,dc=internal"
+     /home/engines/scripts/ldap/ldapdelete.sh "$dn"
+  fi
+   
--- images/03.ServiceImages/ldap/home/engines/scripts/services/ou/rm_ou.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/ou/rm_ou.sh
@@ -1,24 +1,17 @@
 #/!bin/bash
 
+dn="ou=$cn,ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
 
-if test -z $cn
-  then
-   dn="ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
-   delou=1
- else
-   dn="ou=$cn,ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
- fi
  
  /home/engines/scripts/ldap/ldapdelete.sh "$dn"
  /home/engines/scripts/services/access/rm_access.sh "$dn"
  
-if ! test $delou
- then
-  `c=/home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Containers,ou=Engines,dc=engines,dc=internal ou=$parent_engine  |wc -l`
-   if test $c -eq 0
-    then
-     dn="ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
-      /home/engines/scripts/ldap/ldapdelete.sh "$dn"
+
+`c=/home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Containers,ou=Engines,dc=engines,dc=internal ou=$parent_engine  |wc -l`
+  if test $c -eq 0
+   then
+    dn="ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
+     /home/engines/scripts/ldap/ldapdelete.sh "$dn"
       
-   fi
-fi   
+  fi
+  
