8377b0fdd82f4bd188642ed4a10e6b3431c437fc
James L Vodanovich <james@jvodan.com>
2018-09-27 14:43:43 +1000

    del if last

--- images/03.ServiceImages/ldap/home/engines/scripts/services/ou/rm_ou.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/ou/rm_ou.sh
@@ -7,8 +7,9 @@ dn="ou=$cn,ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=i
  /home/engines/scripts/services/access/rm_access.sh "$dn"
  
 
-`c=/home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Containers,ou=Engines,dc=engines,dc=internal ou=$parent_engine  |wc -l`
-  if test $c -eq 0
+`c=/home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Containers,ou=Engines,dc=engines,dc=internal ou=$parent_engine dn |grep dn |wc -l
+`
+  if test $c -eq 1
    then
     dn="ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
      /home/engines/scripts/ldap/ldapdelete.sh "$dn"
