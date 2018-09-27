c7a8942566d6b7b4b02fb83d8329dd4093f049ea
James L Vodanovich <james@jvodan.com>
2018-09-27 14:06:38 +1000

    f

--- images/03.ServiceImages/ldap/home/engines/scripts/services/ou/rm_ou.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/services/ou/rm_ou.sh
@@ -14,7 +14,7 @@ if test -z $cn
  
 if ! test $delou
  then
-  /home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Containers,ou=Engines,dc=engines,dc=internal ou=$parent_engine  |wc -l`
+  `c=/home/engines/scripts/ldap/ldapsearch.sh ou=${top_ou},ou=Containers,ou=Engines,dc=engines,dc=internal ou=$parent_engine  |wc -l`
    if test $c -eq 0
     then
      dn="ou=$parent_engine,ou=$top_ou,ou=Containers,ou=Engines,dc=engines,dc=internal"
