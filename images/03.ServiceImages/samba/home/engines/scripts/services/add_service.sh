c76de2c084bca92364da38edaa373cba7aed05c6
James L Vodanovich <james@jvodan.com>
2018-12-30 12:39:30 +1100

    smbfs ldap

--- images/03.ServiceImages/samba/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/samba/home/engines/scripts/services/add_service.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-
  . /home/engines/functions/checks.sh
-if test $# -eq 0 
- then
- 	cat -  | ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i /home/.ssh/add_rsa auth@auth.engines.internal /home/auth/scripts/nfs/add_service.sh
-else 
-	echo $1 | ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i /home/.ssh/add_rsa auth@auth.engines.internal /home/auth/scripts/nfs/add_service.sh 
-fi
--- images/03.ServiceImages/samba/home/engines/scripts/services/rm_service.sh
+++ images/03.ServiceImages/samba/home/engines/scripts/services/rm_service.sh
@@ -3,12 +3,3 @@
 
  . /home/engines/functions/checks.sh
 
-
-if test $# -eq 0 
- then
- 	cat -  | ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i ~/.ssh/rm_rsa auth@auth.engines.internal /home/auth/scripts/nfs/rm_service.sh
- else
-	echo $1 | ssh -p 2222  -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no -i ~/.ssh/rm_rsa auth@auth.engines.internal /home/auth/scripts/nfs/rm_service.sh
-fi
-
- 
