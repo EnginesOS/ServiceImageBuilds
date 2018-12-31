cbc6e6ecb74a724bcf33294d9d07e97682679ec1
James L Vodanovich <james@jvodan.com>
2018-12-31 12:48:00 +1100

    add group before user

--- images/03.ServiceImages/samba/home/engines/scripts/engine/rebuild_config_file.sh
+++ images/03.ServiceImages/samba/home/engines/scripts/engine/rebuild_config_file.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /home/engines/etc/samba/smb.conf.ini /home/engines/etc/samba/smd.d/*cf > /home/engines/etc/smb.conf
+cat /home/engines/etc/samba/smb.conf.ini /home/engines/etc/samba/smd.d/*cf > /etc/samb/smb.conf
