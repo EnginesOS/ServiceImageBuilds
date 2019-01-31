69d62488eb7e9eb77f4d30d2c748d1127653c99d
james <james@engines.org>
2019-02-01 09:15:24 +1100

    further work on samba

--- images/03.ServiceImages/samba/home/engines/scripts/engine/rebuild_config_file.sh
+++ images/03.ServiceImages/samba/home/engines/scripts/engine/rebuild_config_file.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat /home/engines/etc/samba/smb.conf.ini /home/engines/etc/samba/smd.d/*cf > /etc/samb/smb.conf
+cat /home/engines/etc/samba/smb.conf.ini /home/engines/etc/samba/smd.d/*cf > /etc/samba/smb.conf
 echo $ldap_password > /etc/ldap.secret
 smbpasswd -w $ldap_password
