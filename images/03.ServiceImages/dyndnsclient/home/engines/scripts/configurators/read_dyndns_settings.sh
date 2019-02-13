d0d5ed5b784597611ac35dd8e116cb2d78f8352e
james <james@engines.org>
2019-02-13 13:51:43 +1100

    handle * in domainname

--- images/03.ServiceImages/dyndnsclient/home/engines/scripts/configurators/read_dyndns_settings.sh
+++ images/03.ServiceImages/dyndnsclient/home/engines/scripts/configurators/read_dyndns_settings.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-login=`cat/home/engines/scripts/configurators/saved/login`
-provide=`cat/home/engines/scripts/configurators/saved/provider`
-domain_name=`cat/home/engines/scripts/configurators/saved/domain_name`
-password=`cat/home/engines/scripts/configurators/saved/password`
+login=`cat /home/engines/scripts/configurators/saved/login`
+provide=`cat /home/engines/scripts/configurators/saved/provider`
+domain_name=`cat /home/engines/scripts/configurators/saved/domain_name`
+password=`cat /home/engines/scripts/configurators/saved/password`
 echo '{
 	"provider":"'$provider'",
 	"login":"'$login'",
-	"domain_name":"'$domain_name'",
-	"password":"'$password'"}'
+	"domain_name":"'"$domain_name"'",
+	"password":"'"$password"'"}'
 
 fi
 exit 0
