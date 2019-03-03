205868f1cda1dce631df05c92975c38eaf3fca4d
james <james@rumah>
2019-03-03 12:13:16 +1100

    new key id format

--- images/03.ServiceImages/backup/home/engines/scripts/configurators/init_gpg_key.sh
+++ images/03.ServiceImages/backup/home/engines/scripts/configurators/init_gpg_key.sh
@@ -17,7 +17,7 @@ if ! test -f /home/backup/.gnupg/pass
  	gpg --gen-key --batch /tmp/key.tmpl
  	rm /tmp/key.tmpl
    
-	gpg --list-keys  |grep -1 dsa1024 | tail -1 | cut -f2 -d/ |cut -f1 -d" " > /home/backup/.gnupg/key_id
+	gpg --list-keys  |grep -1 dsa1024 | tail -1 | sed "/[ ]/s///g" > /home/backup/.gnupg/key_id
 	key_id=`cat /home/backup/.gnupg/key_id`
     cp /home/backup/.gnupg/pass /home/backup/.gnupg/pass_${key_id}
     chmod og-wxr /home/backup/.gnupg/pass /home/backup/.gnupg/pass_${key_id}
