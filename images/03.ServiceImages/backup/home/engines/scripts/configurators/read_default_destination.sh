f68a8dc205bdd6c5e1e59564c9d16e55fba2ea61
james <james@rumah>
2019-03-03 12:57:08 +1100

    typo

--- images/03.ServiceImages/backup/home/engines/scripts/configurators/read_default_destination.sh
+++ images/03.ServiceImages/backup/home/engines/scripts/configurators/read_default_destination.sh
@@ -8,7 +8,7 @@ then
  	{"dest_address":"'$dest_address'",
  	"dest_folder":"'$dest_folder'",
  	"dest_pass":"'$dest_pass'",
- 	"dest_proto":"'$dest_proto'".
+ 	"dest_proto":"'$dest_proto'",
  	"dest_user":"'$dest_user'"}}'
 else
   echo '{"default_destination":"Not Set"}'
