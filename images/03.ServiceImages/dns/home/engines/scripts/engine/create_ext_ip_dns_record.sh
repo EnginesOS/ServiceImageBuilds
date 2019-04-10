6a8dfa4fe013838651e704f49228fe2ed89c78fe
james <james@rumah>
2019-04-10 13:43:13 +1000

    int ip and ext ip seperatly

---
+++ images/03.ServiceImages/dns/home/engines/scripts/engine/create_ext_ip_dns_record.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+. /home/engines/scripts/services/dns_functions.sh
+
+
+if test -f /home/engines/system/net/public
+ then
+  ip=`cat /home/engines/system/net/public`
+  hostname=publichost
+  no_inarpra=1
+  add_to_internal_domain
+fi  
+
+
