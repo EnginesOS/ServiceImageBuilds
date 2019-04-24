4d7685a0c12145afafcfd062591deb962de308cf
James Vodanovich <james@jvodan.com>
2019-04-25 08:46:07 +1000

    test

--- images/03.ServiceImages/ldap/home/engines/scripts/first_run/_first_run.sh
+++ images/03.ServiceImages/ldap/home/engines/scripts/first_run/_first_run.sh
@@ -4,5 +4,5 @@ echo populating /etc/ldap/slapd.d
 
 
 
-slaptest -f /home/engines/etc/slapd.conf -F  /etc/ldap/slapd.d/
+#slaptest -f /home/engines/etc/slapd.conf -F  /etc/ldap/slapd.d/
 touch /home/engines/run/flags/first_run
