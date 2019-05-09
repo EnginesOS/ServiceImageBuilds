0a5443e5bb27319b47c12a55dd7694e719d707fa
James Vodanovich <james@jvodan.com>
2019-05-09 16:32:28 +1000

    include enginesrelease in containers env

--- images/01.BaseImages/001.ubuntu.16.04/setup.sh
+++ images/01.BaseImages/001.ubuntu.16.04/setup.sh
@@ -5,3 +5,5 @@ cp Dockerfile.$arch Dockerfile
 BuildDate=`date +%y%m%d`
 export BuildDate
 echo BuildDate=$BuildDate >.env
+EnginesRelease=`pwd | cut -d/ -f 5`
+echo EnginesRelease=$EnginesRelease >> .env
