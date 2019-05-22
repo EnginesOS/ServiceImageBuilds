a0aa4296468cb445b150c1a3f320aefb58b5386b
James Vodanovich <james@jvodan.com>
2019-05-23 09:28:08 +1000

    dkim implementation

--- images/03.ServiceImages/smtp/home/engines/scripts/engine/rebuild_dkim.key.sh
+++ images/03.ServiceImages/smtp/home/engines/scripts/engine/rebuild_dkim.key.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 cd /etc/dkim/keys/
-rm /etc/dkim/dkim.key
-touch /etc/dkim/dkim.key
+rm /etc/opendkim/KeyTable /etc/opendkim/SigningTable 
+touch /etc/opendkim/KeyTable /etc/opendkim/SigningTable 
 
 for domain in `ls `
  do 
- 	echo '*@'$domain:$domain:/etc/mail/dkim-keys/$domain/mail.private >>  /etc/dkim/dkim.key
+ 	echo '*@'$domain:$domain:/etc/mail/dkim-keys/$domain/mail.private >>  /etc/opendkim/KeyTable
+ 	echo '*@'$domain default._domainkey.$domain >> /etc/opendkim/SigningTable 
  done
