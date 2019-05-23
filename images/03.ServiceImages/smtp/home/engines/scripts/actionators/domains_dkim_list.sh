2c91e66d45a3223a5cb140956751d6d33354ad92
James Vodanovich <james@jvodan.com>
2019-05-23 12:30:53 +1000

    handle no domain dkims

--- images/03.ServiceImages/smtp/home/engines/scripts/actionators/domains_dkim_list.sh
+++ images/03.ServiceImages/smtp/home/engines/scripts/actionators/domains_dkim_list.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-cd /etc/opendkim/keys/
+
+
+
 echo -n '{"dkim_domains":['
-n=0
-for domain in `ls`
- do
-  if test $n -ne 0
-   then
-    echo =n ,
-    n=1
-  fi  
-  echo -n '"'$domain'"'
+
+if test -d /etc/opendkim/keys/
+ then
+  cd /etc/opendkim/keys/
+  n=0
+  for domain in `ls`
+   do
+    if test $n -ne 0
+     then
+      echo -n ,
+      n=1
+    fi  
+    echo -n '"'$domain'"'
 done
+fi
 
 echo ']}'
 
