5d47cd8adf75b351f6a0c105742981cbb1e9f245
James Vodanovich <james@jvodan.com>
2019-05-24 22:13:55 +1000

    tidy

--- images/03.ServiceImages/dns/home/engines/scripts/configurators/read_dns_forwarders.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/configurators/read_dns_forwarders.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
+ echo '{'
 
- echo '{"forwarders":'["'
- 
 if test -f /home/engines/scripts/configurators/saved/dns_forwarder1
- then 
+ then
    dns_forwarder1=`cat home/engines/scripts/configurators/saved/dns_forwarder1`
+
   if test -f /home/engines/scripts/configurators/saved/dns_forwarder2
    then
-    dns_forwarder1=`cat home/engines/scripts/configurators/saved/dns_forwarder2`
+    dns_forwarder2=`cat home/engines/scripts/configurators/saved/dns_forwarder2`
   fi
-  
-  if ! test -z $dns_forwarder1
-   then
-    echo $dns_forwarder1'"'
-  fi
-   
-  if ! test -z $dns_forwarder1
-   then
-    echo ',"'$dns_forwarder2'"'
-  fi
- 
- echo ']}'
-
-else
-	echo '{"forwarders":[]}'
 fi
+  if ! test -z $dns_forwarder1
+   then
+    echo '"dns_server":"'$dns_forwarder1'"'
+  fi
+
+  if ! test -z $dns_forwarder1
+   then
+    echo ':"dns_server2":"'$dns_forwarder2'"'
+  fi
+
+        echo '}'
+
 exit 0
+~            
--- images/03.ServiceImages/dns/home/engines/scripts/configurators/set_dns_forwarders.sh
+++ images/03.ServiceImages/dns/home/engines/scripts/configurators/set_dns_forwarders.sh
@@ -1,31 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 
  if test -z $dns_server
   then
    $dns_server=" "
- fi 
- 
+ fi
+
 if test ${#dns_server} -ge 7
  then
-  echo  -n ${#dns_server} >/home/engines/scripts/configurators/saved/dns_forwarder1
-  echo  -n ${#dns_server2} >> /home/engines/scripts/configurators/saved/dns_forwarder2
+  echo  -n ${dns_server} >/home/engines/scripts/configurators/saved/dns_forwarder1
+if test ${#dns_server2} -ge 7
+  then
+  echo  -n ${dns_server2} >> /home/engines/scripts/configurators/saved/dns_forwarder2
+ else
+  if test -f /home/engines/scripts/configurators/saved/dns_forwarder2
+  then
+  rm /home/engines/scripts/configurators/saved/dns_forwarder2
+ fi
+ fi
    echo "forwarders {
-	$dns_server; " > /home/bind/engines/forwarders
+        $dns_server; " > /home/bind/engines/forwarders
 
     if ! test -z $dns_server2
      then
        if test ${#dns_server2} -ge 7
         then
-         echo "	$dns_server2; " >> /home/bind/engines/forwarders
-        fi 
-    fi 
+         echo " $dns_server2; " >> /home/bind/engines/forwarders
+        fi
+    fi
   echo "}; " >> /home/bind/engines/forwarders
   else
     echo "Clearing forwarders"
-   echo "" > /home/bind/engines/forwarders 
+   echo "" > /home/bind/engines/forwarders
 fi
-  
+
 echo Success
 exit 0
  
