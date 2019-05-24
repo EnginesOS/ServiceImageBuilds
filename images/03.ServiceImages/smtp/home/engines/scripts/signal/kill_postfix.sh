dfd9f401c89687c7891ee995b90d3b1d21d143b0
James Vodanovich <james@jvodan.com>
2019-05-24 21:08:17 +1000

    sudo refactor

--- images/03.ServiceImages/smtp/home/engines/scripts/signal/kill_postfix.sh
+++ images/03.ServiceImages/smtp/home/engines/scripts/signal/kill_postfix.sh
@@ -3,7 +3,7 @@
 if test -f /var/spool/postfix/pid/master.pid
  then
   pid=`cat /var/spool/postfix/pid/master.pid`
-  sudo -n /home/engines/scripts/signal/_kill_postfix.sh $1
+  sudo -n /home/engines/scripts/signal/sudo/_kill_postfix.sh $1
    if ! test $1 = HUP
     then
       wait_for_pid_exit
