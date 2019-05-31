c31202d8c260e10c4a58f074b54bf9d0fb2e9148
James Vodanovich <james@jvodan.com>
2019-05-31 11:31:19 +1000

    correct keyna

--- images/03.ServiceImages/cron/home/engines/scripts/configurators/read_default_notifcation_address.sh
+++ images/03.ServiceImages/cron/home/engines/scripts/configurators/read_default_notifcation_address.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 if test -f /home/engines/scripts/configurators/saved/default_notifcation_address
  then
-  default_notifcation_address=`cat /home/engines/scripts/configurators/saved/default_notifcation_address`
-  echo '{"default_notifcation_address":"'$default_notifcation_address'"}'
+  addr=`cat /home/engines/scripts/configurators/saved/default_notifcation_address`
 else
-  echo '{"default_notifcation_address":"Not Set"}'
+ addr=Not Set
 fi
+
+echo '{"notification_address":"'$addr'"}'
+
 
 exit 0
