048d3eea9a01fe25fbefb503a69220c4b837667a
James Vodanovich <james@jvodan.com>
2019-06-15 09:59:55 +1000

    F

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/email_user_pkcs12.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/email_user_pkcs12.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cert_type=user
 /home/engines/scripts/actionators/email_pkcs12.sh >/tmp/tt
-smtp="smtp=smtp.engines.internal:25"
+smtp="smtp.engines.internal:25"
 export smtp
 echo "Here is your cert" | mailx -s "PKCS12 Cert" -r certs@$external_domain_name -A /tmp/tt $email_address 
