be5f49a95f74f1334e69499dc55ab7cc5102063f
James Vodanovich <james@jvodan.com>
2019-06-16 09:40:19 +1000

    test new ca location

--- images/01.BaseImages/01.base/home/engines/scripts/system/_update_ca.sh
+++ images/01.BaseImages/01.base/home/engines/scripts/system/_update_ca.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if test -f /home/engines/etc/ssl/engines_internal_ca.crt
+if test -f /home/engines/etc/ssl/CA/engines_internal_ca.crt
  then
-   cp /home/engines/etc/ssl/engines_internal_ca.crt  /usr/share/ca-certificates/engines
-   cp /home/engines/etc/ssl/engines_internal_ca.crt  /etc/ssl/certs/engines_internal_ca.pem
+   cp /home/engines/etc/ssl/CA/engines_internal_ca.crt  /usr/share/ca-certificates/engines
+   cp /home/engines/etc/ssl/CA/engines_internal_ca.crt  /etc/ssl/certs/engines_internal_ca.pem
    update-ca-certificates
    touch /home/engines/run/flags/ca-update
 fi
