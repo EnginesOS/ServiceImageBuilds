87e8037c044eed21e495929ded086ae07e31fa94
James Vodanovich <james@jvodan.com>
2019-06-16 21:52:33 +1000

    list a ca's certs

---
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/ca_certs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+. /home/engines/scripts/engine/cert_dirs.sh
+ . /home/engines/functions/checks.sh
+required_values=" ca_name"
+check_required_values
+
+for type in `ls $StoreRoot/$ca_name/ |egrep "csr"|setup`
+do
+	echo Cert $type
+	  for cert in `ls  $StoreRoot/$ca_name/$type | sed "/.pem/s///"`
+	    do
+	    	echo Cert Common Name $cert
+	    done
+done
