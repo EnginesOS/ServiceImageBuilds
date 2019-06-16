4fae08cf3460fe817a4323ce883203c50fa8ec0c
James Vodanovich <james@jvodan.com>
2019-06-16 10:01:20 +1000

    support multiple ca's

--- images/01.BaseImages/01.base/home/engines/scripts/system/_update_ca.sh
+++ images/01.BaseImages/01.base/home/engines/scripts/system/_update_ca.sh
@@ -2,8 +2,6 @@
 
 if test -f /home/engines/etc/ssl/CA/engines_internal_ca.crt
  then
-   cp /home/engines/etc/ssl/CA/engines_internal_ca.crt  /usr/share/ca-certificates/engines
-   cp /home/engines/etc/ssl/CA/engines_internal_ca.crt  /etc/ssl/certs/engines_internal_ca.pem
    update-ca-certificates
    touch /home/engines/run/flags/ca-update
 fi
