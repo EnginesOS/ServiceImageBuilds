2cb3e5cbcff861a3ae0676f7f572f0aa4318a1fe
James Vodanovich <james@jvodan.com>
2019-06-17 15:52:52 +1000

    required values

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/cert_details.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/cert_details.sh
@@ -5,12 +5,9 @@
 
 if test $cert_location = live
   then 
-   required_values="common_name  cert_type consumer_type consumer_name"
-elif test $cert_location = generated
-  then  
-   required_values="common_name cert_type consumer_type consumer_name"
+   required_values="common_name ca_name cert_type consumer_type consumer_name"
 else
-  required_values="common_name  cert_type"
+  required_values="common_name ca_name"
 fi   
 
 check_required_values
@@ -23,27 +20,7 @@ consumer_type=$owner_type
 consumer_name=$owner
 resolve_cert_dir
 
-#if test $cert_location = user
-# then
-#  path=/generated/certs/user/$common_name
-# elif test $cert_location = live
-#  then 
-#   required_values="common_name  cert_location consumer_type consumer_name"
-#   check_required_values
-#    path=/live/${consumer_type}s/$consumer_name/certs/$common_name
-# elif test $cert_location = imported
-#  then
-#   path=/imported/certs/$common_name
-# elif test $cert_location = generated
-#  then  
-#   required_values="common_name cert_location consumer_type consumer_name"
-#   check_required_values
-#   path=/generated/certs/${consumer_type}s/$consumer_name/$common_name
-# elif test $cert_location = external_ca
-#  then 
-#   path=/external_ca/certs/$common_name
-#fi  
-#
+ 
   
 if test -f $cert_dir/$common_name.crt
   then
