3312eba1fd35b5fe305b3db1b8e091376da89ebf
James Vodanovich <james@jvodan.com>
2019-06-17 12:49:51 +1000

    add error checking

--- images/03.ServiceImages/certs/home/engines/scripts/engine/create_ca.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/engine/create_ca.sh
@@ -48,16 +48,40 @@ cat /home/engines/scripts/configurators/saved/$ca_name.ca_setup
 
 mkdir -p $StoreRoot/private/$ca_name
  
-cat /home/engines/templates/openssl.cnf | sed "/CA_NAME/s//$ca_name" > /$StoreRoot/private/$ca_name/open_ssl.cnf
+cat /home/engines/templates/openssl.cnf | sed "/CA_NAME/s//$ca_name/g" > /$StoreRoot/private/$ca_name/open_ssl.cnf
  
-openssl genrsa -out $StoreRoot/private/$ca_name/${ca_name}_CA.key  2048
-openssl req -x509 -new -nodes -key $StoreRoot/private/$ca_name/${ca_name}_CA.key  -days 1024 -sha256 -out $StoreRoot/public/ca/certs/${ca_name}_CA.pem < /home/engines/scripts/configurators/saved/$ca_name.ca_setup
-
+openssl genrsa \
+		-out $StoreRoot/private/$ca_name/${ca_name}_CA.key\
+		2048 \
+		-config $StoreRoot/private/$ca_name/open_ssl.cnf
+if ! test -f $StoreRoot/private/$ca_name/${ca_name}_CA.key
+ then
+  echo '{"status":"error","message":"Failed to create key '$StoreRoot/private/$ca_name/${ca_name}_CA.key'" }'
+  exit 3
+fi  
+openssl req \
+		-x509 \
+		-new \
+		-nodes \
+		-key $StoreRoot/private/$ca_name/${ca_name}_CA.key\  
+		-days 1024 \
+		-sha256 \
+		-out $StoreRoot/public/ca/certs/${ca_name}_CA.pem \
+		< /home/engines/scripts/configurators/saved/$ca_name.ca_setup
+if ! test -f $StoreRoot/public/ca/certs/${ca_name}_CA.pem
+ then
+  echo '{"status":"error","message":"Failed to create CA certificate '$StoreRoot/public/ca/certs/${ca_name}_CA.pem'" }'
+  exit 3
+fi  
 openssl ca -gencrl\
 		   -keyfile $StoreRoot/private/$ca_name/${ca_name}_CA.key  \
 		   -cert /$StoreRoot/public/ca/certs/${ca_name}_CA.pem\
 		    -config $StoreRoot/private/$ca_name/open_ssl.cnf
- 
+ if ! test -$? -eq 0
+ then
+  echo '{"status":"error","message":"Failed to create CA CRL '$StoreRoot/public/ca/certs/${ca_name}_CA.pem'" }'
+  exit 3
+fi 
 chmod og-r $StoreRoot/private/$ca_name/${ca_name}_CA.key     
 echo '{"status":"success"}'
 exit 0
