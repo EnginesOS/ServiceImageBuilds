51a8f46b3a54419a8ef881e5c57b5530cfb5b0d6
James Vodanovich <james@jvodan.com>
2019-06-18 12:18:08 +1000

    f

--- images/03.ServiceImages/backup/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/backup/home/engines/scripts/services/add_service.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
-#cat -  | /home/engines/bin/json_to_env  | sed "/sources='\[/s//sources\[/" | sed "/\]'/s//\]/" > /tmp/.env
-#. /tmp/.env
-
- . /home/engines/functions/checks.sh
+. /home/engines/functions/checks.sh
 . /home/engines/scripts/engine/backup_dirs.sh
  
-
-
 required_values="backup_name dest_folder dest_proto parent_engine backup_type src_type"
 check_required_values
-#echo $1 >/home/engines/scripts/configurators/saved/backup_$parent_engine
-
-#echo "$*" >>/var/log/backup/addbackup.log
-
-Backup_ConfigDir=/home/backup/.duply/
 
 ts=`date`
 echo "$ts:$*" >>/var/log/backup/addbackup.log
@@ -32,22 +22,23 @@ export backup_type
 
 if test $src_type = 'engine'
  then
-   /home/engines/scripts/services/add_backup.sh ${parent_engine}:system
+   /home/engines/scripts/engine/add_backup.sh ${parent_engine}:system
    n=1
-   curl -k https://172.17.0.1:2380/v0/backup/engine/services/${parent_engine} | /home/engines/bin/json_to_env >/tmp/.src
+   curl -k https://172.17.0.1:2380/v0/backup/engine/services/${parent_engine}\
+ | tr -d "\n\r"  |sed "/:/s//=/g" | sed "/,/s//\n/g"  | tr -d "{}\""  >/tmp/.src
    . /tmp/.src
    service=`eval echo service$n`
 	 while ! test -z $service
  	 do
-       /home/engines/scripts/services/add_backup.sh `eval echo service$n`
+       /home/engines/scripts/engine/add_backup.sh `eval echo service$n`
        n=`expr $n + 1`
        service=`eval echo service$n`
  	 done
- 	/home/engines/scripts/services/add_backup.sh config:${parent_engine}	
+ 	/home/engines/scripts/engine/add_backup.sh config:${parent_engine}	
 elif test $backup_type = 'engine_only'
   then
-    /home/engines/scripts/services/add_backup.sh config:${parent_engine}
+    /home/engines/scripts/engine/add_backup.sh config:${parent_engine}
 else
-   /home/engines/scripts/services/add_backup.sh ${parent_engine}/service/${publisher_namespace}/${type_path}/${service_handle}
+   /home/engines/scripts/engine/add_backup.sh ${parent_engine}/service/${publisher_namespace}/${type_path}/${service_handle}
 fi
  exit 0
