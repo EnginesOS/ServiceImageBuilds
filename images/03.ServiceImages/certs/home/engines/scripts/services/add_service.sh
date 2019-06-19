ad120cadb5dda8f7fddd2997fa051142ac277db6
James Vodanovich <james@jvodan.com>
2019-06-19 20:17:42 +1000

    tidy

--- images/03.ServiceImages/certs/home/engines/scripts/services/add_service.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/services/add_service.sh
@@ -3,9 +3,10 @@
  load_cert_defaults
  . /home/engines/functions/checks.sh
 common_name=$cert_name
-install_target=$container_type/$parent_engine
-required_values="install_target container_type parent_engine common_name country state city organisation person"
+
+required_values="container_type parent_engine common_name country state city organisation person"
 check_required_values
+install_target=$container_type/$parent_engine
 cert_type=generated
 export cert_type wild cert_usage container_type install_target parent_engine common_name country state city organisation person ca_name cert_usage
 if $cert_usage = ca
