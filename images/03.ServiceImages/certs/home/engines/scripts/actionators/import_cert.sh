fdfb3d69eb96c327254b1305bf0fe1e7745d240a
James Vodanovich <james@jvodan.com>
2019-06-24 10:59:34 +1000

    install cert now called assign cert

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/import_cert.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/import_cert.sh
@@ -45,7 +45,7 @@ if ! test -z ${install_target}
     then
      /home/engines/scripts/engine/set_default.sh all imported ${common_name}
     else      
-     sudo -n /home/engines/scripts/engine/sudo/_install_target.sh ${install_target} imported ${common_name} ${common_name}
+     sudo -n /home/engines/scripts/engine/sudo/_assign_certificate.sh ${install_target} imported ${common_name} ${common_name}
    fi  
 fi
  
