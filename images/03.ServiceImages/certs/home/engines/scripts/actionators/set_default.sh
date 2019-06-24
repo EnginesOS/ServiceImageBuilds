06db9c71d205748a4bd06c0ae1bf2992d3de6aa7
James Vodanovich <james@jvodan.com>
2019-06-24 13:32:37 +1000

    new values

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/set_default.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/set_default.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
  . /home/engines/functions/checks.sh
-required_values="common_name ca_name install_target"
+required_values="common_name ca_name install_target_type install_target_name"
 check_required_values
-echo "set default" /_install_target.sh ${ca_name} ${common_name}  ${install_target}
-if ! test -z $cert_src
- then
-  common_name=$cert_src/${common_name}
-fi
+install_target=${install_target_type}s/${install_target_name}
+echo "set default" /_install_target.sh ${ca_name} ${common_name} ${install_target}
  
 /home/engines/scripts/engine/set_default.sh  ${ca_name} ${common_name} ${install_target}
