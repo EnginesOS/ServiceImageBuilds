be4b873443445030ca1aaa96720e91e37322aaa6
James Vodanovich <james@jvodan.com>
2019-06-28 13:49:55 +1000

    fix comma's in json string

--- images/03.ServiceImages/certs/home/engines/scripts/actionators/list_csr.sh
+++ images/03.ServiceImages/certs/home/engines/scripts/actionators/list_csr.sh
@@ -5,9 +5,11 @@ n=0
 echo '{"pending_csr_list":['
 for csr in `ls $pending_csr_dir`
  do
-   if test $n -eq 0
+   if test $n -ne 0
       then
        echo ","
+   else
+    n=1
    fi
    csr=`echo $csr |sed "s/\.csr//"` 
    
