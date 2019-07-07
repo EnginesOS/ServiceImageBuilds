b5b7c7744f4aa5f2d6e419eddb3969de086d6fde
James Vodanovich <james@jvodan.com>
2019-07-07 11:33:49 +1000

    ensure any dirs left are group container (g+rwx

--- images/05.SystemApplications/fsconfigurator/home/remove_container.sh
+++ images/05.SystemApplications/fsconfigurator/home/remove_container.sh
@@ -8,22 +8,34 @@ for cmd in $*
  	state)
  	     dir="/client/state/"
  		/home/remove_dir_contents.sh $dir
- 		chown 21000.22020 -R /client/state/
+ 		chown 21000.22020 -R $dir
+ 		chmod g+rwx R $dir
  		;;
  	logs)
  		dir="/client/log/"
  		/home/remove_dir_contents.sh $dir
+ 		chown 21000.22020 -R $dir
+ 		chmod g+rwx R $dir
  		;;
  	fs)	
  		dir="/dest/fs/"
- 		/home/remove_dir_contents.sh $dir 	    
+ 		/home/remove_dir_contents.sh $dir
+ 		chown 21000.22020 -R $dir	  
+ 		chmod g+rwx R $dir  
  		;;
  	all)
  		dir="/client/log/"
  		/home/remove_dir_contents.sh $dir
- 		chown 21000.22020 -R /client/state/ 		
+ 		chown 21000.22020 -R $dir	  
+ 		chmod g+rwx R $dir  
  		dir="/client/state/"
  		/home/remove_dir_contents.sh $dir
+ 		chown 21000.22020 -R $dir	  
+ 		chmod g+rwx R $dir  		
+ 		dir="/client/fs/"
+ 		/home/remove_dir_contents.sh $dir
+ 		chown 21000.22020 -R $dir	  
+ 		chmod g+rwx R $dir  		
  		;;
   esac
  done
