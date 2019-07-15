a7cbf3e6e7ca15fd61fb1208ffae1027a7a5e496
James Vodanovich <james@jvodan.com>
2019-07-15 18:34:36 +1000

    f

--- images/03.ServiceImages/smtp/home/engines/scripts/signal/kill_sleep.sh
+++ images/03.ServiceImages/smtp/home/engines/scripts/signal/kill_sleep.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo -n /home/engines/scripts/signal/sudo/_kill_sleep.sh $1
  
--- images/03.ServiceImages/smtp/home/engines/scripts/startup/init_dbs.sh
+++ images/03.ServiceImages/smtp/home/engines/scripts/startup/init_dbs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 sudo -n /home/engines/scripts/startup/sudo/_setup_dirs.sh
 
