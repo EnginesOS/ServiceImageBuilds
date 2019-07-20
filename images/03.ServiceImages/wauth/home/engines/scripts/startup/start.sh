952731f7594d1942bcb38ed05c91afab01d3962f
James Vodanovich <james@jvodan.com>
2019-07-20 13:57:26 +1000

    correct params

--- images/03.ServiceImages/wauth/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/wauth/home/engines/scripts/startup/start.sh
@@ -6,11 +6,11 @@ startup_complete
 
 wait
 exit_code=$?
-/home/app/nginx-ldap-auth-daemon.py -h 0.0.0.0\
+/home/app/nginx-ldap-auth-daemon.py --host 0.0.0.0\
 									-b "ou=people,dc=engines,dc=internal"\
 									-u ldap://ldap:389 \
-									 -f 'uid=%(username)s)' \
 									 -D $ldap_dn\
-									 -w $ldap_password
+									 -w $ldap_password &
+									
 									 
 shutdown_complete
