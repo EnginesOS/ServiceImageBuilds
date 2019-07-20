ed1a48b1ccd2a0e4e3fd94388058859ba9a2f688
James Vodanovich <james@jvodan.com>
2019-07-20 13:21:20 +1000

    args in ''

--- images/03.ServiceImages/wauth/home/engines/scripts/startup/start.sh
+++ images/03.ServiceImages/wauth/home/engines/scripts/startup/start.sh
@@ -10,7 +10,7 @@ exit_code=$?
 /home/app/nginx-ldap-auth-daemon.py -h 0.0.0.0\
 									-b "ou=people,dc=engines,dc=internal"\
 									-u ldap://ldap:389 \
-									 -f uid=%(username)s) \
+									 -f 'uid=%(username)s)' \
 									 -D $ldap_dn\
 									 -w $ldap_password
 									 
