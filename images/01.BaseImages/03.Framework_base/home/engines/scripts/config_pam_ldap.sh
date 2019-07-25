5162d60cd1c8d1c4373e1125c59498fb4ee3e96c
James Vodanovich <james@jvodan.com>
2019-07-26 09:31:40 +1000

    f

--- images/01.BaseImages/03.Framework_base/home/engines/scripts/config_pam_ldap.sh
+++ images/01.BaseImages/03.Framework_base/home/engines/scripts/config_pam_ldap.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+cat /home/engines/templates/ldap.conf | while read LINE
+ do
+    eval echo "$LINE" >> /tmp/ldap.conf
+ done
+ 
  sudo -n /home/engines/scripts/sudo/_config_pam_ldap.sh
--- images/01.BaseImages/03.Framework_base/home/engines/scripts/sudo/_config_pam_ldap.sh
+++ images/01.BaseImages/03.Framework_base/home/engines/scripts/sudo/_config_pam_ldap.sh
@@ -2,11 +2,7 @@
 
 cp /home/engines/templates/common-password /etc/pam.d
 echo "session optional pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session
-rm /etc/ldap.con
 
-cat /home/engines/templates/ldap.conf | while read LINE
- do
-    eval echo "$LINE" >> /etc/ldap.conf
- done
- 
+
+ mv /tmp/ldap.conf /etc/ldap.conf
  
