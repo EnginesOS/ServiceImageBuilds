21a1deb85f2914fb83f3d17494e3f1050d55622a
James Vodanovich <james@jvodan.com>
2019-07-25 14:04:31 +1000

    ldap login support

--- images/01.BaseImages/03.Framework_base/home/engines/scripts/sudo/_config_pam_ldap.sh
+++ images/01.BaseImages/03.Framework_base/home/engines/scripts/sudo/_config_pam_ldap.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 cat /home/engines/templates/ldap.conf >/etc/ldap.conf
+cp /home/engines/templates/copmmon
+echo "session optional pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session
