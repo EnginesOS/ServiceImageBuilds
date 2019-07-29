53fb98e72a25769c30112572406ddc01de2464ea
James Vodanovich <james@jvodan.com>
2019-07-29 19:49:41 +1000

    only setup ldap if we have ldap cred

--- images/01.BaseImages/01.base/home/engines/scripts/config_pam_ldap.sh
+++ images/01.BaseImages/01.base/home/engines/scripts/config_pam_ldap.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+if  test -z "$ldap_dn" -o -z "$ldap_password"
+ then
+ echo no vars
+  exit
+fi
+
+
 cat /home/engines/templates/etc/ldap.conf | while read LINE
- do
-    eval echo "$LINE" >> /tmp/ldap.conf
- done
- 
+  do
+     eval echo "$LINE" >> /tmp/ldap.conf
+  done
+
  sudo -n /home/engines/scripts/sudo/_config_pam_ldap.sh
+
