1de15309afe2fdb5a943030f358340739289b327
James Vodanovich <james@jvodan.com>
2019-08-25 14:56:39 +1000

    support variable key lenght

--- images/03.ServiceImages/volmgr/home/engines/scripts/services/sudo/_create_secret.sh
+++ images/03.ServiceImages/volmgr/home/engines/scripts/services/sudo/_create_secret.sh
@@ -6,7 +6,7 @@ if ! test -d  /var/secrets/${container_type}s/${parent_engine}/
    mkdir -p /var/secrets/${container_type}s/${parent_engine}/
 fi   
 
-dd if=/dev/urandom count=16 bs=1  | od -h | awk '{ print $2$3$4$6$6$7$8$9}'\
+dd if=/dev/urandom count=$1 bs=1  | od -h | awk '{ print $2$3$4$6$6$7$8$9}'\
 	| head -1 >/var/secrets/${container_type}s/${parent_engine}/${service_handle} 
 
 chown -R ${ContUser}.containers /var/secrets/${container_type}s/${parent_engine}/${service_handle}
