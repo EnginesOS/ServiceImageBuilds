a81d84dba0d537a54bd38d224e75d8a507641965
James Vodanovich <james@jvodan.com>
2019-09-28 11:13:57 +1000

    fix chmods and add g+-r

--- images/01.BaseImages/03.Framework_base/home/engines/scripts/grant_ugo_r_access.sh
+++ images/01.BaseImages/03.Framework_base/home/engines/scripts/grant_ugo_r_access.sh
@@ -9,4 +9,4 @@ else
 fi
 
 path=`echo $entered_path |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
-sudo -n -u data-user /home/engines/scripts/sudo/_grant_ugo_rw_access.sh $recursive $path
+sudo -n -u data-user /home/engines/scripts/sudo/_grant_ugo_r_access.sh $recursive $path
--- images/01.BaseImages/03.Framework_base/home/engines/scripts/sudo/_grant_rw_access.sh
+++ images/01.BaseImages/03.Framework_base/home/engines/scripts/sudo/_grant_rw_access.sh
@@ -9,6 +9,6 @@ else
 fi
 path=`echo $entered_path |sed '/[ ;\\\"\`]/s///g ' | sed '/\.\./s///g'`
 #FIXME needs to handle target of symbolic link
-echo chmod g+w $recursive /home/$path
- chmod g+w $recursive /home/$path
+echo chmod g+rw $recursive /home/$path
+ chmod g+rw $recursive /home/$path
  
